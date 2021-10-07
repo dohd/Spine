9bd7779c9aa6c8c680dda82507465ede0453a511
Dohd <alloicebrian@gmail.com>
2021-10-07 11:59:32 +0300

    Update focus.leads.edit template

--- resources/views/focus/leads/edit.blade.php
+++ resources/views/focus/leads/edit.blade.php
@@ -1,11 +1,11 @@
 @extends ('core.layouts.app')
 
-@section ('title', 'Branch Management |  Branch Edit')
+@section ('title', 'Lead Management |  Lead Edit')
 
 @section('page-header')
     <h1>
-         Branch Management
-        <small>Edit Branch</small>
+         Lead Management
+        <small>Edit Lead</small>
     </h1>
 @endsection
 
@@ -14,14 +14,13 @@
         <div class="content-wrapper">
             <div class="content-header row">
                 <div class="content-header-left col-md-6 col-12 mb-2">
-                    <h4 class="mb-0">Edit Branch</h4>
-
+                    <h4 class="mb-0">Edit Lead</h4>
                 </div>
                 <div class="content-header-right col-md-6 col-12">
                     <div class="media width-250 float-right">
 
                         <div class="media-body media-right text-right">
-                             @include('focus.branches.partials.branches-header-buttons')
+                             @include('focus.leads.partials.leads-header-buttons')
                         </div>
                     </div>
                 </div>
@@ -30,26 +29,22 @@
                 <div class="row">
                     <div class="col-12">
                         <div class="card">
-
                             <div class="card-content">
-
                                 <div class="card-body">
-                                    {{ Form::model($branches, ['route' => ['biller.branches.update', $branches], 'class' => 'form-horizontal', 'role' => 'form', 'method' => 'PATCH', 'id' => 'edit-branch']) }}
+                                    {{ Form::model($lead, ['route' => ['biller.leads.update', $lead], 'class' => 'form-horizontal', 'role' => 'form', 'method' => 'PATCH', 'id' => 'edit-lead']) }}
 
-                                    <div class="form-group">
-                                        {{-- Including Form blade file --}}
-                                        @include("focus.branches.form")
-                                        <div class="edit-form-btn">
-                                            {{ link_to_route('biller.branches.index', trans('buttons.general.cancel'), [], ['class' => 'btn btn-danger btn-md']) }}
-                                            {{ Form::submit(trans('buttons.general.crud.update'), ['class' => 'btn btn-primary btn-md']) }}
-                                            <div class="clearfix"></div>
-                                        </div><!--edit-form-btn-->
-                                    </div><!--form-group-->
+                                        <div class="form-group">
+                                            {{-- Including Form blade file --}}
+                                            @include("focus.leads.form")
+                                            <div class="edit-form-btn">
+                                                {{ link_to_route('biller.leads.index', trans('buttons.general.cancel'), [], ['class' => 'btn btn-danger btn-md']) }}
+                                                {{ Form::submit(trans('buttons.general.crud.update'), ['class' => 'btn btn-primary btn-md']) }}
+                                                <div class="clearfix"></div>
+                                            </div><!--edit-form-btn-->
+                                        </div><!--form-group-->
 
                                     {{ Form::close() }}
                                 </div>
-
-
                             </div>
                         </div>
                     </div>
