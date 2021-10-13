243e3276128c004e775427d0997d9ddcbf0adab1
Dohd <alloicebrian@gmail.com>
2021-10-13 08:32:01 +0300

    Update focus.leads.index

--- resources/views/focus/leads/index.blade.php
+++ resources/views/focus/leads/index.blade.php
@@ -3,38 +3,36 @@
 @section ('title', 'Leads Management')
 
 @section('page-header')
-    <h1>Leads Management</h1>
+<h1>Leads Management</h1>
 @endsection
 
 @section('content')
-    <div class="">
-        <div class="content-wrapper">
-            <div class="content-header row">
-                <div class="content-header-left col-md-6 col-12 mb-2">
-                    <h4 class=" mb-0">Leads Management</h4>
+<div class="">
+    <div class="content-wrapper">
+        <div class="content-header row">
+            <div class="content-header-left col-md-6 col-12 mb-2">
+                <h4 class=" mb-0">Leads Management</h4>
 
-                </div>
-                <div class="content-header-right col-md-6 col-12">
-                    <div class="media width-250 float-right">
+            </div>
+            <div class="content-header-right col-md-6 col-12">
+                <div class="media width-250 float-right">
 
-                        <div class="media-body media-right text-right">
-                            @include('focus.leads.partials.leads-header-buttons')
-                        </div>
+                    <div class="media-body media-right text-right">
+                        @include('focus.leads.partials.leads-header-buttons')
                     </div>
                 </div>
             </div>
-            <div class="content-body">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="card">
+        </div>
+        <div class="content-body">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
 
-                            <div class="card-content">
+                        <div class="card-content">
 
-                                <div class="card-body">
-                                    <table id="leads-table"
-                                           class="table table-striped table-bordered zero-configuration" cellspacing="0"
-                                           width="100%">
-                                        <thead>
+                            <div class="card-body">
+                                <table id="leads-table" class="table table-striped table-bordered zero-configuration" cellspacing="0" width="100%">
+                                    <thead>
                                         <tr>
                                             <th>#</th>
                                             <th>Lead ID</th>
@@ -42,86 +40,135 @@
                                             <th>New/Existing</th>
                                             <th>Client & Branch</th>
                                             <th>Source</th>
-                                            
-                                          <th>{{ trans('general.createdat') }}</th>
+                                            <th>{{ trans('general.createdat') }}</th>
+                                            <th>Status</th>
                                             <th>{{ trans('labels.general.actions') }}</th>
                                         </tr>
-                                        </thead>
+                                    </thead>
 
-
-                                        <tbody>
+                                    <tbody>
                                         <tr>
-                                            <td colspan="100%" class="text-center text-success font-large-1"><i
-                                                        class="fa fa-spinner spinner"></i></td>
+                                            <td colspan="100%" class="text-center text-success font-large-1">
+                                                <i class="fa fa-spinner spinner"></i>
+                                            </td>
                                         </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
-
-
+                                    </tbody>
+                                </table>
                             </div>
+
+
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
 
 @section('after-scripts')
-    {{-- For DataTables --}}
-    {{ Html::script(mix('js/dataTable.js')) }}
+{{-- For DataTables --}}
+{{ Html::script(mix('js/dataTable.js')) }}
 
-    <script>
-        $(function () {
-            setTimeout(function () {
-                draw_data()
-            }, {{config('master.delay')}});
+<script>
+    $(function() {
+        setTimeout(function() {
+            draw_data()
+        }, "{{ config('master.delay')}}");
+    });
+
+    function draw_data() {
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
         });
 
-        function draw_data() {
-            $.ajaxSetup({
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                }
-            });
+        const datatableLang = { @lang("datatable.strings") };
 
-            var dataTable = $('#leads-table').dataTable({
-                processing: true,
-                serverSide: true,
-                responsive: true,
-                language: {
-                    @lang('datatable.strings')
+        var dataTable = $('#leads-table').dataTable({
+            processing: true,
+            serverSide: true,
+            responsive: true,
+            language: datatableLang,
+            ajax: {
+                url: '{{ route("biller.leads.get") }}',
+                type: 'post',
+                data: { c_type: 0 }
+            },
+            columns: [
+                {
+                    data: 'DT_Row_Index',
+                    name: 'id'
                 },
-                ajax: {
-                    url: '{{ route("biller.leads.get") }}',
-                    type: 'post',
-                    data: {c_type: 0}
+                {
+                    data: 'reference',
+                    name: 'reference'
                 },
-                columns: [
-                    {data: 'DT_Row_Index', name: 'id'},
-                    {data: 'reference', name: 'reference'},
-                    {data: 'title', name: 'title'},
-                    {data: 'client_status', name: 'client_status'},
-                    {data: 'client_name', name: 'client_name'},
-                    {data: 'source', name: 'source'},
-                    {data: 'created_at', name: '{{config('module.leads.table')}}.created_at'},
-                    {data: 'actions', name: 'actions', searchable: false, sortable: false}
-                ],
-                order: [[0, "asc"]],
-                searchDelay: 500,
-                dom: 'Blfrtip',
-                buttons: {
-                    buttons: [
-
-                        {extend: 'csv', footer: true, exportOptions: {columns: [0, 1]}},
-                        {extend: 'excel', footer: true, exportOptions: {columns: [0, 1]}},
-                        {extend: 'print', footer: true, exportOptions: {columns: [0, 1]}}
-                    ]
+                {
+                    data: 'title',
+                    name: 'title'
+                },
+                {
+                    data: 'client_status',
+                    name: 'client_status'
+                },
+                {
+                    data: 'client_name',
+                    name: 'client_name'
+                },
+                {
+                    data: 'source',
+                    name: 'source'
+                },
+                {
+                    data: 'created_at',
+                    name: "{{config('module.leads.table ')}}.created_at"
+                },
+                {
+                    data: 'status',
+                    name: 'status'
+                },
+                {
+                    data: 'actions',
+                    name: 'actions',
+                    searchable: false,
+                    sortable: false
                 }
-            });
-            $('#branches-table_wrapper').removeClass('form-inline');
+            ],
+            order: [
+                [0, "asc"]
+            ],
+            searchDelay: 500,
+            dom: 'Blfrtip',
+            buttons: {
+                buttons: [
+                    {
+                        extend: 'csv',
+                        footer: true,
+                        exportOptions: {
+                            columns: [0, 1]
+                        }
+                    },
+                    {
+                        extend: 'excel',
+                        footer: true,
+                        exportOptions: {
+                            columns: [0, 1]
+                        }
+                    },
+                    {
+                        extend: 'print',
+                        footer: true,
+                        exportOptions: {
+                            columns: [0, 1]
+                        }
+                    }
+                ]
+            }
+        });
 
-        }
-    </script>
+        // $('#branches-table_wrapper').removeClass('form-inline');
+    }
+</script>
 @endsection
