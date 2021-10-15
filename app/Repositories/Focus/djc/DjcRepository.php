bccfd7dbfce8c02e8c8ae0238bde5a10d72ae450
Dohd <alloicebrian@gmail.com>
2021-10-15 16:50:01 +0300

    Format document

--- app/Repositories/Focus/djc/DjcRepository.php
+++ app/Repositories/Focus/djc/DjcRepository.php
@@ -19,31 +19,24 @@ use Mavinoo\LaravelBatch\LaravelBatchFacade as Batch;
  */
 class DjcRepository extends BaseRepository
 {
-      /**
+    /**
      *file_path .
      *
      * @var string
      */
     protected $file_path;
+
     /**
      * Storage Class Object.
      *
      * @var \Illuminate\Support\Facades\Storage
      */
     protected $storage;
+
     /**
      * Associated Repository Model.
      */
-
-
     const MODEL = Djc::class;
-
-    /**
-     * This method is used by Table Controller
-     * For getting the table data to show in
-     * the grid
-     * @return mixed
-     */
 
     public function __construct()
     {
@@ -51,17 +44,21 @@ class DjcRepository extends BaseRepository
         $this->storage = Storage::disk('public');
     }
 
-
+    /**
+     * This method is used by Table Controller
+     * For getting the table data to show in
+     * the grid
+     * @return mixed
+     */
     public function getForDataTable()
     {
-        
-       $q=$this->query();
-      // $q->when(!request('rel_type'), function ($q) {
-           // return $q->where('c_type', '=',request('rel_type',0));
+        $q = $this->query();
+        // $q->when(!request('rel_type'), function ($q) {
+        // return $q->where('c_type', '=',request('rel_type',0));
         //});
-       //$q->when(request('rel_type'), function ($q) {
-           // return $q->where('rel_id', '=',request('rel_id',0));
-       // });
+        //$q->when(request('rel_type'), function ($q) {
+        // return $q->where('rel_id', '=',request('rel_id',0));
+        // });
 
         return $q->get();
     }
@@ -71,95 +68,94 @@ class DjcRepository extends BaseRepository
      *
      * @param array $input
      * @throws GeneralException
-     * @return bool
+     * @return object
      */
     public function create(array $input)
     {
-
         $input['data']['report_date'] = date_for_database($input['data']['report_date']);
 
-         if (!empty($input['data']['image_one'])) {
-                    $input['data']['image_one']= $this->uploadFile($input['data']['image_one']);
-                }
-     if (!empty($input['data']['image_two'])) {
-                    $input['data']['image_two']= $this->uploadFile($input['data']['image_two']);
-                }
-     if (!empty($input['data']['image_three'])) {
-                    $input['data']['image_three']= $this->uploadFile($input['data']['image_three']);
-                }
-      if (!empty($input['data']['image_four'])) {
-                    $input['data']['image_four']= $this->uploadFile($input['data']['image_four']);
-                }
-      
- 
+        if (!empty($input['data']['image_one'])) {
+            $input['data']['image_one'] = $this->uploadFile($input['data']['image_one']);
+        }
+        if (!empty($input['data']['image_two'])) {
+            $input['data']['image_two'] = $this->uploadFile($input['data']['image_two']);
+        }
+        if (!empty($input['data']['image_three'])) {
+            $input['data']['image_three'] = $this->uploadFile($input['data']['image_three']);
+        }
+        if (!empty($input['data']['image_four'])) {
+            $input['data']['image_four'] = $this->uploadFile($input['data']['image_four']);
+        }
+
         DB::beginTransaction();
-        $action_taken= $input['data']['action_taken'];
-        $root_cause= $input['data']['root_cause'];
-        $recommendations= $input['data']['recommendations'];
-        $input['data'] = array_map( 'strip_tags', $input['data']);
-        $input['data']['action_taken']=strip_tags($action_taken,config('general.allowed'));
-        $input['data']['root_cause']=strip_tags($root_cause,config('general.allowed'));
-        $input['data']['recommendations']=strip_tags($recommendations,config('general.allowed'));
+        $action_taken = $input['data']['action_taken'];
+        $root_cause = $input['data']['root_cause'];
+        $recommendations = $input['data']['recommendations'];
+        $input['data'] = array_map('strip_tags', $input['data']);
+        $input['data']['action_taken'] = strip_tags($action_taken, config('general.allowed'));
+        $input['data']['root_cause'] = strip_tags($root_cause, config('general.allowed'));
+        $input['data']['recommendations'] = strip_tags($recommendations, config('general.allowed'));
         $result = Djc::create($input['data']);
- if ($result) {
+
+        if ($result) {
             $dataitems = array();
             $i = 0;
-
-        foreach ($input['data_item']['tag_number'] as $key => $value) {
-
-
-
-
-
-                 $dataitems[] = array('djc_id' => $result->id, 'tag_number' => $input['data_item']['tag_number'][$key], 'make' => $input['data_item']['make'][$key], 'equipment_type' => $input['data_item']['equipment_type'][$key], 'joc_card' => $input['data_item']['joc_card'][$key], 'capacity' => $input['data_item']['capacity'][$key], 'location' => $input['data_item']['location'][$key], 'last_service_date' => date_for_database($input['data_item']['last_service_date'][$key]), 'next_service_date' => date_for_database($input['data_item']['next_service_date'][$key]), 'ins' => $result->ins);
-
-
+            foreach ($input['data_item']['tag_number'] as $key => $value) {
+                $dataitems[] = array(
+                    'djc_id' => $result->id, 
+                    'tag_number' => $input['data_item']['tag_number'][$key], 
+                    'make' => $input['data_item']['make'][$key], 
+                    'equipment_type' => $input['data_item']['equipment_type'][$key], 
+                    'joc_card' => $input['data_item']['joc_card'][$key], 
+                    'capacity' => $input['data_item']['capacity'][$key], 
+                    'location' => $input['data_item']['location'][$key], 
+                    'last_service_date' => date_for_database($input['data_item']['last_service_date'][$key]), 
+                    'next_service_date' => date_for_database($input['data_item']['next_service_date'][$key]), 
+                    'ins' => $result->ins
+                );
                 $i++;
             }
 
-        DjcItem::insert($dataitems);
+            DjcItem::insert($dataitems);
+            DB::commit();
 
-    DB::commit();
-     return $result->id;
+            return $result->id;
+        }
 
- }
-
-        throw new GeneralException('Error Creating Branch');
-
-
-
-
+        throw new GeneralException('Error Creating Djc');
     }
 
     /**
      * For updating the respective Model in storage
      *
-     * @param Productcategory $productcategory
-     * @param  $input
+     * @param Djc $djc
+     * @param  array $input
      * @throws GeneralException
-     * return bool
+     * @return object
      */
-    public function update(Branch $branch, array $input)
+    public function update(array $input)
     {
-        $input = array_map( 'strip_tags', $input);
-    	if ($branch->update($input))
-            return true;
+        
+        error_log('===djc update called===');
+        error_log(print_r($input['data'], true));
+        error_log(print_r($input['data_item'], true));
 
-        throw new GeneralException(trans('exceptions.backend.productcategories.update_error'));
+        // $input = array_map('strip_tags', $input);
+        // if ($djc->update($input)) return true;
+
+        // throw new GeneralException('Error Updating Djc');
     }
 
     /**
      * For deleting the respective model from storage
      *
-     * @param Productcategory $productcategory
+     * @param Djc $djc
      * @throws GeneralException
      * @return bool
      */
-    public function delete(Branch $branch)
+    public function delete(Djc $djc)
     {
-        if ($branch->delete()) {
-            return true;
-        }
+        if ($djc->delete()) return true;
 
         throw new GeneralException(trans('exceptions.backend.productcategories.delete_error'));
     }
@@ -167,7 +163,6 @@ class DjcRepository extends BaseRepository
     public function uploadFile($file)
     {
         $path = $this->file_path;
-
         $file_name = time() . $file->getClientOriginalName();
 
         $this->storage->put($path . $file_name, file_get_contents($file->getRealPath()));
