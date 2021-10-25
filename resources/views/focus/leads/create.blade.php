8956e5c0b56b902d20d0822f7063b07171399d94
Dohd <alloicebrian@gmail.com>
2021-10-25 12:59:08 +0300

    Remove empty lines

--- resources/views/focus/leads/create.blade.php
+++ resources/views/focus/leads/create.blade.php
@@ -15,7 +15,6 @@
             <div class="content-header row">
                 <div class="content-header-left col-md-6 col-12 mb-2">
                     <h4 class="mb-0">Create Lead</h4>
-
                 </div>
                 <div class="content-header-right col-md-6 col-12">
                     <div class="media width-250 float-right">
@@ -35,8 +34,6 @@
 
                                 <div class="card-body">
                                     {{ Form::open(['route' => 'biller.leads.store', 'class' => 'form-horizontal', 'role' => 'form', 'method' => 'post', 'id' => 'create-productcategory']) }}
-
-
                                     <div class="form-group">
                                         {{-- Including Form blade file --}}
                                         @include("focus.leads.form")
@@ -44,13 +41,10 @@
                                             {{ link_to_route('biller.leads.index', trans('buttons.general.cancel'), [], ['class' => 'btn btn-danger btn-md']) }}
                                             {{ Form::submit(trans('buttons.general.crud.create'), ['class' => 'btn btn-primary btn-md']) }}
                                             <div class="clearfix"></div>
-                                        </div><!--edit-form-btn-->
-                                    </div><!-- form-group -->
-
+                                        </div>
+                                    </div>
                                     {{ Form::close() }}
                                 </div>
-
-
                             </div>
                         </div>
                     </div>
