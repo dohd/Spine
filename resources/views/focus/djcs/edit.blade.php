5aa1cd11ac7e1dbc0cac9acc08cc9411e67b520d
Dohd <alloicebrian@gmail.com>
2021-10-29 17:57:13 +0300

    Remove invalid @include

--- resources/views/focus/djcs/edit.blade.php
+++ resources/views/focus/djcs/edit.blade.php
@@ -219,7 +219,6 @@
 @include("focus.modal.customer")
 @endsection
 @section('extra-scripts')
-@inclue("focus.djcs.edit")
 
 <link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
 <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
