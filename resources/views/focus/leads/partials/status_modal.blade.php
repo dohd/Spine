bc77d604cf21023dd60905d232ca0a68811021ba
Dohd <alloicebrian@gmail.com>
2021-10-30 13:12:09 +0300

    Add status_modal

---
+++ resources/views/focus/leads/partials/status_modal.blade.php
@@ -0,0 +1,27 @@
+<div class="modal fade" id="status-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content w-75">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modal-status-label">Update Status</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            {{ Form::model($lead, ['route' => ['biller.leads.edit_status', $lead->id], ]) }}
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="status">Status</label>
+                        <select class="form-control" name="status" id="status">
+                            <option value="0">Open</option>
+                            <option value="1">Closed</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    {{ Form::submit('Update', ['class' => "btn btn-primary"]) }}
+                </div>
+            {{ Form::close() }}
+        </div>
+    </div>
+</div>
--- resources/views/focus/leads/view.blade.php
+++ resources/views/focus/leads/view.blade.php
@@ -15,7 +15,6 @@
         </div>
         <div class="content-header-right col-md-6 col-12">
             <div class="media width-250 float-right">
-
                 <div class="media-body media-right text-right">
                     @include('focus.leads.partials.leads-header-buttons')
                 </div>
@@ -24,9 +23,15 @@
     </div>
     
     <div class="card">
-        <h5 class="card-header">Lead</h5>
+        <div class="card-header">
+            <button type="button" class="btn btn-sm btn-success font-weight-bold" data-toggle="modal" data-target="#status-modal">
+                <i class="fa fa-pencil"></i> Status
+            </button>
+            @include('focus.leads.partials.status_modal')
+        </div>
+
         <div class="card-body">
-            <h5 class="card-title mb-1">{{ $lead->title }}</h5>
+            <h5 class="card-title mb-1"><b>Title:</b>&nbsp;&nbsp;{{ $lead->title }}</h5>
             <table id="leads-table" class="table table-lg table-bordered zero-configuration" cellspacing="0" width="100%">
                 <tbody>
                     <tr>
@@ -43,13 +48,13 @@
                     </tr> 
                     <tr>
                         <th>Client Name</th>
-                        <td>{{ $lead->client_name ?: $lead->customer->name }}</td>
+                        <td>{{ $lead->customer ? $lead->customer->name : $lead->client_name }}</td>
                     </tr>
                     <tr>
                         <th>Client Ref / Callout ID</th>
                         <td>{{ $lead->client_ref }}</td>
                     </tr>
-                    @if ($lead->branch->name)
+                    @if ($lead->branch)
                         <tr>
                             <th>Client Branch</th>
                             <td>{{ $lead->branch->name }}</td>
@@ -57,17 +62,16 @@
                     @endif
                     <tr>
                         <th>Client Contact</th>
-                        <td>{{ $lead->client_contact ?: $lead->customer->phone }}</td>
+                        <td>{{ $lead->customer ? $lead->customer->phone : $lead->client_contact }}</td>
                     </tr>
                     <tr>
                         <th>Client Email</th>
-                        <td>{{ $lead->client_email ?: $lead->customer->email }}</td>
-                    </tr>                      
-                    
+                        <td>{{ $lead->customer? $lead->customer->email : $lead->client_email }}</td>
+                    </tr>  
                     <tr><th></th></tr>                                     
                     <tr>
                         <th>Date of Request</th>
-                        <td>{{ date('d-m-Y', strtotime($lead->date_of_request)) }}</td>
+                        <td>{{ dateFormat($lead->date_of_request) }}</td>
                     </tr>
                     <tr>
                         <th>Cost</th>
@@ -87,7 +91,7 @@
                     </tr>
                     <tr>
                         <th>Created at</th>
-                        <td>{{ date('d-m-Y', strtotime($lead->created_at)) }}</td>
+                        <td>{{ dateFormat($lead->created_at) }}</td>
                     </tr>
                 </tbody>
             </table>
@@ -99,4 +103,8 @@
 @section('after-scripts')
 {{-- For DataTables --}}
 {{ Html::script(mix('js/dataTable.js')) }}
+<script>
+    // default status modal select value
+    $('#status').val("{{ $lead->status }}");
+</script>
 @endsection
