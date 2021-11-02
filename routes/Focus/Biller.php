5c68dc1d21b31f6b524d5fe3c07c9c955a82872e
Dohd <alloicebrian@gmail.com>
2021-11-02 13:01:26 +0300

    Update update_pi route

--- routes/Focus/Biller.php
+++ routes/Focus/Biller.php
@@ -242,7 +242,7 @@ Route::group(['namespace' => 'quote'], function () {
     Route::get('quotes/create_pi', 'QuotesController@create_pi')->name('quotes.create_pi');
     Route::post('quotes/store_pi', 'QuotesController@store_pi')->name('quotes.store_pi');
     Route::get('quotes/edit_pi', 'QuotesController@edit_pi')->name('quotes.edit_pi');
-    Route::patch('quotes/update_pi', 'QuotesController@update_pi')->name('quotes.update_pi');
+    Route::patch('quotes/{id}/update_pi', 'QuotesController@update_pi')->name('quotes.update_pi');
 
     Route::post('quotes/lpo', 'QuotesController@update_lpo')->name('quotes.lpo');
     Route::resource('quotes', 'QuotesController');
