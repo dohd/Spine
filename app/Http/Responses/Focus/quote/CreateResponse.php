3452d6f24c515a550e897a6804ca93f6fb5e6329
Dohd <alloicebrian@gmail.com>
2021-11-05 23:50:19 +0300

    Add page variable and pi conditional logic

--- app/Http/Responses/Focus/quote/CreateResponse.php
+++ app/Http/Responses/Focus/quote/CreateResponse.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Responses\Focus\quote;
 
+use App\Models\bank\Bank;
 use App\Models\quote\Quote;
 use Illuminate\Contracts\Support\Responsable;
 use App\Models\lead\Lead;
 
 class CreateResponse implements Responsable
 {
+    protected $page;
+
+    /**
+     * @param string $quote
+     */
+    public function __construct($page)
+    {
+        $this->page = $page;
+    }
+
     /**
      * To Response
      *
@@ -19,6 +30,15 @@ class CreateResponse implements Responsable
     {
         $last_invoice = Quote::orderBy('id', 'desc')->where('i_class', '=', 0)->first();
         $leads = Lead::all();
+
+        // create proformer invoice
+        if ($this->page == 'pi') {
+            $banks = Bank::all();
+            
+            return view('focus.quotes.create_pi')
+                ->with(compact('last_invoice', 'leads', 'banks'))
+                ->with(bill_helper(2, 4));
+        }
 
         return view('focus.quotes.create')
             ->with(compact('last_invoice','leads'))
