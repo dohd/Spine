4ab906681063dedf3a2b5fd915fecccefd9c826e
Dohd <alloicebrian@gmail.com>
2021-11-06 13:00:29 +0300

    Add input labels and quote select menu

--- resources/views/focus/projects/modal/project_new.blade.php
+++ resources/views/focus/projects/modal/project_new.blade.php
@@ -1,7 +1,6 @@
 <div class="modal" id="AddProjectModal" role="dialog" aria-labelledby="data_project" aria-hidden="true">
     <div class="modal-dialog modal-xl" role="document">
         <div class="modal-content ">
-
             <section class="todo-form">
                 <form id="data_form_project" class="todo-input">
                     <div class="modal-header">
@@ -13,51 +12,65 @@
                     <div class="modal-body">
                         <div class="row">
                             <fieldset class="form-group col-8">
-                                <input type="text" class="new-todo-item form-control required"
-                                       placeholder="{{trans('projects.name')}}" name="name">
+                                <div><label for="projectTitle">Project Title</label></div>
+                                <input type="text" class="new-todo-item form-control required" placeholder="{{trans('projects.name')}}" name="name">
                             </fieldset>
-                             <fieldset class="form-group col-4">
-                                <input type="text" class="new-todo-item form-control required"
-                                       placeholder="Project Number" name="project_number">
+                            <fieldset class="form-group col-4">
+                                <div><label for="projectNumber">Project No.</label></div>
+                                <input type="text" class="new-todo-item form-control required" placeholder="Project Number" name="project_number">
                             </fieldset>
                         </div>
                         <div class="row">
-                            <fieldset class="form-group col-md-4">
-                                <input type="text" class="new-todo-item form-control"
-                                       placeholder="LPO Amount" name="lpo_amount">
+                            <fieldset class="form-group position-relative has-icon-left  col-md-4">
+                                <div><label for="customer">Search Customer</label></div>
+                                <select id="person" name="customer_id" class="form-control required select-box" data-placeholder="{{trans('customers.customer')}}">
+                                </select>
                             </fieldset>
-
-                            <fieldset class="form-group col-md-4">
-                                <input type="text" class="new-todo-item form-control"
-                                       placeholder="LPO Number" name="lpo_number">
+                            <fieldset class="form-group position-relative has-icon-left  col-md-4">
+                                <div><label for="branch">Branch</label></div>
+                                <select id="branch_id" name="branch_id" class="form-control  select-box" data-placeholder="Branch">
+                                </select>
                             </fieldset>
-                             <fieldset class="form-group col-md-4">
-
-                                   <select class="form-control  select-box" name="sales_account" id="sales_account"
-                                        data-placeholder="Project Type/Sales Account" >
-                                        <option value="">Project Type/Sales Account</option>
-
+                            <fieldset class="form-group col-md-4">
+                                <div><label for="projectType">Project Type / Sales Account</label></div>
+                                <select class="form-control  select-box" name="sales_account" id="sales_account" data-placeholder="Project Type/Sales Account">
+                                    <option value="">Project Type/Sales Account</option>
                                     @foreach($accounts as $account)
-                                        <option value="{{$account['id']}}">{{$account['code']}} {{$account['holder']}}</option>
+                                    <option value="{{$account['id']}}">{{$account['code']}} {{$account['holder']}}</option>
                                     @endforeach
                                 </select>
-
                             </fieldset>
-                         
                         </div>
                         <div class="row">
+                            <fieldset class="form-group position-relative has-icon-left  col-md-6">
+                                <div><label for="quote">Primary / Main Quote</label></div>
+                                <select id="main_quote" name="main_quote" class="form-control required select-box" data-placeholder="Primary / Main Quote">
+                                    <option value="1">Quote 1</option>
+                                    <option value="2">Quote 2</option>
+                                    <option value="3">Quote 3</option>
+                                </select>
+                            </fieldset>
+                            <fieldset class="form-group position-relative has-icon-left  col-md-6">
+                                <div><label for="quote">Seconday / Other Quotes</label></div>
+                                <select multiple id="other_quote" name="other_quote" class="form-control required select-box" data-placeholder="Primary / Main Quote">
+                                    <option value="1">Quote 1</option>
+                                    <option value="2">Quote 2</option>
+                                    <option value="3">Quote 3</option>
+                                </select>
+                            </fieldset>
+                        </div>
+                        
+                        <div class="row">
                             <fieldset class="form-group col-md-4">
+                                <div><label for="status">Status</label></div>
                                 <select class="custom-select" id="todo-select" name="status">
-
                                     @foreach($mics->where('section','=',2) as $row)
                                         <option value="{{$row['id']}}">{{$row['name']}}</option>
                                     @endforeach
-
-
                                 </select>
                             </fieldset>
-
                             <fieldset class="form-group col-md-4">
+                                <div><label for="projectPriority">Project Priority</label></div>
                                 <select class="custom-select" id="todo-select" name="priority">
                                     <option value="Medium" selected>{{trans('projects.priority')}}</option>
                                     <option value="Low">{{trans('tasks.Low')}}</option>
@@ -67,9 +80,8 @@
                                 </select>
                             </fieldset>
                             <fieldset class="form-group col-md-4">
-                                <select class="form-control select-box" name="tags[]" id="tags"
-                                        data-placeholder="{{trans('tags.select')}}" multiple>
-
+                                <div><label for="tags">Tags</label></div>
+                                <select class="form-control select-box" name="tags[]" id="tags" data-placeholder="{{trans('tags.select')}}" multiple>
                                     @foreach($mics->where('section','=',1) as $tag)
                                         <option value="{{$tag['id']}}">{{$tag['name']}}</option>
                                     @endforeach
@@ -77,81 +89,56 @@
                             </fieldset>
                         </div>
 
-                         
-
-                        <fieldset class="form-group position-relative has-icon-left col-12">
-                            <div class="form-control-position">
-                                <i class="icon-emoticon-smile"></i>
-                            </div>
-                            <input type="text" id="new-todo-desc" class="new-todo-desc form-control required"
-                                   placeholder="{{trans('tasks.short_desc')}}" name="short_desc">
-
+                        <div><label for="Description">Description</label></div>
+                        <fieldset class="form-group position-relative has-icon-left col-12">                            
+                            <div class="form-control-position"><i class="icon-emoticon-smile"></i></div>
+                            <input type="text" id="new-todo-desc" class="new-todo-desc form-control required" placeholder="{{trans('tasks.short_desc')}}" name="short_desc">
                         </fieldset>
                         <fieldset class="form-group col-12">
-                            <textarea class="new-todo-item form-control required"
-                                      placeholder="{{trans('tasks.description')}}"
-                                      rows="6" name="note"></textarea>
+                            <textarea class="new-todo-item form-control required" placeholder="{{trans('tasks.description')}}" rows="6" name="note"></textarea>
                         </fieldset>
+
                         <div class="form-group row">
                             <div class="col-md-4 col-xs-12 mt-1">
                                 <div class="row">
-                                    <label class="col-sm-4 col-xs-6 control-label"
-                                           for="sdate">{{trans('meta.from_date')}}</label>
-
+                                    <label class="col-sm-4 col-xs-6 control-label" for="sdate">{{trans('meta.from_date')}}</label>
                                     <div class="col-sm-6 col-xs-6">
-                                        <input type="text" class="form-control from_date required"
-                                               placeholder="Start Date" name="start_date"
-                                               autocomplete="false" data-toggle="datepicker">
-
+                                        <input type="text" class="form-control from_date required" placeholder="Start Date" name="start_date" autocomplete="false" data-toggle="datepicker">
                                         <input type="time" name="time_from" class="form-control" value="00:00">
                                     </div>
                                 </div>
                             </div>
                             <div class="col-md-4 col-xs-12 mt-1">
                                 <div class="row">
-                                    <label class="col-sm-4 col-xs-6  control-label"
-                                           for="sdate">{{trans('meta.to_date')}}</label>
-
+                                    <label class="col-sm-4 col-xs-6  control-label" for="sdate">{{trans('meta.to_date')}}</label>
                                     <div class="col-sm-6 col-xs-6 ">
-                                        <input type="text" class="form-control required to_date"
-                                               placeholder="End Date" name="end_date"
-                                               data-toggle="datepicker" autocomplete="false">
-
+                                        <input type="text" class="form-control required to_date" placeholder="End Date" name="end_date" data-toggle="datepicker" autocomplete="false">
                                         <input type="time" name="time_to" class="form-control" value="23:59">
                                     </div>
                                 </div>
                             </div>
-
                             <div class="col-md-4 col-xs-12 mt-1">
                                 <div class="row">
-                                    <label class="col-sm-4 col-xs-6 control-label"
-                                           for="sdate">{{trans('tasks.link_to_calender')}}</label>
-
+                                    <label class="col-sm-4 col-xs-6 control-label" for="sdate">{{trans('tasks.link_to_calender')}}</label>
                                     <div class="col-sm-6 col-xs-6">
-                                        <input type="checkbox" class="form-control"
-                                               name="link_to_calender"
-                                        >
-
+                                        <input type="checkbox" class="form-control" name="link_to_calender">
                                         {{ Form::text('color', '#0b97f4', ['class' => 'form-control round', 'id'=>'color','placeholder' => trans('miscs.color'),'autocomplete'=>'off']) }}
                                     </div>
                                 </div>
                             </div>
-
                         </div>
-
                         <div class="row">
                             <fieldset class="form-group col-md-6">
-                                <input type="text" class="new-todo-item form-control"
-                                       placeholder="{{trans('projects.phase')}}" name="phase">
-                            </fieldset>
-
-                            <fieldset class="form-group col-md-3">
-                                <input type="text" class="new-todo-item form-control"
-                                       placeholder="{{trans('projects.worth')}}" name="worth">
+                                <div><label for="projectPlanningPhase">Project Planning Phase</label></div>
+                                <input type="text" class="new-todo-item form-control" placeholder="{{trans('projects.phase')}}" name="phase">
                             </fieldset>
                             <fieldset class="form-group col-md-3">
-                                <select class="form-control select-box" name="project_share"
-                                        data-placeholder="{{trans('projects.project_share')}}">
+                                <div><label for="projectBudget">Project Budget</label></div>
+                                <input type="text" class="new-todo-item form-control" placeholder="{{trans('projects.worth')}}" name="worth">
+                            </fieldset>
+                            <fieldset class="form-group col-md-3">
+                                <div><label for="projectShare">Project Share</label></div>
+                                <select class="form-control select-box" name="project_share" data-placeholder="{{trans('projects.project_share')}}">
                                     <option value="0">{{trans('projects.private')}}</option>
                                     <option value="1">{{trans('projects.internal')}}</option>
                                     <option value="2">{{trans('projects.external')}}</option>
@@ -162,37 +149,20 @@
                                 </select>
                             </fieldset>
                         </div>
-
                         <div class="row">
                             <fieldset class="form-group position-relative has-icon-left col-md-4">
-
-                                <select class="form-control  select-box" name="employees[]" id="employee"
-                                        data-placeholder="{{trans('tasks.assign')}}" multiple>
-
+                                <div><label for="taskAssign">Assign To</label></div>
+                                <select class="form-control  select-box" name="employees[]" id="employee" data-placeholder="{{trans('tasks.assign')}}" multiple>
                                     @foreach($employees as $employee)
-                                        <option value="{{$employee['id']}}">{{$employee['first_name']}} {{$employee['last_name']}}</option>
+                                    <option value="{{$employee['id']}}">{{$employee['first_name']}} {{$employee['last_name']}}</option>
                                     @endforeach
                                 </select>
-                            </fieldset>
-                            <fieldset class="form-group position-relative has-icon-left  col-md-4">
-
-                                <select id="person" name="customer_id" class="form-control required select-box"  data-placeholder="{{trans('customers.customer')}}" >
-                                </select>
-                            </fieldset>
-
-                               <fieldset class="form-group position-relative has-icon-left  col-md-4">
-
-                                <select id="branch_id" name="branch_id" class="form-control  select-box"  data-placeholder="Branch" >
-                                </select>
-                            </fieldset>
-
-
+                            </fieldset>                            
                         </div>
                     </div>
                     <div class="modal-footer">
                         <fieldset class="form-group position-relative has-icon-left mb-0">
-                            <button type="button" id="submit-data_project" class="btn btn-info add-todo-item"
-                                    data-dismiss="modal"><i class="fa fa-paper-plane-o d-block d-lg-none"></i>
+                            <button type="button" id="submit-data_project" class="btn btn-info add-todo-item" data-dismiss="modal"><i class="fa fa-paper-plane-o d-block d-lg-none"></i>
                                 <span class="d-none d-lg-block">{{trans('projects.new_project')}}</span></button>
                         </fieldset>
                     </div>
