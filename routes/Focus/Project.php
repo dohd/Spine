ae5c23720a728d2a757346f4eda8f65133076441
Dohd <alloicebrian@gmail.com>
2021-11-19 16:13:20 +0300

    Add rjc routes

--- routes/Focus/Project.php
+++ routes/Focus/Project.php
@@ -1,30 +1,30 @@
 <?php
+
 /**
  * project
  *
  */
 
-    Route::group(['namespace' => 'project', 'middleware' => 'project'], function () {
-                Route::post('projects/load', 'ProjectsController@load')->name('projects.load');
+Route::group(['namespace' => 'project', 'middleware' => 'project'], function () {
+  Route::post('projects/load', 'ProjectsController@load')->name('projects.load');
 
 
-        Route::post('projects/update_status', 'ProjectsController@update_status')->name('projects.update_status');
-        Route::post('projects/store_meta', 'ProjectsController@store_meta')->name('projects.store_meta');
-        Route::post('projects/delete_meta', 'ProjectsController@delete_meta')->name('projects.delete_meta');
-        Route::post('projects/log_history', 'ProjectsController@log_history')->name('projects.log_history');
-        Route::post('projects/notes', 'ProjectsController@notes')->name('projects.notes');
-        Route::post('projects/invoices', 'ProjectsController@invoices')->name('projects.invoices');
-      
-    });
+  Route::post('projects/update_status', 'ProjectsController@update_status')->name('projects.update_status');
+  Route::post('projects/store_meta', 'ProjectsController@store_meta')->name('projects.store_meta');
+  Route::post('projects/delete_meta', 'ProjectsController@delete_meta')->name('projects.delete_meta');
+  Route::post('projects/log_history', 'ProjectsController@log_history')->name('projects.log_history');
+  Route::post('projects/notes', 'ProjectsController@notes')->name('projects.notes');
+  Route::post('projects/invoices', 'ProjectsController@invoices')->name('projects.invoices');
+});
 
-    Route::group(['namespace' => 'project'], function () {
+Route::group(['namespace' => 'project'], function () {
+  Route::get('projects/create_rjc', 'ProjectsController@create_rjc')->name('projects.create_rjc');
+  Route::post('projects/store_rjc', 'ProjectsController@store_rjc')->name('projects.store_rjc');
 
-        Route::resource('projects', 'ProjectsController');
-         Route::post('projects/search', 'ProjectsController@search')->name('projects.search');
-        //For Datatable
-        Route::post('projects/get', 'ProjectsTableController')->name('projects.get');
-        Route::post('projects/project_load_select', 'ProjectsController@project_load_select')->name('projects.project_load_select');
-          Route::post('projects/search/{bill_type}', 'ProjectsController@project_search')->name('projects.project_search');
-
-    });
-
+  Route::resource('projects', 'ProjectsController');
+  Route::post('projects/search', 'ProjectsController@search')->name('projects.search');
+  //For Datatable
+  Route::post('projects/get', 'ProjectsTableController')->name('projects.get');
+  Route::post('projects/project_load_select', 'ProjectsController@project_load_select')->name('projects.project_load_select');
+  Route::post('projects/search/{bill_type}', 'ProjectsController@project_search')->name('projects.project_search');
+});
