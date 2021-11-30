48d73e499a30f75cebab58a21aeb117aec9ab757
Dohd <alloicebrian@gmail.com>
2021-11-30 16:02:35 +0300

    Add fillable to ProjectQuote model

--- app/Models/project/ProjectQuote.php
+++ app/Models/project/ProjectQuote.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class ProjectQuote extends Model
 {
     protected $table = 'project_quotes';
+
+    protected $fillable = [
+        'project_id', 'quote_id'
+    ];
 }
