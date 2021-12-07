6a6a2ee321eb7951a27c5a2c3d9907fb0d746b6b
Dohd <alloicebrian@gmail.com>
2021-12-07 14:50:11 +0300

    Create ProjectsQuotesTable controller

---
+++ app/Http/Controllers/Focus/quote/ProjectQuotesTableController.php
@@ -0,0 +1,93 @@
+<?php
+/*
+ * Rose Business Suite - Accounting, CRM and POS Software
+ * Copyright (c) UltimateKode.com. All Rights Reserved
+ * ***********************************************************************
+ *
+ *  Email: support@ultimatekode.com
+ *  Website: https://www.ultimatekode.com
+ *
+ *  ************************************************************************
+ *  * This software is furnished under a license and may be used and copied
+ *  * only  in  accordance  with  the  terms  of such  license and with the
+ *  * inclusion of the above copyright notice.
+ *  * If you Purchased from Codecanyon, Please read the full License from
+ *  * here- http://codecanyon.net/licenses/standard/
+ * ***********************************************************************
+ */
+namespace App\Http\Controllers\Focus\quote;
+
+use App\Http\Controllers\Controller;
+use Yajra\DataTables\Facades\DataTables;
+use App\Repositories\Focus\quote\QuoteRepository;
+use App\Http\Requests\Focus\quote\ManageQuoteRequest;
+
+/**
+ * Class QuotesTableController.
+ */
+class ProjectQuotesTableController extends Controller
+{
+    /**
+     * variable to store the repository object
+     * @var QuoteRepository
+     */
+    protected $quote;
+
+    /**
+     * contructor to initialize repository object
+     * @param QuoteRepository $quote ;
+     */
+    public function __construct(QuoteRepository $quote)
+    {
+        $this->quote = $quote;
+    }
+
+    /**
+     * This method return the data of the model
+     * @param ManageQuoteRequest $request
+     *
+     * @return mixed
+     */
+    public function __invoke(ManageQuoteRequest $request)
+    {
+        $core = $this->quote->getForDataTable();
+        return Datatables::of($core)
+            ->addIndexColumn()
+            ->addColumn('notes', function($quote) {
+                return $quote->notes;
+            })
+            ->addColumn('project_ref', function($quote) {
+                return 'P-0001';
+            })
+            ->addColumn('tid', function ($quote) {
+                return '<a class="font-weight-bold" href="' . route('biller.quotes.show', [$quote->id]) . '">' . $quote->tid . '</a>';
+            })
+            ->addColumn('customer', function ($quote) {
+                if (isset($quote->customer) && isset($quote->lead->branch)) {
+                    return $quote->customer->name.' - '.$quote->lead->branch->name.' '
+                        .'<a class="font-weight-bold" href="' . route('biller.customers.show', [$quote->customer->id]) . '"><i class="ft-eye"></i></a>';
+                }
+                return;
+            })
+            ->addColumn('created_at', function ($quote) {
+                return dateFormat($quote->invoicedate);
+            })
+            ->addColumn('total', function ($quote) {
+                return number_format($quote->total, 2);
+            })
+            ->addColumn('verified', function ($quote) {
+                return $quote->verified;
+            })
+            ->addColumn('lpo_no', function($quote) {
+                return 'lpo-0001';
+            })
+            ->addColumn('actions', function ($quote) {
+                $valid_token = token_validator('', 'q'.$quote->id .$quote->tid, true);
+
+                return '<a href="'.route('biller.print_quote', [$quote->id, 4, $valid_token, 1, 'verified=Yes']).'" class="btn btn-purple round" target="_blank" data-toggle="tooltip" data-placement="top" title="Print"><i class="fa fa-print"></i></a> '
+                    .'<a href="'. route('biller.quotes.verify', $quote) .'" class="btn btn-primary round" data-toggle="tooltip" data-placement="top" title="Verify"><i class="fa fa-check"></i></a>';
+            })
+            ->rawColumns(['notes', 'tid', 'customer', 'actions', 'status', 'total'])
+            ->make(true);
+    }
+}
