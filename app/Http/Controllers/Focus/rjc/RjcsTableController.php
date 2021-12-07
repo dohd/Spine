d35574e659ed7c3d1575493dc87125cc23be55eb
Dohd <alloicebrian@gmail.com>
2021-12-07 14:51:56 +0300

    Create  RjcsTableController

---
+++ app/Http/Controllers/Focus/rjc/RjcsTableController.php
@@ -0,0 +1,72 @@
+<?php
+/*
+ * Rose Business Suite - Accounting, CRM and POS Software
+ * Copyright (c) UltimateKode.com. All Rights Reserved
+ * ***********************************************************************
+ *
+ *  Email: support@ultimatekode.com
+ *  Website: https://www.ultimatekode.com
+ *
+ *  ************************************************************************
+ *  * This software is furnished under a license and may be used and copied
+ *  * only  in  accordance  with  the  terms  of such  license and with the
+ *  * inclusion of the above copyright notice.
+ *  * If you Purchased from Codecanyon, Please read the full License from
+ *  * here- http://codecanyon.net/licenses/standard/
+ * ***********************************************************************
+ */
+
+namespace App\Http\Controllers\Focus\rjc;
+
+use App\Http\Controllers\Controller;
+use Yajra\DataTables\Facades\DataTables;
+use App\Repositories\Focus\rjc\RjcRepository;
+use App\Http\Requests\Focus\rjc\ManageRjcRequest;
+
+/**
+ * Class AccountsTableController.
+ */
+class RjcsTableController extends Controller
+{
+    /**
+     * variable to store the repository object
+     * @var AccountRepository
+     */
+    protected $rjc;
+
+    /**
+     * contructor to initialize repository object
+     * @param AccountRepository $account ;
+     */
+    public function __construct(RjcRepository $rjc)
+    {
+        $this->rjc = $rjc;
+    }
+
+    /**
+     * This method returns the datatable view
+     */
+    public function __invoke(ManageRjcRequest $request)
+    {
+        $core = $this->rjc->getForDataTable();
+        return Datatables::of($core)
+            ->escapeColumns(['id'])
+            ->addIndexColumn()
+            ->addColumn('customer', function ($rjc) {
+                if (isset($rjc->client) && isset($rjc->branch)) {
+                    return $rjc->client->company . ' - ' . $rjc->branch->name . ' <a class="font-weight-bold" href="' . route('biller.customers.show', [$rjc->client->id]) . '"><i class="ft-eye"></i></a>';
+                }
+                return;
+            })
+            ->addColumn('created_at', function ($rjc) {
+                return dateFormat($rjc->created_at);
+            })
+            ->addColumn('actions', function ($rjc) {
+                $valid_token = token_validator('', 'd' . $rjc->id, true);
+                
+                return '<a href="' . route('biller.print_djc', [$rjc->id, 10, $valid_token, 1]) . '" target="_blank"  class="btn btn-purple round" data-toggle="tooltip" data-placement="top" title="Print"><i class="fa fa-print"></i></a> '
+                    . $rjc->action_buttons;
+            })
+            ->make(true);
+    }
+}
