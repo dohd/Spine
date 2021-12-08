cd1ceb8e4844910cefa236196a5adfe227e117d5
Dohd <alloicebrian@gmail.com>
2021-12-08 15:53:39 +0300

    Update rjcs view

--- resources/views/focus/rjcs/view.blade.php
+++ resources/views/focus/rjcs/view.blade.php
@@ -1,35 +1,33 @@
 @extends ('core.layouts.app')
 
-@section ('title', 'Djc Report Management')
+@section ('title', 'Rjc Report Management')
 
 @section('page-header')
-<h1>Djc Report</h1>
+<h1>Rjc Report</h1>
 @endsection
 
 @section('content')
 <div class="p-2">
     <div class="content-header row">
         <div class="content-header-left col-md-6 col-12 mb-2">
-            <h4 class=" mb-0">Djc Report Management</h4>
-
+            <h4 class=" mb-0">Rjc Report Management</h4>
         </div>
         <div class="content-header-right col-md-6 col-12">
             <div class="media width-250 float-right">
-
                 <div class="media-body media-right text-right">
-                    @include('focus.djcs.partials.djcs-header-buttons')
+                    @include('focus.rjcs.partials.rjcs-header-buttons')
                 </div>
             </div>
         </div>
     </div>
     
     <div class="card">
-        <h5 class="card-header">Diagnosis Job-card</h5>
+        <h5 class="card-header">Repair Job Card</h5>
         <div class="card-body">            
             <ul class="nav nav-tabs nav-top-border no-hover-bg nav-justified" role="tablist">
                 <li class="nav-item">
                     <a class="nav-link active" id="active-tab1" data-toggle="tab" href="#active1" aria-controls="active1" role="tab" aria-selected="true">
-                        Customer & Reference Details
+                        Project & Reference Details
                     </a>
                 </li>
                 <li class="nav-item">
@@ -44,42 +42,42 @@
                 </li>
             </ul>
 
-            <div class="tab-content px-1 pt-1" id='djc-report'>
+            <div class="tab-content px-1 pt-1" id='rjc-report'>
                 <div class="tab-pane active in" id="active1" aria-labelledby="customer-details" role="tabpanel">
                     <table id="customer-table" class="table table-lg table-bordered zero-configuration" cellspacing="0" width="100%">
                         <tbody>
                             <tr>
-                                <th>Client</th>
-                                <td>{{ @$djc->customer->name ?: @$djc->lead->client_name }}</td>
+                                <th>Project</th>
+                                <td>{{ $rjc->project->name }}</td>
                             </tr>
                             <tr>
-                                <th>Branch</th>
-                                <td>{{ @$djc->branch->name }}</td>
+                                <th>Project No.</th>
+                                <td>{{ $rjc->project->project_number }}</td>
                             </tr>
                             <tr>
                                 <th>Region</th>
-                                <td>{{ $djc->region }}</td>
+                                <td>{{ $rjc->region }}</td>
                             </tr>
                             <tr>
                                 <th>Attention</th>
-                                <td>{{ $djc->attention }}</td>
+                                <td>{{ $rjc->attention }}</td>
                             </tr>
                             <tr><th></th></tr>
                             <tr>
                                 <th>Report No</th>
-                                <td>{{ $djc->tid }}</td>
+                                <td>{{ $rjc->tid }}</td>
                             </tr>
                             <tr>
                                 <th>Date</th>
-                                <td>{{ dateFormat($djc->report_date) }}</td>
+                                <td>{{ dateFormat($rjc->report_date) }}</td>
                             </tr>
                             <tr>
                                 <th>Client Ref No.</th>
-                                <td>{{ $djc->reference }}</td>
+                                <td>{{ $rjc->reference }}</td>
                             </tr>
                             <tr>
                                 <th>Prepared By</th>
-                                <td>{{ $djc->prepared_by }}</td>
+                                <td>{{ $rjc->prepared_by }}</td>
                             </tr>
                         </tbody>
                     </table>
@@ -89,11 +87,11 @@
                     <table id="technician-table" class="table table-lg table-bordered zero-configuration" cellspacing="0" width="100%">
                         <tr>
                             <th>Reference</th>
-                            <td>{{ $djc->subject }}</td>
+                            <td>{{ $rjc->subject }}</td>
                         </tr>
                         <tr>
                             <th>Technician</th>
-                            <td>{{ $djc->technician }}</td>
+                            <td>{{ $rjc->technician }}</td>
                         </tr>
                     </table>
                     <br/>
@@ -107,7 +105,7 @@
                             <th>Last Service Date</th>
                             <th>Next Service Date</th>
                         </tr>
-                        @foreach ($djc_items as $row)
+                        @foreach ($rjc_items as $row)
                             <tr>
                                 <td>{{ $row->tag_number }}</td>
                                 <td>{{ $row->joc_card }}</td>
@@ -124,20 +122,16 @@
                 <div class="tab-pane" id="active4" aria-labelledby="other-details" role="tabpanel">
                     <table id="others-table" class="table table-lg table-bordered zero-configuration" cellspacing="0" width="100%">
                         <tr>
-                            <th>Call Description</th>
-                            <td>{{ @$djc->lead->note }}</td>
-                        </tr>
-                        <tr>
                             <th>Findings and Root Cause</th>
-                            <td>{{ $djc->root_cause }}</td>
+                            <td>{{ $rjc->root_cause }}</td>
                         </tr>
                         <tr>
                             <th>Action Taken</th>
-                            <td>{{ $djc->action_taken }}</td>
+                            <td>{{ $rjc->action_taken }}</td>
                         </tr>
                         <tr>
                             <th>Recommendation</th>
-                            <td>{{ $djc->recommendations }}</td>
+                            <td>{{ $rjc->recommendations }}</td>
                         </tr>
                     </table>
                 </div>
@@ -147,18 +141,13 @@
 </div>
 @endsection
 
-
 @section('extra-scripts')
 <script type="text/javascript">
-    // create a temporary div element
-    const div = document.createElement('div');
-    // loop through td elements
-    $('#others-table td').each(function(i, td) {
-        if (i === 0) return;
-        // append td value to div
-        div.innerHTML = td.innerText;
-        // replace camel case in div text with a space prefix 
-        td.innerText = div.innerText.replace(/[A-Z]/g, function(el) { return ` ${el}`; });
+    const temp_div = document.createElement('div');
+    $('#others-table td').each(function() {
+        $(temp_div).html($(this).text());
+        const td_text = $(temp_div).text().replace(/[A-Z]/g, function(el) { return ' ' + el; });
+        $(this).text(td_text);
     });
 </script>
 @endsection
