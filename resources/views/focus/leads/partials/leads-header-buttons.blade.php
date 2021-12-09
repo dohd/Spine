9673717a0368c2849d57fea4a97226f293552c52
Dohd <alloicebrian@gmail.com>
2021-12-09 09:57:30 +0300

    Add Djc, Quote, PI buttons

--- resources/views/focus/leads/partials/leads-header-buttons.blade.php
+++ resources/views/focus/leads/partials/leads-header-buttons.blade.php
@@ -1,12 +1,23 @@
 <!--Action Button-->
-<div class="">
+<div>
     <div class="btn-group" role="group" aria-label="Basic example">
-        <a href="{{ route( 'biller.leads.index' ) }}" class="btn btn-info  btn-lighten-2"><i
-                    class="fa fa-list-alt"></i> {{trans( 'general.list' )}}</a>
-        @permission( 'customer-create' ) <a href="{{ route( 'biller.leads.create' ) }}"
-                                                 class="btn btn-pink  btn-lighten-3"><i
-                    class="fa fa-plus-circle"></i> {{trans( 'general.create' )}}</a> @endauth
+        <a href="{{ route('biller.leads.index') }}" class="btn btn-info  btn-lighten-2">
+            <i class="fa fa-list-alt"></i> {{trans( 'general.list' )}}
+        </a>
+        @permission('customer-create' ) 
+            <a href="{{ route('biller.leads.create') }}" class="btn btn-pink  btn-lighten-3">
+                <i class="fa fa-plus-circle"></i> Lead
+            </a>
+            &nbsp;&nbsp;
+            <a href="{{ route('biller.djcs.create') }}" class="btn btn-success  btn-lighten-3">
+                <i class="fa fa-plus-circle"></i> Djc
+            </a> &nbsp;
+            <a href="{{ route('biller.quotes.create') }}" class="btn btn-warning">
+                <i class="fa fa-plus-circle"></i> Quote
+            </a> &nbsp;
+            <a href="{{ route('biller.quotes.create', 'page=pi') }}" class="btn btn-cyan">
+                <i class="fa fa-plus-circle"></i> PI
+            </a> 
+        @endauth
     </div>
 </div>
-<div class="clearfix"></div>
-
