2b73a77e908493ca46014a2cc204132840e8bb70
Dohd <alloicebrian@gmail.com>
2021-12-17 13:06:27 +0300

    Add list buttons in both quote and pi

--- resources/views/focus/quotes/partials/quotes-header-buttons.blade.php
+++ resources/views/focus/quotes/partials/quotes-header-buttons.blade.php
@@ -7,18 +7,24 @@
             $curr_uri = $_SERVER['REQUEST_URI'];
             if (strpos($curr_uri, 'page=pi')) $link = route('biller.quotes.index', 'page=pi');
         @endphp
-        <a href="{{ $link }}" class="btn btn-info  btn-lighten-2 round">
+        <a href="{{ $link }}" class="btn btn-info  btn-lighten-2">
             <i class="fa fa-list-alt"></i> {{trans('general.list')}}
         </a>
 
         @permission('quote-create')
             @if (request()->getQueryString() == 'page=pi')
-                <a href="{{ route('biller.quotes.create', 'page=pi') }}" class="btn btn-pink  btn-lighten-3 round">
+                <a href="{{ route('biller.quotes.create', 'page=pi') }}" class="btn btn-pink  btn-lighten-3">
                     <i class="fa fa-plus-circle"></i> PI
+                </a>&nbsp;&nbsp;
+                <a href="{{ route('biller.quotes.index') }}" class="btn btn-success">
+                    <i class="fa fa-list-alt"></i> Quote
                 </a>
             @else
-                <a href="{{ route('biller.quotes.create') }}" class="btn btn-pink  btn-lighten-3 round">
+                <a href="{{ route('biller.quotes.create') }}" class="btn btn-pink  btn-lighten-3">
                     <i class="fa fa-plus-circle"></i> Quote
+                </a>&nbsp;&nbsp;
+                <a href="{{ route('biller.quotes.index', 'page=pi') }}" class="btn btn-success">
+                    <i class="fa fa-list-alt"></i> PI
                 </a>
             @endif
         @endauth
