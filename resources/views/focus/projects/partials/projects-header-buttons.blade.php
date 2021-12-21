35fc7f0e720c839f6700de95cf38e50e36765739
Dohd <alloicebrian@gmail.com>
2021-12-21 10:58:41 +0300

    Update projects-header-buttons

--- resources/views/focus/projects/partials/projects-header-buttons.blade.php
+++ resources/views/focus/projects/partials/projects-header-buttons.blade.php
@@ -1,7 +1,12 @@
-<div class="">
-    <div class="btn-group" role="group" aria-label="Basic example">
-        <a href="{{ route( 'biller.projects.index' ) }}" class="btn btn-info  btn-lighten-2">
-            <i class="fa fa-list-alt"></i> {{trans( 'general.list' )}}
-        </a>
+<div>
+    <div class="btn-group">
+        @permission( 'project-create' )
+        <a href="#" class="btn btn-info w-2" id="addt" data-toggle="modal" data-target="#AddProjectModal">
+            <i class="fa fa-plus-circle"></i> Create
+        </a>&nbsp;&nbsp;
+        @endauth
+        <a href="{{ route('biller.quotes.project_quotes') }}" class="btn btn-success">
+            <i class="fa fa-list-alt"></i> Verification
+        </a>  
     </div>
 </div>
