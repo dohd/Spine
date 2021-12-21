3381aaafeb7f772cc1316e0d45f44b9f38faa756
Dohd <alloicebrian@gmail.com>
2021-12-21 10:09:03 +0300

    Comment out previous code

--- app/Http/Middleware/ProjectMiddleware.php
+++ app/Http/Middleware/ProjectMiddleware.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Middleware;
 
-use App\Models\project\Project;
+// use App\Models\project\Project;
 use Closure;
 
 
@@ -15,19 +15,20 @@ class ProjectMiddleware
      _ @param  \Closure  $next
      _ @return mixed
      */
-    public function handle($request, Closure $next, $guard = "crm")
+    public function handle($request, Closure $next)
     {
-        $project = Project::find($request->project_id);
-        $user_d = auth()->user();
-        if ($project->creator->id == $user_d->id) {
-            return $next($request);
-        }
+        return $next($request);
 
-        if (@$project->users->find($user_d->id)->id and ($project->project_share == 3 or $project->project_share == 5)) {
-            return $next($request);
-        }
+        // $project = Project::find($request->project_id);
+        // $user_d = auth()->user();
+        // if ($project->creator->id == $user_d->id) {
+        //     return $next($request);
+        // }
 
-        return redirect(route('biller.projects.index'));
+        // if (@$project->users->find($user_d->id)->id and ($project->project_share == 3 or $project->project_share == 5)) {
+        //     return $next($request);
+        // }
 
+        // return redirect(route('biller.projects.index'));
     }
 }
