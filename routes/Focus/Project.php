99ac0e25e581201a9a07ba3b723c4cafb958abc2
Dohd <alloicebrian@gmail.com>
2021-12-21 10:08:10 +0300

    Remove empty lines

--- routes/Focus/Project.php
+++ routes/Focus/Project.php
@@ -7,8 +7,6 @@
 
 Route::group(['namespace' => 'project', 'middleware' => 'project'], function () {
   Route::post('projects/load', 'ProjectsController@load')->name('projects.load');
-
-
   Route::post('projects/update_status', 'ProjectsController@update_status')->name('projects.update_status');
   Route::post('projects/store_meta', 'ProjectsController@store_meta')->name('projects.store_meta');
   Route::post('projects/delete_meta', 'ProjectsController@delete_meta')->name('projects.delete_meta');
