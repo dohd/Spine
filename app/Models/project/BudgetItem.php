7e1c2be2a24e9d53a4c9a8d007983b10b918f457
Dohd <alloicebrian@gmail.com>
2022-01-06 10:27:34 +0300

    Create BudgetItem model

---
+++ app/Models/project/BudgetItem.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models\project;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BudgetItem extends Model
+{
+    /**
+     * Guarded fields of model
+     * @var array
+     */
+    protected $guarded = [
+        'id'
+    ];
+}
