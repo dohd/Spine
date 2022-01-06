7e32c0d2b6ffb95d83874c5be9988772c001d553
Dohd <alloicebrian@gmail.com>
2022-01-06 10:27:47 +0300

    Create BudgetSkillset model

---
+++ app/Models/project/BudgetSkillset.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models\project;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BudgetSkillset extends Model
+{
+    /**
+     * Guarded fields of model
+     * @var array
+     */
+    protected $guarded = [
+        'id'
+    ];
+}
