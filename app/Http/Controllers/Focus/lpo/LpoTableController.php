e2a2d4eeb4d38b2262a737d71cb424631f01552d
Dohd <alloicebrian@gmail.com>
2022-01-07 10:24:43 +0300

    Create LpoTableController

---
+++ app/Http/Controllers/Focus/lpo/LpoTableController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Focus\lpo;
+
+use App\Http\Controllers\Controller;
+
+class LpoTableController extends Controller
+{
+
+    /**
+     * This method return the data of the model
+     * @return mixed
+     */
+    public function __invoke()
+    {
+
+    }
+}
