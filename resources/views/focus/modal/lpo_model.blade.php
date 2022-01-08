0e8ac513c1eb39f9896b4c1c8f4ec18f91daa1ab
Dohd <alloicebrian@gmail.com>
2022-01-08 16:11:03 +0300

    Add select lpo dropdown

--- resources/views/focus/modal/lpo_model.blade.php
+++ resources/views/focus/modal/lpo_model.blade.php
@@ -8,18 +8,30 @@
             <div class="modal-body">
                 <form id="form_model_4">
                     <div class="row">
+                        <div class="col mb-1 form-group">
+                            <label for="customer">Select LPO</label>
+                            <select id="lpo_id" name="lpo_id" class="form-control" required>
+                                <option value="0">-- Select LPO --</option>
+                                @foreach ($lpos as $lpo)
+                                    <option value="{{ $lpo->id }}">{{ $lpo->lpo_no }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="row">
                         <div class="col mb-1"><label for="pmethod">LPO Date</label>
-                            <input type="text" class="form-control mb-1" placeholder="LPO  Date" name="lpo_date" data-toggle="datepicker">
+                            <input type="date" class="form-control mb-1" placeholder="LPO  Date" name="lpo_date" data-toggle="datepicker" readonly>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col mb-1"><label for="note">LPO Amount</label>
-                            <input type="text" class="form-control" name="lpo_amount" placeholder="LPO Amount" value="{{ @$words['pay_note']}}">
+                            <input type="text" class="form-control" name="lpo_amount" placeholder="LPO Amount" readonly>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col mb-1"><label for="note">LPO Number</label>
-                            <input type="text" class="form-control" name="lpo_number" placeholder="LPO Number" value="{{ @$words['pay_note']}}">
+                            <input type="text" class="form-control" name="lpo_number" placeholder="LPO Number" readonly>
                         </div>
                     </div>
                     <div class="modal-footer">
