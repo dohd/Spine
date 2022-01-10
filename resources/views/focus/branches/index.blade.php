406a415c2cb97f744dcb0cf5440ba5aa2d85c472
Dohd <alloicebrian@gmail.com>
2022-01-10 11:05:17 +0300

    Update orderBy to descending

--- resources/views/focus/branches/index.blade.php
+++ resources/views/focus/branches/index.blade.php
@@ -2,39 +2,30 @@
 
 @section ('title', 'Branch Management')
 
-@section('page-header')
-    <h1>Branch Management</h1>
-@endsection
-
 @section('content')
-    <div class="">
-        <div class="content-wrapper">
-            <div class="content-header row">
-                <div class="content-header-left col-md-6 col-12 mb-2">
-                    <h4 class=" mb-0">Branch Management</h4>
+<div class="">
+    <div class="content-wrapper">
+        <div class="content-header row">
+            <div class="content-header-left col-md-6 col-12 mb-2">
+                <h4 class="content-header-title">Branch Management</h4>
+            </div>
+            <div class="content-header-right col-md-6 col-12">
+                <div class="media width-250 float-right">
 
-                </div>
-                <div class="content-header-right col-md-6 col-12">
-                    <div class="media width-250 float-right">
-
-                        <div class="media-body media-right text-right">
-                            @include('focus.branches.partials.branches-header-buttons')
-                        </div>
+                    <div class="media-body media-right text-right">
+                        @include('focus.branches.partials.branches-header-buttons')
                     </div>
                 </div>
             </div>
-            <div class="content-body">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="card">
-
-                            <div class="card-content">
-
-                                <div class="card-body">
-                                    <table id="branches-table"
-                                           class="table table-striped table-bordered zero-configuration" cellspacing="0"
-                                           width="100%">
-                                        <thead>
+        </div>
+        <div class="content-body">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-content">
+                            <div class="card-body">
+                                <table id="branches-table" class="table table-striped table-bordered zero-configuration" cellspacing="0" width="100%">
+                                    <thead>
                                         <tr>
                                             <th>#</th>
                                             <th>Company</th>
@@ -45,19 +36,13 @@
                                             <th>{{ trans('general.createdat') }}</th>
                                             <th>{{ trans('labels.general.actions') }}</th>
                                         </tr>
-                                        </thead>
-
-
-                                        <tbody>
+                                    </thead>
+                                    <tbody>
                                         <tr>
-                                            <td colspan="100%" class="text-center text-success font-large-1"><i
-                                                        class="fa fa-spinner spinner"></i></td>
+                                            <td colspan="100%" class="text-center text-success font-large-1"><i class="fa fa-spinner spinner"></i></td>
                                         </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
-
-
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
                     </div>
@@ -65,62 +50,99 @@
             </div>
         </div>
     </div>
+</div>
 @endsection
 
 @section('after-scripts')
-    {{-- For DataTables --}}
-    {{ Html::script(mix('js/dataTable.js')) }}
+{{-- For DataTables --}}
+{{ Html::script(mix('js/dataTable.js')) }}
 
-    <script>
-        $(function () {
-            setTimeout(function () {
-                draw_data()
-            }, {{config('master.delay')}});
+<script>
+    setTimeout(() => draw_data(), "{{ config('master.delay') }}");
+
+    $.ajaxSetup({
+        headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') }
+    });
+
+    function draw_data() {        
+        const tableLan = { @lang('datatable.strings') }
+        var dataTable = $('#branches-table').dataTable({
+            processing: true,
+            serverSide: true,
+            responsive: true,
+            language: tableLan,
+            ajax: {
+                url: '{{ route("biller.branches.get") }}',
+                type: 'post',
+                data: { c_type: 0 }
+            },
+            columns: [{
+                    data: 'DT_Row_Index',
+                    name: 'id'
+                },
+                {
+                    data: 'customer',
+                    name: 'customer'
+                },
+                {
+                    data: 'name',
+                    name: 'name'
+                },
+                {
+                    data: 'location',
+                    name: 'location'
+                },
+                {
+                    data: 'contact_name',
+                    name: 'contact_name'
+                },
+                {
+                    data: 'contact_phone',
+                    name: 'contact_phone'
+                },
+                {
+                    data: 'created_at',
+                    name: "{{ config('module.branches.table')}}.created_at"
+                },
+                {
+                    data: 'actions',
+                    name: 'actions',
+                    searchable: false,
+                    sortable: false
+                }
+            ],
+            order: [
+                [0, "desc"]
+            ],
+            searchDelay: 500,
+            dom: 'Blfrtip',
+            buttons: {
+                buttons: [
+
+                    {
+                        extend: 'csv',
+                        footer: true,
+                        exportOptions: {
+                            columns: [0, 1]
+                        }
+                    },
+                    {
+                        extend: 'excel',
+                        footer: true,
+                        exportOptions: {
+                            columns: [0, 1]
+                        }
+                    },
+                    {
+                        extend: 'print',
+                        footer: true,
+                        exportOptions: {
+                            columns: [0, 1]
+                        }
+                    }
+                ]
+            }
         });
-
-        function draw_data() {
-            $.ajaxSetup({
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                }
-            });
-
-            var dataTable = $('#branches-table').dataTable({
-                processing: true,
-                serverSide: true,
-                responsive: true,
-                language: {
-                    @lang('datatable.strings')
-                },
-                ajax: {
-                    url: '{{ route("biller.branches.get") }}',
-                    type: 'post',
-                    data: {c_type: 0}
-                },
-                columns: [
-                    {data: 'DT_Row_Index', name: 'id'},
-                    {data: 'customer', name: 'customer'},
-                    {data: 'name', name: 'name'},
-                    {data: 'location', name: 'location'},
-                    {data: 'contact_name', name: 'contact_name'},
-                    {data: 'contact_phone', name: 'contact_phone'},
-                    {data: 'created_at', name: '{{config('module.branches.table')}}.created_at'},
-                    {data: 'actions', name: 'actions', searchable: false, sortable: false}
-                ],
-                order: [[0, "asc"]],
-                searchDelay: 500,
-                dom: 'Blfrtip',
-                buttons: {
-                    buttons: [
-
-                        {extend: 'csv', footer: true, exportOptions: {columns: [0, 1]}},
-                        {extend: 'excel', footer: true, exportOptions: {columns: [0, 1]}},
-                        {extend: 'print', footer: true, exportOptions: {columns: [0, 1]}}
-                    ]
-                }
-            });
-            $('#branches-table_wrapper').removeClass('form-inline');
-
-        }
-    </script>
+    }
+</script>
 @endsection
