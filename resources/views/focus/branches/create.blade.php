7d2b90019ce954d5bab5a9db9e82602ec60e3fa8
Dohd <alloicebrian@gmail.com>
2022-01-11 12:20:38 +0300

    Format create branch page

--- resources/views/focus/branches/create.blade.php
+++ resources/views/focus/branches/create.blade.php
@@ -1,56 +1,40 @@
 @extends ('core.layouts.app')
 
-@section ('title', 'Branch Management |  Branch Create')
-
-@section('page-header')
-    <h1>
-      Branch Management
-        <small>Create Branch</small>
-    </h1>
-@endsection
+@section ('title', 'Branch Management | Branch Create')
 
 @section('content')
-    <div class="">
-        <div class="content-wrapper">
-            <div class="content-header row">
-                <div class="content-header-left col-md-6 col-12 mb-2">
-                    <h4 class="mb-0">Create Branch</h4>
-
-                </div>
-                <div class="content-header-right col-md-6 col-12">
-                    <div class="media width-250 float-right">
-
-                        <div class="media-body media-right text-right">
-                            @include('focus.branches.partials.branches-header-buttons')
-                        </div>
+<div class="">
+    <div class="content-wrapper">
+        <div class="content-header row">
+            <div class="content-header-left col-md-6 col-12 mb-2">
+                <h4>Create Branch</h4>
+            </div>
+            <div class="content-header-right col-md-6 col-12">
+                <div class="media width-250 float-right">
+                    <div class="media-body media-right text-right">
+                        @include('focus.branches.partials.branches-header-buttons')
                     </div>
                 </div>
             </div>
-            <div class="content-body">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="card">
-
-                            <div class="card-content">
-
-                                <div class="card-body">
-                                    {{ Form::open(['route' => 'biller.branches.store', 'class' => 'form-horizontal', 'role' => 'form', 'method' => 'post', 'id' => 'create-productcategory']) }}
-
-
-                                    <div class="form-group">
-                                        {{-- Including Form blade file --}}
-                                        @include("focus.branches.form")
-                                        <div class="edit-form-btn">
-                                            {{ link_to_route('biller.branches.index', trans('buttons.general.cancel'), [], ['class' => 'btn btn-danger btn-md']) }}
-                                            {{ Form::submit(trans('buttons.general.crud.create'), ['class' => 'btn btn-primary btn-md']) }}
-                                            <div class="clearfix"></div>
-                                        </div><!--edit-form-btn-->
-                                    </div><!-- form-group -->
-
-                                    {{ Form::close() }}
-                                </div>
-
-
+        </div>
+        <div class="content-body">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-content">
+                            <div class="card-body">
+                                {{ Form::open(['route' => 'biller.branches.store', 'class' => 'form-horizontal', 'role' => 'form', 'method' => 'post', 'id' => 'create-productcategory']) }}
+                                <div class="form-group">
+                                    {{-- Including Form blade file --}}
+                                    @include("focus.branches.form")
+                                    <div class="edit-form-btn">
+                                        {{ link_to_route('biller.branches.index', trans('buttons.general.cancel'), [], ['class' => 'btn btn-danger btn-md']) }}
+                                        {{ Form::submit(trans('buttons.general.crud.create'), ['class' => 'btn btn-primary btn-md']) }}
+                                        <div class="clearfix"></div>
+                                    </div>
+                                    <!--edit-form-btn-->
+                                </div><!-- form-group -->
+                                {{ Form::close() }}
                             </div>
                         </div>
                     </div>
@@ -58,4 +42,5 @@
             </div>
         </div>
     </div>
+</div>
 @endsection
