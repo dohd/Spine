e0daf1a3e45c7a6ca3f7c849655bc97dffaab08d
Dohd <alloicebrian@gmail.com>
2022-01-14 09:47:40 +0300

    Update Leads form

--- resources/views/focus/leads/form.blade.php
+++ resources/views/focus/leads/form.blade.php
@@ -9,13 +9,13 @@
             <div class="form-group row">
                 <div class='col-md-12'>
                     <div class='col m-1'>
-                        {{ Form::label( 'method', trans('transactions.payer_type'),['class' => 'col-12 control-label']) }}
+                        <div><label for="client-type">Select Client Type</label></div>                        
                         <div class="d-inline-block custom-control custom-checkbox mr-1">
-                            <input type="radio" class="custom-control-input bg-primary" name="client_status" id="colorCheck1" value="customer" checked="">
+                            <input type="radio" class="custom-control-input bg-primary clientstatus" name="client_status" id="colorCheck1" value="customer" checked>
                             <label class="custom-control-label" for="colorCheck1">Existing</label>
                         </div>
                         <div class="d-inline-block custom-control custom-checkbox mr-1">
-                            <input type="radio" class="custom-control-input bg-purple" name="client_status" value="new" id="colorCheck3">
+                            <input type="radio" class="custom-control-input bg-purple clientstatus" name="client_status" value="new" id="colorCheck3">
                             <label class="custom-control-label" for="colorCheck3">New Client</label>
                         </div>
                     </div>
@@ -38,25 +38,31 @@
             </div>
 
             <div class="form-group row">
-                <div class="col-sm-6"><label for="client_name" class="caption"> Name</label>
+                <div class="col-sm-6"><label for="client_name" class="caption">Client Name</label>
                     <div class="input-group">
                         <div class="input-group-addon"><span class="icon-bookmark-o" aria-hidden="true"></span></div>
-                        {{ Form::text('client_name', null, ['class' => 'form-control round required', 'placeholder' => 'Name','id'=>'payer-name', 'readonly']) }}
+                        {{ Form::text('client_name', null, ['class' => 'form-control round', 'placeholder' => 'Name', 'id'=>'payer-name', 'readonly']) }}
                     </div>
                 </div>
-                <div class="col-sm-6"><label for="client_email" class="caption"> Email</label>
+                <div class="col-sm-6"><label for="client_email" class="caption">Client Email</label>
                     <div class="input-group">
                         <div class="input-group-addon"><span class="icon-bookmark-o" aria-hidden="true"></span></div>
-                        {{ Form::text('client_email', null, ['class' => 'form-control round required', 'placeholder' => 'Email','id'=>'client_email', 'readonly']) }}
+                        {{ Form::text('client_email', null, ['class' => 'form-control round', 'placeholder' => 'Email','id'=>'client_email', 'readonly']) }}
                     </div>
                 </div>
             </div>
 
             <div class="form-group row">
-                <div class="col-sm-6"><label for="client_contact" class="caption"> Contact</label>
+                <div class="col-sm-6"><label for="client_contact" class="caption">Client Contact</label>
                     <div class="input-group">
                         <div class="input-group-addon"><span class="icon-bookmark-o" aria-hidden="true"></span></div>
-                        {{ Form::text('client_contact', null, ['class' => 'form-control round required', 'placeholder' => 'Contact','id'=>'client_contact', 'readonly']) }}
+                        {{ Form::text('client_contact', null, ['class' => 'form-control round', 'placeholder' => 'Contact','id'=>'client_contact', 'readonly']) }}
+                    </div>
+                </div>
+                <div class="col-sm-6"><label for="client_address" class="caption">Client Address</label>
+                    <div class="input-group">
+                        <div class="input-group-addon"><span class="icon-bookmark-o" aria-hidden="true"></span></div>
+                        {{ Form::text('client_address', null, ['class' => 'form-control round', 'placeholder' => 'Contact', 'id' => 'client_address', 'readonly']) }}
                     </div>
                 </div>
             </div>
@@ -86,7 +92,7 @@
                 <div class="col-sm-6"><label for="date_of_request" class="caption">Callout Date</label>
                     <div class="input-group">
                         <div class="input-group-addon"><span class="icon-calendar4" aria-hidden="true"></span></div>
-                        {{ Form::text('date_of_request', null, ['class' => 'form-control round required', 'data-toggle'=>'datepicker', 'id' => 'date_of_request']) }}
+                        {{ Form::text('date_of_request', null, ['class' => 'form-control round datepicker', 'id' => 'date_of_request']) }}
                     </div>
                 </div>
             </div>
@@ -147,53 +153,37 @@
 {{ Html::script('core/app-assets/vendors/js/extensions/sweetalert.min.js') }}
 
 <script type="text/javascript">
-    // on selecting a payer type
-    $("input[name=client_status]").on('change', function() {
-        var payerType = $('input[name=client_status]:checked').val();
-
-        if (payerType !== 'customer') {
-            $('#person').attr('disabled', true);
-            $('#branch_id').attr('disabled', true);
-
-            $('#person').val('');
-            $('#branch_id').val('');
-            $('#payer-name').attr('readonly', false);
-            $('#client_email').attr('readonly', false);
-            $('#client_contact').attr('readonly', false);
-
-            $('#payer-name').val('');
-            $('#client_email').val('');
-            $('#client_contact').val('');
-        } else {
-            $('#person').attr('disabled', false);
-            $('#branch_id').attr('disabled', false);
-            $('#person').val('');
-            $('#branch_id').val('');
-
-            $('#payer-name').attr('readonly', true);
-            $('#client_email').attr('readonly', true);
-            $('#client_contact').attr('readonly', true);
-            $('#client_email').val('');
-            $('#client_contact').val('');
+    // On selecting client type radio
+    $('.clientstatus').change(function() {
+        if ($(this).val() === 'new') {
+            $('#person').attr('disabled', true).val('');
+            $('#branch_id').attr('disabled', true).val('');
+            $('#payer-name').attr('readonly', false).val('');
+            $('#client_email').attr('readonly', false).val('');
+            $('#client_contact').attr('readonly', false).val('');
+            $('#client_address').attr('readonly', false).val('');
+        } 
+        if ($(this).val() === 'customer') {
+            $('#person').attr('disabled', false).val('');
+            $('#branch_id').attr('disabled', false).val('');
+            $('#payer-name').attr('readonly', true).val('');
+            $('#client_email').attr('readonly', true).val('');;
+            $('#client_contact').attr('readonly', true).val('');
+            $('#client_address').attr('readonly', true).val('');
         }
     });
 
-    $('[data-toggle="datepicker"]')
-        .datepicker({
-            format: "{{config('core.user_date_format')}}"
-        })
-        .datepicker('setDate', new Date());
+    // Initialize datepicker
+    $('.datepicker').datepicker({ format: "{{config('core.user_date_format')}}"})    
+    $('#date_of_request').datepicker('setDate', new Date());
 
     // set ajax headers
     $.ajaxSetup({
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        }
+        headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') }
     });
 
-    // remove visibility for status checkbox
+    // Remove visibility for status checkbox
     $('.status-group').css('display', 'none');
-
 
     /**
      * Edit Lead form script
@@ -201,47 +191,36 @@
     const lead = @json(@$lead);
     const branch = @json(@$lead->branch);
     const customer = @json(@$lead->customer);
-    // if branch_id is 0 then its a new customer otherwise an existing customer
     if (lead && lead.hasOwnProperty('branch_id')) {
+        // if branch_id is 0 then its a new customer
         if (lead['branch_id'] === 0) {
-            $('#colorCheck1').prop('checked', false);
-            $('#colorCheck3').prop('checked', true);
+            $('#colorCheck1').attr('checked', false);
+            $('#colorCheck3').attr('checked', true);
+            $('#person').attr('disabled', true);
+            $('#branch_id').attr('disabled', true).select2();
+            $('#payer-name').attr('readonly', false);
+            $('#client_email').attr('readonly', false);
+            $('#client_contact').attr('readonly', false);
+            $('#client_address').attr('readonly', false);
+            $('input[type=radio]').attr('disabled', false);
+        } 
+        else {
+            $('#colorCheck1').attr('checked', true);
+            $('#colorCheck3').attr('checked', false);
+            $('#payer-name').attr('readonly', true);
+            $('#client_email').attr('readonly', true);
+            $('#client_contact').attr('readonly', true);
+            $('#client_address').attr('readonly', true);
 
-            $('#person').prop('disabled', true);
-            $('#branch_id').prop('disabled', true).select2();
-
-            $('#payer-name').prop('readonly', false);
-            $('#client_email').prop('readonly', false);
-            $('#client_contact').prop('readonly', false);
-        } else {
-            $('#colorCheck1').prop('checked', true);
-            $('#colorCheck3').prop('checked', false);
-
-            $('#payer-name').prop('readonly', true);
-            $('#client_email').prop('readonly', true);
-            $('#client_contact').prop('readonly', true);
-
-            // set default select option for customer and branch
-            $('#person').select2({
-                data: [{
-                    id: customer['id'],
-                    text: customer['name']
-                }]
-            });
-            $('#branch_id').select2({
-                data: [{
-                    id: branch['id'],
-                    text: branch['name']
-                }]
-            });
+            $('#person').append(new Option(customer['name'], customer['id']));
+            $('#branch_id').append(new Option(branch['name'], branch['id']));
+            $('input[type=radio]').prop('disabled', true);
         }
-        $('input[type=radio]').prop('disabled', true);
+
         $('#reference').val(lead['reference']);
         $('#ref_type').val(lead['source']);
-
-        // parse date using php date function
-        const date = "{{ date_for_database(@$lead->date_of_request) }}";
-        $('[data-toggle="datepicker"]').datepicker('setDate', new Date(date));
+        const date = @json(date_for_database(@$lead->date_of_request));
+        if (date) $('#date_of_request').datepicker('setDate', new Date(date));
     }
 
     // fetch customers
@@ -252,29 +231,22 @@
             dataType: 'json',
             type: 'POST',
             quietMillis: 50,
-            data: function(person) {
-                return {
-                    person
-                };
-            },
+            data: function(person) { return { person }; },
             processResults: function(data) {
                 return {
-                    results: $.map(data, function(item) {
-                        return {
-                            id: item.id,
-                            text: `${item.name} - ${item.company}`,
-                        }
-                    })
+                    results: data.map(v => ({ 
+                        id: item.id, 
+                        text: `${item.name} - ${item.company}`,
+                    }))
                 };
             },
         }
     });
 
     // on selecting a customer
-    $("#person").on('change', function() {
+    $("#person").change(function() {
         $("#branch_id").val('').trigger('change');
         const id = $('#person :selected').val();
-
         // fetch branches with params id from selected customer
         $("#branch_id").select2({
             ajax: {
@@ -282,22 +254,11 @@
                 dataType: 'json',
                 type: 'GET',
                 quietMillis: 50,
-                params: {
-                    'cat_id': id
-                },
-                data: function(product) {
-                    return {
-                        product
-                    };
-                },
+                params: { 'cat_id': id },
+                data: function(product) { return { product };},
                 processResults: function(data) {
-                    return {
-                        results: $.map(data, function(item) {
-                            return {
-                                text: item.name,
-                                id: item.id
-                            }
-                        })
+                    return { 
+                        results: data.map(v => ({ text: v.name, id: v.id }))                      
                     };
                 },
             }
