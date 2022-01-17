9d8864092cbbaebe75c4c55cc7cff6b559f95e43
Dohd <alloicebrian@gmail.com>
2022-01-17 14:43:09 +0300

    Update create_project_invoice page

--- resources/views/focus/invoices/create_project_invoice.blade.php
+++ resources/views/focus/invoices/create_project_invoice.blade.php
@@ -9,6 +9,7 @@
             <div class="card-content">
                 <div class="card-body">
                     <form method="post" id="data_form">
+                    {{ Form::open(['route' => 'biller.invoices.store_project_invoice', 'method' => 'POST']) }}
                         <div class="row mb-1">
                             <div class="col-3"><label for="payer" class="caption">Customer Name*</label>
                                 <div class="input-group">
@@ -89,19 +90,18 @@
                         <div class="row mb-1">
                             <div class="col-4">                                
                                 <div class="input-group">
-                                    <label for="ledger-off">Ledger Action</label>
+                                    <label for="ledger-off">Enable Ledger Account</label>
                                 </div>
-                                <select name="dr_account_id" class="form-control" id="dr_account_id">
-                                    <option value="">-- Select Ledger Action --</option>
-                                    <option value="0">Off</option>
-                                    <option value="1">Default</option>                                    
+                                <select class="form-control" name="ledger_toggle" id="ledgertoggle">
+                                    <option value="0" selected>No</option>
+                                    <option value="1">Yes</option>                                    
                                 </select>
                             </div>
                             <div class="col-4">                                
                                 <div class="input-group">
-                                    <label for="refer_no" class="caption">Ledger Account Asset(Debit)*</label>
+                                    <label for="refer_no" class="caption">Ledger Account Asset (Debit)*</label>
                                 </div>
-                                <select name="dr_account_id" class="form-control" id="dr_account_id">
+                                <select name="dr_account_id" class="form-control" id="dr_account" required disabled>
                                     <option value="">-- Select Ledger Account--</option>
                                     @foreach ($receivables as $account)
                                         <option value="{{ $account->id }}"> {{ $account->holder }}</option>
@@ -110,126 +110,97 @@
                             </div>
                             <div class="col-4">
                                 <div class="input-group">
-                                    <label for="tid" class="caption">Ledger Account/Income Account(Credit)*</label>
+                                    <label for="tid" class="caption">Ledger Account/Income Account (Credit)*</label>
                                 </div>
-                                <select name="cr_account_id" class="form-control" id="cr_account_id">
+                                <select name="cr_account_id" class="form-control" id="cr_account" required disabled>
                                     <option value="">-- Select Ledger Account --</option>
                                     @foreach ($income_accounts as $account)
                                         <option value="{{ $account->id }}">{{ $account->holder }}</option>
                                     @endforeach
                                 </select>
                             </div>
-                        </div>
+                        </div>                     
                         
+                        <div class="row mb-1">
+                            <div class="col-12">
+                                <div class="input-group"><label for="title" class="caption">Reference</label></div>
+                                {{ Form::text('ref', null, ['class' => 'form-control text-danger']) }}
+                            </div>
+                        </div>
+
                         <div class="row mb-1">
                             <div class="col-12">
                                 <div class="input-group"><label for="notes" class="caption">{{ trans('general.note') }}*</label></div>
                                 {{ Form::textarea('notes', null, ['class' => 'form-control text-danger html_editor', 'rows' => '2']) }}
                             </div>
                         </div>
-            
-                        <div class="tab-pane active in mt-2" id="active1" aria-labelledby="active-tab1" role="tabpanel">                            
-                            <table class="table-responsive tfr my_stripe">
-                                <thead>
-                                    <tr class="item_header bg-gradient-directional-blue white ">
-                                        <th width="35%" class="text-center">Description</th>
-                                        <th width="25%" class="text-center">Reference</th>
-                                        <th width="10%" class="text-center">UOM</th>
-                                        <th width="10%" class="text-center">Qty</th>
-                                        <th width="10%" class="text-center">Rate Exclusive</th>
-                                        <th width="10%" class="text-center">{{ trans('general.amount') }} ({{ config('currency.symbol') }})</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
+
+                        <table id="quotation" class="table-responsive tfr my_stripe_single pb-1">
+                            <thead>
+                                <tr class="item_header bg-gradient-directional-blue white">
+                                    <th class="text-center">#</th>
+                                    <th width="25%" class="text-center">Reference</th>
+                                    <th width="35%" class="text-center">Description</th>
+                                    <th width="10%" class="text-center">UOM</th>
+                                    <th width="10%" class="text-center">Qty</th>
+                                    <th width="10%" class="text-center">Rate (VAT exc)</th>
+                                    <th width="10%" class="text-center">Amount</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($quotes as $k => $val)
                                     @php
-                                        $sub_total = 0;
-                                        $tax_total = 0;
-                                        $grand_total = 0;
-                                        $i = 0;
-                                    @endphp
-                                    @foreach ($quotes as $quote)
-                                    @php
-                                        $i++;
-                                        $ref = '';
-                                        if ($quote->verified_jcs) {
-                                            $ref_array = [];
-                                            foreach ($quote->verified_jcs as $ref_row) {
-                                                $ref = 'JC-' . $ref_row->reference;
-                                                if ($ref_row->type == 2) {
-                                                    $ref = 'DN-' . $ref_row->reference;
-                                                }
-                                                $ref_array[] = $ref;
-                                            }
-                                            $ref = implode(', ', $ref_array);
+                                        // Reference details
+                                        $tid = sprintf('%04d', $val->tid);
+                                        $tid = $val->bank_id ? 'PI-'.$tid : 'QT-'.$tid;
+                                        $lpo_no = $val->lpo ? $val->lpo->lpo_no : '';
+                                        $client_ref = $val->client_ref;
+                                        $branch_code = $val->branch->branch_code;
+
+                                        // Description details
+                                        $title = $val->notes;
+                                        $jcs = array();
+                                        foreach($val->verified_jcs as $jc) {
+                                            if ($jc->type == 2) $jcs[] = 'DN-'.$jc->reference;
+                                            else $jcs[] = 'JC-'.$jc->reference;
                                         }
 
-                                        $tid = sprintf('%04d', $quote->tid);
-                                        if ($quote->bank_id) {
-                                            $tids = 'PI-' . $tid;
-                                        } else {
-                                            $tids = 'QT-' . $tid;
-                                        }
-                                        $sub_total += $quote->verified_amount;
-                                        $note=$quote->notes;
-                                        if(isset($quote->branch_id)) {
-                                            $note=$quote->branch->name.'-'.$quote->notes;
-                                        }
+                                        // Table values
+                                        $reference = implode('; ', [$branch_code, $tid, $lpo_no, $client_ref]);
+                                        $description = $title . '; ' . implode(', ', $jcs);
                                     @endphp
+
                                     <tr>
-                                        <td><input type="text" value="{{ $note }}" class="form-control" name="description[]" id='description-{{ $i }}'></td>
-                                        <td><input type="text" class="form-control req amnt" name="reference[]" id="reference-{{ $i }}" autocomplete="off" value="{{ $tids }} : {{ $ref }}"></td>
-                                        <td><input type="text" class="form-control " name="unit[]" id="unit-{{ $i }}" value="Lot" autocomplete="off"></td>
-                                        <td><input type="text" class="form-control" value="1" name="product_qty[]" id="product_qty-{{ $i }}" autocomplete="off" readonly>
-                                        </td>
-                                        <td><input type="text" class="form-control  " name="product_price[]" id="product_price-{{ $i }}" value="{{ $quote->verified_amount }}" autocomplete="off" readonly></td>
-                                        <td><span class="currenty">{{ config('currency.symbol') }}</span>
-                                            <strong><span class='ttlText' id="result-{{ $i }}">{{ $quote->verified_amount }}</span></strong>
-                                        </td>
-                                        <input type="hidden" name="quote_id[]" id="quote_id-{{ $i }}" value="{{ $quote->id }}">
-                                        <input type="hidden" class="pdIn" name="project_id[]" id="project_id-{{ $i }}" value="{{ $quote->project_quote_id }}">
-                                        <input type="hidden" class="pdIn" name="branch_id[]" id="branch_id-{{ $i }}" value="{{ $quote->branch_id }}">
+                                        <td><span>{{ $k+1 }}</span></td>                
+                                        <td><input type="text" class="form-control" name="reference[]" value="{{ $reference }}" id="reference-{{ $k }}"></td>
+                                        <td><input type="text" class="form-control" name="description[]" value="{{ $description }}" id="description-{{ $k }}"></td>
+                                        <td><input type="text" class="form-control " name="unit[]" id="unit-{{ $k }}" value="Lot"></td>
+                                        <td><input type="text" class="form-control" name="product_qty[]" id="product_qty-{{ $k }}" value="1" readonly></td>
+                                        <td><input type="text" class="form-control" name="product_price[]" id="product_price-{{ $k }}" readonly></td>
+                                        <td><strong><span class='ttlText' id="result-{{ $k }}">0</span></strong></td>
+                                        <input type="hidden" name="quote_id[]" id="quote_id-{{ $k }}">
+                                        <input type="hidden" class="pdIn" name="project_id[]" id="project_id-{{ $k }}">
+                                        <input type="hidden" class="pdIn" name="branch_id[]" id="branch_id-{{ $k }}">
                                     </tr>
-                                    @endforeach
-                                    @php
-                                        $tax_total = $sub_total * 0.16;
-                                        $grand_total = $sub_total * 1.16;
-                                    @endphp
-                                    <tr class="sub_c" style="display: table-row;">
-                                        <td colspan="4" align="right">
-                                            {{ Form::hidden('subtotal',  number_format($sub_total, 2), ['id' => 'subttlform']) }}
-                                            <strong>Sub Total ({{ config('currency.symbol') }}) </strong>
-                                        </td>
-                                        <td align="left" colspan="2">
-                                            <span id="taxr" class="lightMode">{{ number_format($sub_total, 2) }}</span>
-                                        </td>
-                                    </tr>
-                                    <tr class="sub_c" style="display: table-row;">
-                                        <td colspan="4" align="right">
-                                            <strong>Total Tax</strong>
-                                        </td>
-                                        <td align="left" colspan="2"><input type="text" value="{{ number_format($tax_total, 2) }}" name="tax" class="form-control" id="tax" value="{{ $sub_total }}" readonly=""></td>
-                                    </tr>
-                                    <tr class="sub_c" style="display: table-row;">
-                                        <td colspan="4" align="right">
-                                            <strong>{{ trans('general.grand_total') }}
-                                                (<span class="currenty lightMode">{{ config('currency.symbol') }}</span>)</strong>
-                                        </td>
-                                        <td align="left" colspan="2"><input type="text" name="total" class="form-control" id="total" value="{{ number_format($grand_total, 2) }}" readonly="">
-                                        </td>
-                                    </tr>
-                                    <tr class="sub_c" style="display: table-row;">
-                                        <td colspan="2"></td>
-                                        <td align="right" colspan="5"><input type="submit" class="btn btn-success sub-btn btn-lg" value="Create Invoice" id="submit-data" data-loading-text="Creating...">
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>                            
+                                @endforeach
+                            </tbody>
+                        </table>
+
+                        <div class="float-right">
+                            <div>
+                                <label for="subtotal" class="caption font-weight-bold">Subtotal</label>
+                                <input type="number" class="form-control mb-1" id="subtotal">
+                            </div>                                                         
+                            <div>
+                                <label for="totaltax" class="caption font-weight-bold">Total Tax</label>
+                                <input type="number" class="form-control mb-1" id="totaltax">
+                            </div>
+                            <div>
+                                <label for="grandtotal" class="caption font-weight-bold">Grand Total</label>
+                                <input type="number" class="form-control mb-1" id="grandtotal">
+                            </div>                           
                         </div>
-                        <input type="hidden" value="new_i" id="inv_page">
-                        <input type="hidden" name="term_id" value="1" id="term_id">
-                        <input type="hidden" value="{{ route('biller.invoices.store_project_invoice') }}" id="action-url">
-                        <input type="hidden" value="search" id="billtype">
-                    </form>
+                    {{ Form::close() }}
                 </div>
             </div>
         </div>
@@ -249,14 +220,19 @@
         .datepicker({ format: "{{config('core.user_date_format')}}"})
         .datepicker('setDate', new Date());
 
-    $('#tax_id').change(function() {
-        var tax_id = $('#tax_id').val()
-        var subtotal = accounting.unformat($("#subtotal").val(), accounting.settings.number.decimal);
-        var tax = subtotal * tax_id / 100;
-        $('#tax_total').val(accounting.formatNumber(tax));
-        $('#geand_total').val(accounting.formatNumber(tax + subtotal));
-        console.log(subtotal);
-        // console.log(lpo_number);
+    // Disable Account ledgers
+    $('#ledgertoggle').change(function() {
+        $('#dr_account').attr('disabled', true);
+        $('#cr_account').attr('disabled', true);
+        if ($(this).val() == 1) {
+            $('#dr_account').attr('disabled', false);
+            $('#cr_account').attr('disabled', false);
+        }
     });
+
+    // Load default quotes
+    const quotes = @json($quotes);
+    console.log(quotes[0]);
+
 </script>
 @endsection
