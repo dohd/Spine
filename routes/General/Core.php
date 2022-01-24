1b9cbeb8cdff90851a379e5e5f06934a79410ca4
Dohd <alloicebrian@gmail.com>
2022-01-24 08:53:17 +0300

    Add print_project_budget route

--- routes/General/Core.php
+++ routes/General/Core.php
@@ -34,6 +34,7 @@ Route::group(['namespace' => 'Focus', 'as' => 'biller.'], function () {
         Route::get('print_quote/{id}/{type}/{token}/{pdf}', 'BillsController@print_quote_pdf')->name('print_quote');
         Route::get('print_verified_quote/{id}/{type}/{token}/{pdf}', 'BillsController@print_verified_quote_pdf')->name('print_verified_quote');
         Route::get('print_rjc/{id}/{type}/{token}/{pdf}', 'BillsController@print_rjc_pdf')->name('print_rjc');
+        Route::get('print_budget/{id}/{type}/{token}/{pdf}', 'BillsController@print_budget_pdf')->name('print_budget');
 
         Route::get('print_compact/{id}/{type}/{token}/{pdf}', 'BillsController@print_compact')->name('print_compact');
         Route::get('view_bank/{id}/{type}/{token}', 'BillsController@view_bank')->name('view_bank');
