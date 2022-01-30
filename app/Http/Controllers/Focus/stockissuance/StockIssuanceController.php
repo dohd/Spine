9dab71d54c2130092bce000a171ed4aac3dd365a
Dohd <alloicebrian@gmail.com>
2022-01-30 22:37:37 +0300

    Update stock issuance controller

--- app/Http/Controllers/Focus/stockissuance/StockIssuanceController.php
+++ app/Http/Controllers/Focus/stockissuance/StockIssuanceController.php
@@ -7,7 +7,9 @@ use App\Models\product\ProductVariation;
 use App\Models\project\Budget;
 use App\Models\project\BudgetItem;
 use App\Models\quote\Quote;
+use App\Models\stock\IssueItemLog;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class StockIssuanceController extends Controller
 {
@@ -18,24 +20,26 @@ class StockIssuanceController extends Controller
      */
     public function index(Request $request)
     {
-        $input = $request->only('rel_type', 'rel_id');
-        
+        // extract input fields
+        $relshp_type = request('rel_type');
+        $relshp_id = request('rel_id');
+
         $segment = array();
         $words = array();
-        if (isset($input['rel_id']) and isset($input['rel_type'])) {
-            switch ($input['rel_type']) {
-                case 1:
-                    $segment = Customer::find($input['rel_id']);
-                    $words['name'] = trans('customers.title');
-                    $words['name_data'] = $segment->name;
-                    break;
-                case 2:
-                    $segment = Hrm::find($input['rel_id']);
-                    $words['name'] = trans('hrms.employee');
-                    $words['name_data'] = $segment->first_name . ' ' . $segment->last_name;
-                    break;
+        if ($relshp_type && $relshp_id) {
+            if ($relshp_type == 1) {
+                $segment = Customer::find($relshp_id);
+                $words['name'] = trans('customers.title');
+                $words['name_data'] = $segment->name;
+            }
+            else {
+                $segment = Hrm::find($relshp_id);
+                $words['name'] = trans('hrms.employee');
+                $words['name_data'] = $segment->first_name . ' ' . $segment->last_name;
             }
         }
+
+        $input = array($relshp_type, $relshp_id);
 
         return view('focus.stockissuance.index', compact('input', 'segment', 'words'));
     }
@@ -45,11 +49,39 @@ class StockIssuanceController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function issue_stock(Quote $quote)
+    public function issue_stock()
     {
-        $budget = Budget::where('quote_id', $quote->id)->first();
+        // extract input fields
+        $product_id = request('product_id');
+        $item_id = request('item_id');
+        $input_qty = request('issue_qty');
 
-        return view('focus.stockissuance.issue_stock', compact('quote', 'budget'));
+        DB::beginTransaction();
+
+        $store_product = ProductVariation::find($product_id);
+        if (!$store_product || !$item_id) return response()->noContent();
+        if ($store_product->qty < $input_qty) $input_qty = $store_product->qty;
+
+        $budget_item = BudgetItem::find($item_id);
+        // reduce stock by input qty
+        $store_product->decrement('qty', $input_qty);
+        // increament value of issued by input qty
+        $budget_item->increment('issue_qty', $input_qty);
+        // store log
+        $user = auth()->user();
+        IssueItemLog::create([
+            'item_id' => $item_id,
+            'issue_qty' => $input_qty,
+            'issued_by' => $user->id,
+            'issuer' => $user->first_name . ' ' . $user->last_name
+        ]);
+
+        DB::commit();
+
+        return response()->json([
+            'issued' => $input_qty, 
+            'issue_qty' => $budget_item->issue_qty
+        ]);
     }
 
     /**
@@ -57,9 +89,15 @@ class StockIssuanceController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(Request $request)
     {
-        //
+        $query_str = $request->getQueryString();
+        $quote_id = explode('=', $query_str)[0];
+
+        $quote = Quote::find($quote_id);
+        $budget = Budget::where('quote_id', $quote->id)->first();
+
+        return view('focus.stockissuance.issue_stock', compact('quote', 'budget'));
     }
 
     /**
@@ -71,71 +109,37 @@ class StockIssuanceController extends Controller
     public function store(Request $request)
     {
         // extract input fields
-        $item = $request->only([
-            'numbering', 'item_id', 'budget_id', 'product_name', 'unit', 'new_qty', 'issue_qty', 'price',
-            'row_index', 'product_id', 'a_type',
+        $input = $request->only([
+            'numbering', 'product_name', 'unit', 'price', 'new_qty', 'product_id', 
+            'row_index', 'budget_id', 'a_type'
         ]);
 
-        // decreament issued item from stock
-        if ($item['product_id'])
-            ProductVariation::find($item['product_id'])->decrement('qty', $item['issue_qty']); 
-
-        // update or create new item
-        $budget_item = BudgetItem::firstOrNew([
-            'id' => $item['item_id'],
-            'budget_id' => $item['budget_id'],
-        ]);
-        // assign properties to budget_item
-        foreach($item as $key => $value) {
-            if ($key == 'price') $budget_item[$key] = numberClean($value);
-            else $budget_item[$key] = $value;
-        }
-        unset($budget_item['item_id']);
-        if ($budget_item['id'] == 0) unset($budget_item['id']);
-        $budget_item->save();
+        $input['price'] = numberClean($input['price']);
+        $budget_item = BudgetItem::create($input);        
 
         return response()->json($budget_item);
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * Get log for issued stock item
      */
-    public function show($id)
+    public function get_issued_log($id)
     {
-        //
+        $logs = IssueItemLog::where('item_id', $id)->orderBy('id', 'desc')->get();
+
+        return response()->json($logs);
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * Budgeted quotes for stock issuance
      */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    // Stock verification data
     static function getForDataTable()
     {
         $q = Quote::query();
-
+        // Budgeted quotes
+        $quote_ids = Budget::get()->pluck('quote_id');
+        $q->whereIn('id', $quote_ids);
+        
         $q->when(request('i_rel_type') == 1, function ($q) {
             return $q->where('customer_id', request('i_rel_id', 0));
         });
@@ -146,10 +150,6 @@ class StockIssuanceController extends Controller
                 date_for_database(request('end_date'))
             ]);
         }
-
-        // Budgeted quotes
-        $quote_ids = Budget::get()->pluck('quote_id');
-        $q->whereIn('id', $quote_ids);
 
         return $q->get([
             'id', 'notes', 'tid', 'customer_id', 'branch_id', 'lead_id', 'invoicedate', 'invoiceduedate', 
