f920c9f1451e3e222f8b1ab44337a630a8507a20
Dohd <alloicebrian@gmail.com>
2022-02-05 12:35:14 +0300

    Alter remark column on lpos table

---
+++ database/migrations/2022_02_05_080554_alter_remark_column_on_lpos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class AlterRemarkColumnOnLposTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('lpos', function (Blueprint $table) {
+            //
+        });
+
+        DB::statement('ALTER TABLE rose_lpos MODIFY remark VARCHAR(200) NULL');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('lpos', function (Blueprint $table) {
+            //
+        });
+    }
+}
