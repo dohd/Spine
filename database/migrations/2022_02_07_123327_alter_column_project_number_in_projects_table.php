1b9ba1e30a3bfa4fb652f94f6ef010ffaadc80b4
Dohd <alloicebrian@gmail.com>
2022-02-07 15:40:43 +0300

    Alter column project_number in projects table

---
+++ database/migrations/2022_02_07_123327_alter_column_project_number_in_projects_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class AlterColumnProjectNumberInProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            //
+        });
+
+        DB::statement('ALTER TABLE rose_projects MODIFY project_number INT DEFAULT 0');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            //
+        });
+    }
+}
