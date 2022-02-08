44e5fae8ca35a501e239392dd29a2660f9d229a9
Dohd <alloicebrian@gmail.com>
2022-02-08 10:26:18 +0300

    Add quotes relation method

--- app/Models/lead/Traits/LeadRelationship.php
+++ app/Models/lead/Traits/LeadRelationship.php
@@ -4,22 +4,26 @@ namespace App\Models\lead\Traits;
 
 use App\Models\branch\Branch;
 use App\Models\customer\Customer;
+use App\Models\quote\Quote;
 use DB;
+
 /**
  * Class ProductcategoryRelationship
  */
 trait LeadRelationship
 {
-    public function branch()
-    {
-         return $this->belongsTo(Branch::class,'branch_id');
-    }
+     public function quotes() 
+     {
+          return $this->hasMany(Quote::class);
+     }
 
-    public function customer()
-    {
-         return $this->belongsTo(Customer::class,'client_id');
-    }
+     public function branch()
+     {
+          return $this->belongsTo(Branch::class, 'branch_id');
+     }
 
-
-      
+     public function customer()
+     {
+          return $this->belongsTo(Customer::class, 'client_id');
+     }
 }
