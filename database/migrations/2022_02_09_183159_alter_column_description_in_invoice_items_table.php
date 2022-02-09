2e8ee0b23c26739376712ec9593bafeb103ef480
Dohd <alloicebrian@gmail.com>
2022-02-09 21:35:46 +0300

    Alter column description in invoice_items table

---
+++ database/migrations/2022_02_09_183159_alter_column_description_in_invoice_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class AlterColumnDescriptionInInvoiceItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('invoice_items', function (Blueprint $table) {
+            //
+        });
+
+        DB::statement('ALTER TABLE rose_invoice_items MODIFY description VARCHAR(1000)');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('invoice_items', function (Blueprint $table) {
+            //
+        });
+    }
+}
