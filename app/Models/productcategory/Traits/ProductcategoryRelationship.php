40752d6ad731771381f384ec79b478a44273a42c
Dohd <alloicebrian@gmail.com>
2022-02-16 22:35:44 +0300

    Format document

--- app/Models/productcategory/Traits/ProductcategoryRelationship.php
+++ app/Models/productcategory/Traits/ProductcategoryRelationship.php
@@ -5,6 +5,7 @@ namespace App\Models\productcategory\Traits;
 use App\Models\product\Product;
 use App\Models\product\ProductVariation;
 use DB;
+
 /**
  * Class ProductcategoryRelationship
  */
@@ -12,11 +13,11 @@ trait ProductcategoryRelationship
 {
     public function subcategories()
     {
-        return $this->hasMany(Self::class,'rel_id','id');
+        return $this->hasMany(Self::class, 'rel_id', 'id');
     }
 
-     public function products()
+    public function products()
     {
-        return $this->hasManyThrough(ProductVariation::class,Product::class)->select([DB::raw('qty*price as total_value'),'qty']);
+        return $this->hasManyThrough(ProductVariation::class, Product::class)->select([DB::raw('qty*price as total_value'), 'qty']);
     }
 }
