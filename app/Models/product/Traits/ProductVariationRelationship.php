24500aa94486625c909caebad9a879e9ac0a9a10
Dohd <alloicebrian@gmail.com>
2022-02-16 22:35:21 +0300

    Remove category method relation

--- app/Models/product/Traits/ProductVariationRelationship.php
+++ app/Models/product/Traits/ProductVariationRelationship.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Models\product\Traits;
+
 use App\Models\product\Product;
 use App\Models\product\ProductMeta;
 use App\Models\warehouse\Warehouse;
@@ -12,41 +13,28 @@ use App\Models\pricegroup\PriceGroupVariation;
  */
 trait ProductVariationRelationship
 {
-
     public function warehouse()
     {
-        return $this->hasOne(Warehouse::class,'id','warehouse_id');
+        return $this->hasOne(Warehouse::class, 'id', 'warehouse_id');
     }
 
-        public function product_serial()
+    public function product_serial()
     {
-        return $this->hasMany(ProductMeta::class, 'ref_id', 'id')->where('rel_type','=',2)->withoutGlobalScopes();
+        return $this->hasMany(ProductMeta::class, 'ref_id', 'id')->where('rel_type', '=', 2)->withoutGlobalScopes();
     }
 
-
-        public function product()
+    public function product()
     {
         return $this->belongsTo(Product::class);
     }
 
-     public function v_prices()
+    public function v_prices()
     {
-        return $this->hasOne(PriceGroupVariation::class,'product_variation_id','id');
+        return $this->hasOne(PriceGroupVariation::class, 'product_variation_id', 'id');
     }
 
-
-        public function variation_price()
+    public function variation_price()
     {
-         return $this->hasOneThrough(Pricegroup::class,PriceGroupVariation::class,'product_variation_id','pricegroup_id');
+        return $this->hasOneThrough(Pricegroup::class, PriceGroupVariation::class, 'product_variation_id', 'pricegroup_id');
     }
-
-         public function category()
-    {
-        return $this->hasOneThrough(Productcategory::class,Product::class,'product_id','productcategory_id');
-    }
-
-
-
-
-
 }
