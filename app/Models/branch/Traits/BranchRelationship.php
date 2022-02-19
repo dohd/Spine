8363d6616cd65ad496b0c24f0c62dadbe00b354c
Dohd <alloicebrian@gmail.com>
2022-02-19 09:50:38 +0300

    Add leads relation method

--- app/Models/branch/Traits/BranchRelationship.php
+++ app/Models/branch/Traits/BranchRelationship.php
@@ -3,12 +3,18 @@
 namespace App\Models\branch\Traits;
 
 use App\Models\customer\Customer;
+use App\Models\lead\Lead;
 
 /**
  * Class ProductcategoryRelationship
  */
 trait BranchRelationship
 {
+    public function leads()
+    {
+        return $this->hasMany(Lead::class);
+    }
+
     public function customer()
     {
         return $this->belongsTo(Customer::class);
