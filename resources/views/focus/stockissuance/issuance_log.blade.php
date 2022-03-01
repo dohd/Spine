53ff7e6b2de7b8cabcf994785f7bc0506a80b526
Dohd <alloicebrian@gmail.com>
2022-03-01 09:43:36 +0300

    Add warehouse and action column

--- resources/views/focus/stockissuance/issuance_log.blade.php
+++ resources/views/focus/stockissuance/issuance_log.blade.php
@@ -1,5 +1,5 @@
 <div class="modal" tabindex="-1" role="dialog" id="issuanceLog">
-    <div class="modal-dialog" role="document">
+    <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title">Stock Issuance Log</h5>
@@ -12,10 +12,12 @@
                     <thead>
                         <tr class="item_header bg-gradient-directional-blue white">
                             <th width="10%" class="text-center">#</th>  
-                            <th width="20%" class="text-center">Issued Qty</th>
-                            <th width="20%" class="text-center">Requisition No.</th>
+                            <th width="10%">Qty</th>
+                            <th width="10%">Reqxn No.</th>
                             <th width="20%" class="text-center">Issued By</th>
-                            <th width="20%" class="text-center">Date</th>                                                      
+                            <th width="20%">Warehouse</th>
+                            <th width="20%">Date</th> 
+                            <th width="20%">Action</th>                                                  
                         </tr>
                     </thead>
                     <tbody></tbody>                    
