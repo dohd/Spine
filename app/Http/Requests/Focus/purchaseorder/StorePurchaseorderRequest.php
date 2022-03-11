00284c9a6a03cf54ddc2fbf09453d888e219dc59
Dohd <alloicebrian@gmail.com>
2022-03-11 11:20:56 +0300

    Comment out purchaseorder field validators

--- app/Http/Requests/Focus/purchaseorder/StorePurchaseorderRequest.php
+++ app/Http/Requests/Focus/purchaseorder/StorePurchaseorderRequest.php
@@ -31,10 +31,10 @@ class StorePurchaseorderRequest extends FormRequest
         $rules = array();
         if ($this->getMethod() == 'POST') {
             $rules = [
-                'supplier_id' => 'required|integer|min:1',
-                'tid' => 'required|integer|min:1',
-                'total' => 'required',
-                'term_id' => 'required|integer|min:1',
+                // 'supplier_id' => 'required|integer|min:1',
+                // 'tid' => 'required|integer|min:1',
+                // 'total' => 'required',
+                // 'term_id' => 'required|integer|min:1',
             ];
         }
         return $rules;
@@ -43,10 +43,10 @@ class StorePurchaseorderRequest extends FormRequest
     public function messages()
     {
         return [
-            'supplier_id.min' => trans('suppliers.valid_enter'),
-            'tid.required' => trans('invoices.invalid_number'),
-            'tid.min' => trans('invoices.invalid_number'),
-            'term_id.required' => trans('terms.required'),
+            // 'supplier_id.min' => trans('suppliers.valid_enter'),
+            // 'tid.required' => trans('invoices.invalid_number'),
+            // 'tid.min' => trans('invoices.invalid_number'),
+            // 'term_id.required' => trans('terms.required'),
         ];
     }
 }
