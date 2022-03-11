9b302200dffba5358dfc0200c6b3a142f8140b3f
Dohd <alloicebrian@gmail.com>
2022-03-11 09:08:21 +0300

    Update bills and bill_items columns

---
+++ database/migrations/2022_03_11_054514_add_columns_to_bills_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsToBillsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('bills', function (Blueprint $table) {
+            $table->integer('is_po')->default(0);
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('bills', function (Blueprint $table) {
+            //
+        });
+    }
+}
