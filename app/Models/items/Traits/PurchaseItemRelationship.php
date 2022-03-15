5fdf8431d53b16aa316b0feb350b11f11af87587
Dohd <alloicebrian@gmail.com>
2022-03-15 15:32:44 +0300

    Add asset, account and project relation methods

--- app/Models/items/Traits/PurchaseItemRelationship.php
+++ app/Models/items/Traits/PurchaseItemRelationship.php
@@ -2,12 +2,30 @@
 
 namespace App\Models\items\Traits;
 
+use App\Models\account\Account;
+use App\Models\assetequipment\Assetequipment;
+use App\Models\project\Project;
 
 /**
  * Class CustomerRelationship
  */
 trait PurchaseItemRelationship
 {
+    public function asset()
+    {
+        return $this->belongsTo(Assetequipment::class, 'item_id');
+    }
+
+    public function account()
+    {
+        return $this->belongsTo(Account::class, 'item_id');
+    }
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class, 'itemproject_id');
+    }
+
     public function product()
     {
         return $this->belongsTo('App\Models\product\ProductVariation', 'item_id');
