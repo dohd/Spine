574159bed89ea074d0387ab79c46814981e1835c
Dohd <alloicebrian@gmail.com>
2022-03-16 16:38:40 +0300

    Create PurchaseOrderItemRelationship trait

---
+++ app/Models/items/Traits/PurchaseorderItemRelationship.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models\items\Traits;
+
+use App\Models\account\Account;
+use App\Models\assetequipment\Assetequipment;
+use App\Models\project\Project;
+
+trait PurchaseorderItemRelationship
+{
+    public function asset()
+    {
+        return $this->belongsTo(Assetequipment::class, 'item_id');
+    }
+
+    public function account()
+    {
+        return $this->belongsTo(Account::class, 'item_id');
+    }
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class, 'itemproject_id');
+    }
+
+    public function product()
+    {
+        return $this->belongsTo('App\Models\product\ProductVariation', 'item_id');
+    }
+
+    public function variation()
+    {
+        return $this->belongsTo('App\Models\product\ProductVariation', 'item_id')->withoutGlobalScopes();
+    }
+}
