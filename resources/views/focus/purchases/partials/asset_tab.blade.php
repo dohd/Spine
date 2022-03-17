f408aa8f7fea15c7dcb1150ea0cbb2b125e0eab9
Dohd <alloicebrian@gmail.com>
2022-03-17 12:48:21 +0300

    Update purchases stock, expense and asset tab

--- resources/views/focus/purchases/partials/asset_tab.blade.php
+++ resources/views/focus/purchases/partials/asset_tab.blade.php
@@ -12,24 +12,62 @@
             </tr>
         </thead>
         <tbody>
+            <!-- layout -->
             <tr>
                 <td><input type="text" class="form-control assetname" name="name[]" placeholder="Asset Or Equipment"></td>
                 <td><input type="text" class="form-control asset_qty" name="qty[]" value="1" id="assetqty-0"></td>
                 <td><input type="text" class="form-control asset_price" name="rate[]" id="assetprice-0"></td>
-                <td><input type="text" class="form-control asset_vat" name="tax_rate[]" value="0" id="assetvat-0"></td>
+                <td><input type="text" class="form-control asset_vat" name="itemtax[]" value="0" id="assetvat-0"></td>
                 <td class="text-center"><span class="asset_tax">0</span></td>
                 <td>{{config('currency.symbol')}} <b><span class='asset_amount'>0</span></b></td>
                 <td><button type="button" class="btn btn-danger remove d-none">remove</button></td>
                 <input type="hidden" id="assetitemid-0" name="item_id[]">
-                <input type="hidden" class="assettaxr" name="tax[]">
+                <input type="hidden" class="assettaxr" name="taxrate[]">
                 <input type="hidden" class="assetamountr" name="amount[]">
                 <input type="hidden" class="assetitemprojectid" name="itemproject_id[]" value="0">
                 <input type="hidden" name="type[]" value="Asset">
+                <input type="hidden" name="id[]" value="0">
+                <input type="hidden" name="uom[]">
             </tr>
             <tr>
-                <td colspan="3"><textarea class="form-control" name="description[]" placeholder="Product Description" id="assetdescr-0"></textarea></td>
+                <td colspan="3"><textarea class="form-control descr" name="description[]" placeholder="Product Description" id="assetdescr-0"></textarea></td>
                 <td colspan="4"></td>
             </tr>
+            <!-- end layout -->
+            
+            <!-- fetched rows -->
+            @isset ($po)
+                @php ($i = 0)
+                @foreach ($po->products as $item)
+                    @if ($item->type == 'Asset')
+                        <tr>
+                            <td><input type="text" class="form-control assetname" name="name[]" value="{{ $item->asset->name }}" placeholder="Asset Or Equipment"></td>
+                            <td><input type="text" class="form-control asset_qty" name="qty[]" value="{{ (int) $item->qty }}" id="assetqty-{{$i}}"></td>
+                            <td><input type="text" class="form-control asset_price" name="rate[]" value="{{ (float) $item->rate }}" id="assetprice-{{$i}}"></td>
+                            <td><input type="text" class="form-control asset_vat" name="itemtax[]" value="{{ (int) $item->itemtax }}" id="assetvat-{{$i}}"></td>
+                            <td class="text-center"><span class="asset_tax">{{ (float) $item->taxrate }}</span></td>
+                            <td>{{config('currency.symbol')}} <b><span class='asset_amount'>{{ (float) $item->amount }}</span></b></td>
+                            <td><button type="button" class="btn btn-danger remove">remove</button></td>
+                            <input type="hidden" id="assetitemid-{{$i}}" name="item_id[]" value="{{ $item->item_id }}">
+                            <input type="hidden" class="assettaxr" name="taxrate[]" value="{{ (float) $item->taxrate }}">
+                            <input type="hidden" class="assetamountr" name="amount[]" value="{{ (float) $item->amount }}">
+                            <input type="hidden" class="assetitemprojectid" name="itemproject_id[]" value="0">
+                            <input type="hidden" name="type[]" value="Asset">
+                            <input type="hidden" name="id[]" value="{{ $item->id }}">
+                            <input type="hidden" name="uom[]">
+                        </tr>
+                        <tr>
+                            <td colspan="3">
+                                <textarea class="form-control descr" name="description[]" placeholder="Product Description" id="assetdescr-{{$i}}">{{ $item->description }}</textarea>
+                            </td>
+                            <td colspan="4"></td>
+                        </tr>
+                        @php ($i++)
+                    @endif
+                @endforeach
+            @endisset
+            <!-- end fetched rows -->
+
             <tr class="bg-white">
                 <td>
                     <button type="button" class="btn btn-success" aria-label="Left Align" id="addasset">
--- resources/views/focus/purchases/partials/stock_tab.blade.php
+++ resources/views/focus/purchases/partials/stock_tab.blade.php
@@ -12,12 +12,13 @@
             </tr>
         </thead>
         <tbody>
+            <!-- layout -->
             <tr>
                 <td><input type="text" class="form-control stockname" name="name[]" placeholder="Product Name" id='stockname-0'></td>
-                <td><input type="text" class="form-control qty" name="qty[]" id="qty-0" value="1"></td>                    
+                <td><input type="text" class="form-control qty" name="qty[]" id="qty-0" value="1"></td>  
                 <td><input type="text" class="form-control price" name="rate[]" id="price-0"></td>
                 <td>
-                    <select class="form-control rowtax" name="tax_rate[]" id="rowtax-0">
+                    <select class="form-control rowtax" name="itemtax[]" id="rowtax-0">
                         @foreach ($additionals as $tax)
                             <option value="{{ (int) $tax->value }}" {{ $tax->is_default ? 'selected' : ''}}>
                                 {{ $tax->name }}
@@ -29,17 +30,60 @@
                 <td class="text-center">{{config('currency.symbol')}} <b><span class='amount' id="result-0">0</span></b></td>              
                 <td><button type="button" class="btn btn-danger d-none remove">Remove</button></td>
                 <input type="hidden" id="stockitemid-0" name="item_id[]">
-                <input type="hidden" class="stocktaxr" name="tax[]">
+                <input type="hidden" class="stocktaxr" name="taxrate[]">
                 <input type="hidden" class="stockamountr" name="amount[]">
                 <input type="hidden" class="stockitemprojectid" name="itemproject_id[]" value="0">
                 <input type="hidden" name="type[]" value="Stock">
+                <input type="hidden" name="id[]" value="0">
             </tr>
             <tr>
-                <td colspan=2>
-                    <textarea id="stockdescr-0" class="form-control" name="description[]" placeholder="Product Description"></textarea>
+                <td colspan="2">
+                    <textarea id="stockdescr-0" class="form-control descr" name="description[]" placeholder="Product Description"></textarea>
                 </td>
-                <td colspan=5></td>
+                <td colspan="6"></td>
             </tr>
+            <!-- end layout -->
+
+            <!-- fetched rows -->
+            @isset ($po)
+                @php ($i = 0)
+                @foreach ($po->products as $item)
+                    @if ($item->type == 'Stock')
+                        <tr>
+                            <td><input type="text" class="form-control stockname" name="name[]" value="{{ $item->product->name }}" placeholder="Product Name" id='stockname-{{$i}}'></td>
+                            <td><input type="text" class="form-control qty" name="qty[]" value="{{ (int) $item->qty }}" id="qty-{{$i}}"></td>                    
+                            <td><input type="text" class="form-control price" name="rate[]" value="{{ (float) $item->rate }}" id="price-{{$i}}"></td>
+                            <td>
+                                <select class="form-control rowtax" name="itemtax[]" id="rowtax-{{$i}}">
+                                    @foreach ($additionals as $tax)
+                                        <option value="{{ (int) $tax->value }}" {{ $tax->value == $item->itemtax ? 'selected' : ''}}>
+                                            {{ $tax->name }}
+                                        </option>
+                                    @endforeach                                                    
+                                </select>
+                            </td>
+                            <td><input type="text" class="form-control taxable" value="{{ (float) $item->taxrate }}" readonly></td>
+                            <td class="text-center">{{config('currency.symbol')}} <b><span class='amount' id="result-{{$i}}">{{ (float) $item->amount }}</span></b></td>              
+                            <td><button type="button" class="btn btn-danger remove">Remove</button></td>
+                            <input type="hidden" id="stockitemid-{{$i}}" name="item_id[]" value="{{ $item->item_id }}">
+                            <input type="hidden" class="stocktaxr" name="taxrate[]" value="{{ (float) $item->taxrate }}">
+                            <input type="hidden" class="stockamountr" name="amount[]" value="{{ (float) $item->amount }}">
+                            <input type="hidden" class="stockitemprojectid" name="itemproject_id[]" value="0">
+                            <input type="hidden" name="type[]" value="Stock">
+                            <input type="hidden" name="id[]" value="{{ $item->id }}">
+                        </tr>
+                        <tr>
+                            <td colspan=2>
+                                <textarea id="stockdescr-{{$i}}" class="form-control descr" name="description[]" placeholder="Product Description">{{ $item->description }}</textarea>
+                            </td>
+                            <td colspan=5></td>
+                        </tr>
+                        @php ($i++)
+                    @endif
+                @endforeach
+            @endisset
+            <!-- end fetched rows -->
+
             <tr class="bg-white">
                 <td>
                     <button type="button" class="btn btn-success" aria-label="Left Align" id="addstock">
@@ -49,13 +93,13 @@
                 <td colspan="6"></td>
             </tr>
             <tr class="bg-white">
-                <td colspan="5" align="right"><b>{{trans('general.total_tax')}}</b></td>                   
+                <td colspan="6" align="right"><b>{{trans('general.total_tax')}}</b></td>                   
                 <td align="left" colspan="2">
                     {{config('currency.symbol')}} <span id="invtax" class="lightMode">0</span>
                 </td>
             </tr>
             <tr class="bg-white">
-                <td colspan="5" align="right">
+                <td colspan="6" align="right">
                     <b>Inventory Total ({{ config('currency.symbol') }})</b>
                 </td>
                 <td align="left" colspan="2">
