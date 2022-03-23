2ff5233fbc39d370966ec32acd7837dc4140703a
Dohd <alloicebrian@gmail.com>
2022-03-23 17:16:00 +0300

    Update invoice create_payment page

--- resources/views/focus/invoices/create_payment.blade.php
+++ resources/views/focus/invoices/create_payment.blade.php
@@ -16,22 +16,16 @@
                 <div class="card-body">
                     {{ Form::open(['route' => 'biller.invoices.store_payment', 'method' => 'POST', 'id' => 'stoPaymnt']) }}
                         <div class="row mb-1">
-                            <div class="col-4">
-                                <label for="date" class="caption">Date</label>
+                            <div class="col-5">
+                                <label for="customer" class="caption">Search Customer</label>
                                 <div class="input-group">
                                     <div class="input-group-addon"><span class="icon-file-text-o" aria-hidden="true"></span></div>
-                                    {{ Form::date('date', null, ['class' => 'form-control round', 'id' => 'date', 'required']) }}
+                                    <select id="person" name="customer_id" class="form-control select-box" required>
+                                        <option value="">-- Select Customer --</option>
+                                    </select>
                                 </div>
                             </div>
-                            <div class="col-4">
-                                <label for="reference" class="caption">Reference</label>
-                                <div class="input-group">
-                                    <div class="input-group-addon"><span class="icon-file-text-o" aria-hidden="true"></span></div>
-                                    {{ Form::text('tid', 1, ['class' => 'form-control round', 'id' => 'tid', 'disabled']) }}
-                                    <input type="hidden" name="tid" value=1>
-                                </div>
-                            </div>  
-                            <div class="col-4">
+                            <div class="col-3">
                                 <label for="ledgerAccount" class="caption">Ledger Account</label>
                                 <div class="input-group">
                                     <div class="input-group-addon"><span class="icon-file-text-o" aria-hidden="true"></span></div>
@@ -42,23 +36,35 @@
                                         @endforeach
                                     </select>
                                 </div>
-                            </div>                          
+                            </div> 
+                            <div class="col-2">
+                                <label for="date" class="caption">Date</label>
+                                <div class="input-group">
+                                    <div class="input-group-addon"><span class="icon-file-text-o" aria-hidden="true"></span></div>
+                                    {{ Form::text('date', null, ['class' => 'form-control datepicker', 'id' => 'date', 'required']) }}
+                                </div>
+                            </div>
+                            
+                            <div class="col-2">
+                                <label for="tid" class="caption">Transaction ID</label>
+                                <div class="input-group">
+                                    <div class="input-group-addon"><span class="icon-file-text-o" aria-hidden="true"></span></div>
+                                    {{ Form::text('tid', 1, ['class' => 'form-control round', 'id' => 'tid', 'readonly']) }}
+                                    <input type="hidden" name="tid" value=1>
+                                </div>
+                            </div>                                                       
                         </div> 
 
                         <div class="row mb-1">
-                            
-                            <div class="col-4">
-                                <label for="customer" class="caption">Search Customer</label>
+                            <div class="col-2">
+                                <label for="deposit" class="caption">Deposit (Ksh.)</label>
                                 <div class="input-group">
-                                    <div class="input-group-addon"><span class="icon-file-text-o" aria-hidden="true"></span></div>
-                                    <select id="person" name="customer_id" class="form-control select-box" required>
-                                        <option value="">-- Select Customer --</option>
-                                    </select>
+                                    {{ Form::text('deposit', null, ['class' => 'form-control', 'id' => 'deposit', 'required']) }}
                                 </div>
-                            </div>                            
+                            </div>  
                         </div>
 
-                        <table class="table-responsive tfr my_stripe_single" id="invoice">
+                        <table class="table-responsive tfr my_stripe_single" id="invoiceTbl">
                             <thead>
                                 <tr class="item_header bg-gradient-directional-blue white">
                                     <th width="10%" class="text-center">#</th>
@@ -68,26 +74,27 @@
                                     <th width="20%" class="text-center">Deposit (Ksh.)</th>
                                 </tr>
                             </thead>
-                            <tbody></tbody>                
+                            <tbody>
+                                <tr class="bg-white">
+                                    <td colspan="3"></td>
+                                    <td colspan="2">
+                                        <div class="row no-gutters mb-1">
+                                            <div class="col-6 pl-3 pt-1"><b>Total Invoice Amount</b></div>
+                                            <div class="col-6">{{ Form::text('amount_ttl', 0, ['class' => 'form-control', 'id' => 'amount_ttl', 'readonly']) }}</div>
+                                        </div>
+                                        <div class="row no-gutters">
+                                            <div class="col-6 pl-5 pt-1"><b>Total Deposit</b></div>
+                                            <div class="col-6">{{ Form::text('deposit_ttl', 0, ['class' => 'form-control', 'id' => 'deposit_ttl', 'readonly']) }}</div>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>                
                         </table>
 
                         <div class="row mt-1">                            
-                            <div class="col-3 ml-auto"> 
-                                <div class="mb-1">
-                                    <label for="total" class="caption"><b>Total Amount</b></label>                               
-                                    <div class="input-group">                                    
-                                        {{ Form::text('total', 0, ['class' => 'form-control', 'id' => 'total', 'readonly']) }}
-                                    </div> 
-                                </div>
-                                <div>
-                                    <label for="amount" class="caption"><b>Total Deposit</b></label>
-                                    <div class="input-group">
-                                        <div class="input-group-addon"><span class="icon-file-text-o" aria-hidden="true"></span></div>
-                                        {{ Form::text('total_depo', null, ['class' => 'form-control round', 'step' => 2, 'id' => 'totalDepo', 'required']) }}
-                                    </div>
-                                </div>                                
-                                {{ Form::submit('Create Payment', ['class' => 'btn btn-primary btn-lg mt-1']) }}                          
-                            </div>                            
+                            <div class="col-12"> 
+                                {{ Form::submit('Receive Payment', ['class' => 'btn btn-primary btn-lg float-right']) }}                          
+                            </div>
                         </div>
                     {{ Form::close() }}
                 </div>
@@ -102,7 +109,12 @@
 <script>
     $.ajaxSetup({ headers: {'X-CSRF-TOKEN': "{{ csrf_token() }}"} });
 
-    // initialize customer select2
+    $('.datepicker')
+    .datepicker({format: "{{config('core.user_date_format')}}"})
+    .datepicker('setDate', new Date())
+    .change(function () { $(this).datepicker('hide') });
+
+    // On searching customer
     $('#person').select2({
         ajax: {
             url: "{{ route('biller.customers.select') }}",
@@ -125,23 +137,24 @@
         }
     });
 
-    // On adding deposit
-    $('#invoice').on('change', '.depo', function() {
-       return calcTotal();
-    });
-
-    // fetch customer invoices
+    // load customer invoices
     $('#person').change(function() {
         $.ajax({
             url: "{{ route('biller.invoices.client_invoices') }}?client_id=" + $(this).val(),
-            success: function(data) {
-                if (!data.length) return $('#invoice tbody').html('');
-                data.forEach((v, i) => {
-                    $('#invoice tbody').append(invoiceRow(v, i));
+            success: result => {
+                console.log(result)
+                if (!result.length) $('#invoiceTbl tbody tr:not(:eq(-1))').remove();
+                result.forEach((v, i) => {
+                    $('#invoiceTbl tbody tr:eq(-1)').before(invoiceRow(v, i));
                 });
-                calcTotal();
             }
         });
+    });
+
+    // On adding paid values
+    $('#invoiceTbl').on('change', '.paid', function() {
+        const amount = $(this).parents('tr').find('.amount').text().replace(/,/g, '');
+        if (paid > amount) $(this).val((amount*1).toLocaleString());
     });
 
     // invoice rows
@@ -152,27 +165,40 @@
                 <td class="text-center">${i+1}</td>
                 <td>${v.tid}</td>
                 <td class="text-center">${v.notes}</td>
-                <td class="text-center" id="amount-${i}">${total}</td>
-                <td><input type="text" class="form-control depo" name="depo[]" id="depo-${i}" value="0"></td>
+                <td class="text-center amount"><b>${total}</b></td>
+                <td><input type="text" class="form-control paid" name="paid[]"></td>
                 <input type="hidden" name="invoice_id[]" value="${v.id}" id="invoiceid-${i}">
             </tr>
         `;
     }
 
-    function calcTotal() {
-        let total = 0;
-        let paid = 0;
-        $('#invoice tbody tr').each(function() {
-            const i = $(this).index();
-            const amount = $('#amount-'+i).text().replace(/,/g, '');
-            const depo = $('#depo-'+i).val().replace(/,/g, '');
-            $('#depo-'+i).val((depo*1).toLocaleString());
+    // On deposit change
+    $('#deposit').change(function() {
+        const depo = $(this).val().replace(/,/g, '');
+        $(this).val(parseFloat(depo).toLocaleString());
 
-            total += amount*1;
-            paid += depo*1;
+        let init = depo*1;
+        let amountSum = 0;
+        let depoSum = 0;
+        const rows = $('#invoiceTbl tbody tr').length;
+        $('#invoiceTbl tbody tr').each(function() {
+            if ($(this).index() == rows-1) return;
+
+            const amount = $(this).find('.amount').text().replace(/,/g, '');
+            $(this).find('.paid').val(0);
+
+            if (init > amount){
+                $(this).find('.paid').val((amount*1).toLocaleString());
+            } else if (init > 0) $(this).find('.paid').val((init*1).toLocaleString());
+            const paid = $(this).find('.paid').val().replace(/,/g, '');
+            
+            init -= amount;
+            amountSum += amount*1;
+            depoSum += paid*1;
         });
-        $('#total').val(total.toLocaleString());
-        $('#totalDepo').val(paid.toLocaleString());
-    }
+
+        $('#amount_ttl').val(amountSum.toLocaleString());
+        $('#deposit_ttl').val(depoSum.toLocaleString());
+    });
 </script>
 @endsection
