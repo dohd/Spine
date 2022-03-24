b8f945ac2eb2ecfb54484726056d786d8ad2b335
Dohd <alloicebrian@gmail.com>
2022-03-24 10:10:14 +0300

    Create PaidInvoice models

---
+++ app/Models/invoice/PaidInvoice.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Models\invoice;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PaidInvoice extends Model
+{
+    /**
+     * The database table used by the model.
+     * @var string
+     */
+    protected $table = 'paidinvoices';
+
+    /**
+     * Mass Assignable fields of model
+     * @var array
+     */
+    protected $fillable = [];
+
+    /**
+     * Default values for model fields
+     * @var array
+     */
+    protected $attributes = [];
+
+    /**
+     * Dates
+     * @var array
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at'
+    ];
+
+    /**
+     * Guarded fields of model
+     * @var array
+     */
+    protected $guarded = [
+        'id'
+    ];
+
+    /**
+     * Constructor of Model
+     * @param array $attributes
+     */
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::addGlobalScope('ins', function ($builder) {
+            $builder->where('ins', '=', auth()->user()->ins);
+        });
+    }
+}
---
+++ app/Models/items/PaidInvoiceItem.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models\items;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PaidInvoiceItem extends Model
+{
+    protected $table = 'paidinvoice_items';
+
+    /**
+     * Mass Assignable fields of model
+     * @var array
+     */
+    protected $fillable = [];
+
+    /**
+     * Default values for model fields
+     * @var array
+     */
+    protected $attributes = [];
+
+    /**
+     * Dates
+     * @var array
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at'
+    ];
+
+    /**
+     * Guarded fields of model
+     * @var array
+     */
+    protected $guarded = [
+        'id'
+    ];
+
+    /**
+     * Constructor of Model
+     * @param array $attributes
+     */
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+    }
+    protected static function boot()
+    {
+        parent::boot();
+        static::addGlobalScope('ins', function ($builder) {
+            $builder->where('ins', '=', auth()->user()->ins);
+        });
+    }
+}
