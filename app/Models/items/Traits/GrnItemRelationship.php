7ebec630cc413d028c12b4aa279bcb68909977b9
Dohd <alloicebrian@gmail.com>
2022-03-28 12:48:43 +0300

    Rename purchaseorder_product relation method

--- app/Models/items/Traits/GrnItemRelationship.php
+++ app/Models/items/Traits/GrnItemRelationship.php
@@ -6,7 +6,7 @@ use App\Models\items\PurchaseorderItem;
 
 trait GrnItemRelationship
 {
-    public function purchaseorder_product()
+    public function purchaseorder_item()
     {
         return $this->belongsTo(PurchaseorderItem::class, 'poitem_id');
     }
