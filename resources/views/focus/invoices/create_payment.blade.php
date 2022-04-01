f454af53dd43ccf6f0a4883e165f52b948d7ecaf
Dohd <alloicebrian@gmail.com>
2022-04-01 08:12:46 +0300

    Update header title

--- resources/views/focus/invoices/create_payment.blade.php
+++ resources/views/focus/invoices/create_payment.blade.php
@@ -6,7 +6,7 @@
 <div class="content-wrapper">
     <div class="content-header row">
         <div class="content-header-left col-md-6 col-12 mb-2">
-            <h4 class="content-header-title">Invoice Payment Management</h4>
+            <h4 class="content-header-title">Invoice Payment</h4>
         </div>
     </div>
 
