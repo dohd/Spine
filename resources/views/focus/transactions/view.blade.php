a39c1279f5a126dc71215bab3aa8a97c649b37b0
Dohd <alloicebrian@gmail.com>
2022-04-05 08:40:55 +0300

    Update transactions view page

--- resources/views/focus/transactions/view.blade.php
+++ resources/views/focus/transactions/view.blade.php
@@ -17,52 +17,54 @@
         </div>
     </div>
 
-    <div class="content-body">
-        <div class="row">
-            <div class="col-12">
-                <div class="card">
-                    <div class="card-content">
-                        <div class="card-body">
-                            @php
-                                $tr = $transaction;
-                                $tr_types = ['BILL' => 'Bill', 'PMT' => 'Payment', 'INV' => 'Invoice'];
-                                $tr_details = [
-                                    trans('transactions.account_id') => $tr->account['holder'],
-                                    trans('transactions.trans_category_id') => $tr->category['name'],
-                                    'Transaction Type' => $tr_types[$tr->tr_type],
-                                    trans('transactions.debit') => amountFormat($tr['debit']),
-                                    trans('transactions.credit') => amountFormat($tr['credit']),
-                                    trans('transactions.payment_date') => dateFormat($tr['tr_date']),
-                                    trans('general.employee') => $tr->user['first_name'].' '.$tr->user['last_name'],
-                                    trans('general.note') => $tr['note'],                                    
-                                ];
-                            @endphp
-                            @foreach ($tr_details as $key => $value)
-                                <div class="row">
-                                    <div class="col-3 border-blue-grey border-lighten-5  p-1">
-                                        <p>{{ $key }}</p>
-                                    </div>
-                                    <div class="col border-blue-grey border-lighten-5  p-1 font-weight-bold">
-                                        <p>
-                                            @if ($key == 'Transaction Type')
-                                                <a href="#">{{ $value }}</a>
-                                            @else
-                                                {{ $value }} &nbsp;&nbsp;
-                                                @if ($key == trans('transactions.trans_category_id'))
-                                                    <a href="{{ route('biller.print_payslip', [$transaction['id'], 1, 1]) }}" class="btn btn-blue round">
-                                                        <span class="fa fa-print" aria-hidden="true"></span>
-                                                    </a>
-                                                @endif
-                                            @endif                                            
-                                        </p>
-                                    </div>
-                                </div>
-                            @endforeach 
+    <div class="card">
+        <div class="card-content">
+            <div class="card-body">
+                @php
+                    $tr = $transaction;
+                    $tr_types = ['BILL' => 'BILL', 'PMT' => 'PAYMENT', 'INV' => 'INVOICE'];
+                    
+                    $tr_type_urls = [
+                        'BILL' => $tr->bill ? route('biller.bills.show', $tr->bill->id) : '#',
+                        'PAYMENT' => route('biller.show_transaction_payment', $tr->id),
+                        'INVOICE' => $tr->invoice ? route('biller.invoices.show', $tr->invoice->id) : '#',
+                    ];
+                    
+                    $tr_details = [
+                        trans('transactions.account_id') => $tr->account['holder'],
+                        trans('transactions.trans_category_id') => $tr->category['name'],
+                        'Transaction Type' => $tr_types[$tr->tr_type],
+                        trans('transactions.debit') => amountFormat($tr['debit']),
+                        trans('transactions.credit') => amountFormat($tr['credit']),
+                        trans('transactions.payment_date') => dateFormat($tr['tr_date']),
+                        trans('general.employee') => $tr->user['first_name'].' '.$tr->user['last_name'],
+                        trans('general.note') => $tr['note'],                                    
+                    ];
+                @endphp
+                @foreach ($tr_details as $key => $value)
+                    <div class="row">
+                        <div class="col-3 border-blue-grey border-lighten-5  p-1">
+                            <p>{{ $key }}</p>
+                        </div>
+                        <div class="col border-blue-grey border-lighten-5  p-1 font-weight-bold">
+                            <p>
+                                @if ($key == 'Transaction Type')                                                
+                                    <a href="{{ $tr_type_urls[$value] }}">{{ $value }}</a>
+                                @else
+                                    {{ $value }} &nbsp;&nbsp;
+                                    @if ($key == trans('transactions.trans_category_id'))
+                                        <a href="{{ route('biller.print_payslip', [$transaction['id'], 1, 1]) }}" class="btn btn-blue round">
+                                            <span class="fa fa-print" aria-hidden="true"></span>
+                                        </a>
+                                    @endif
+                                @endif                                
+                            </p>
                         </div>
                     </div>
-                </div>
+                @endforeach 
             </div>
         </div>
     </div>
+    
 </div>
 @endsection
