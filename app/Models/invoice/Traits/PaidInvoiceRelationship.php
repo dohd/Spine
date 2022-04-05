c97908394b7bf86c9bd5335bc1abebf58c4c958e
Dohd <alloicebrian@gmail.com>
2022-04-05 08:37:17 +0300

    Add customer relation method

--- app/Models/invoice/Traits/PaidInvoiceRelationship.php
+++ app/Models/invoice/Traits/PaidInvoiceRelationship.php
@@ -2,10 +2,16 @@
 
 namespace App\Models\invoice\Traits;
 
+use App\Models\customer\Customer;
 use App\Models\items\PaidInvoiceItem;
 
 trait PaidInvoiceRelationship
 {
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
     public function items()
     {
         return $this->hasMany(PaidInvoiceItem::class, 'paidinvoice_id');
