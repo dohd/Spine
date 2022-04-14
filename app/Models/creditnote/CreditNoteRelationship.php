9abfa0436084ce5fc9fe1984a46a0b3c961c5fb5
Dohd <alloicebrian@gmail.com>
2022-04-14 09:31:14 +0300

    Add bill and supplier relation methods

--- app/Models/creditnote/CreditNoteRelationship.php
+++ app/Models/creditnote/CreditNoteRelationship.php
@@ -2,11 +2,23 @@
 
 namespace App\Models\creditnote;
 
+use App\Models\bill\Bill;
 use App\Models\customer\Customer;
 use App\Models\invoice\Invoice;
+use App\Models\supplier\Supplier;
 
 trait CreditNoteRelationship
 {
+    public function bill()
+    {
+        return $this->belongsTo(Bill::class);
+    }
+
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class);
+    }
+
     public function invoice()
     {
         return $this->belongsTo(Invoice::class);
