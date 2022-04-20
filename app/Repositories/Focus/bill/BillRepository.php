49d52ac6158e4eafa9467c26e5c124128dd94b91
Dohd <alloicebrian@gmail.com>
2022-04-20 12:56:15 +0300

    Update payment status block

--- app/Repositories/Focus/bill/BillRepository.php
+++ app/Repositories/Focus/bill/BillRepository.php
@@ -60,18 +60,11 @@ class BillRepository extends BaseRepository
 
         $bill_items = $input['bill_items'];
         foreach ($bill_items as $k => $item) {
-            $item = $item + ['paidbill_id' => $result->id];
+            $item['paidbill_id'] = $result->id;
             $item['paid'] = numberClean($item['paid']);
             $bill_items[$k] = $item;
         }
         PaidbillItem::insert($bill_items);
-
-        // update payment status in bills
-        foreach ($result->items as $item) {
-            $payable = $item->bill->grandttl;
-            if ($item->paid < $payable) $item->bill->update(['status' => 'Partial']);
-            if ($item->paid == $payable) $item->bill->update(['status' => 'Paid']);    
-        }
 
         // update paid amount in bills
         $bill_ids = $result->items()->pluck('bill_id')->toArray();
@@ -80,8 +73,25 @@ class BillRepository extends BaseRepository
             ->groupBy('bill_id')
             ->get()->toArray();
         Batch::update(new Bill, $paid_bills, 'id');
+        
+        // update payment status in bills
+        foreach ($result->items as $item) {            
+            $bill = $item->bill;
+            if ($bill->grandttl == $bill->amountpaid) $bill->update(['status' => 'paid']);  
+            if ($bill->grandttl > $bill->amountpaid) $bill->update(['status' => 'partial']);
+        }
 
-        /** accounting */
+        /** accounts */
+        $this->post_transaction($result);
+
+        DB::commit();
+        if ($result) return true;
+
+        throw new GeneralException(trans('exceptions.backend.purchaseorders.create_error'));
+    }
+
+    public function post_transaction($bill)
+    {
         // credit supplier
         $tr_category = Transactioncategory::where('code', 'PMT')->first(['id', 'code']);
         $cr_data = [
@@ -93,7 +103,7 @@ class BillRepository extends BaseRepository
             'user_id' => $bill['user_id'],
             'ins' => $bill['ins'],
             'tr_type' => $tr_category->code,
-            'tr_ref' => $result['id'],
+            'tr_ref' => $bill['id'],
             'user_type' => 'supplier',
             'is_primary' => 1,
             'note' => $bill['doc_ref_type'] . ' - ' . $bill['doc_ref'],
@@ -110,37 +120,6 @@ class BillRepository extends BaseRepository
         Transaction::create($dr_data);
 
         // update account ledgers debit and credit totals
-        aggregate_account_transactions();
-
-        DB::commit();
-        if ($result) return true;
-
-        throw new GeneralException(trans('exceptions.backend.purchaseorders.create_error'));
-    }
-
-    /**
-     * For updating the respective Model in storage
-     *
-     * @param Bill $bill
-     * @param  $input
-     * @throws GeneralException
-     * return bool
-     */
-    public function update(Purchase $purchase, array $input)
-    {
-        // 
-        throw new GeneralException(trans('exceptions.backend.purchaseorders.update_error'));
-    }
-
-    /**
-     * For deleting the respective model from storage
-     *
-     * @param Bill $bill
-     * @throws GeneralException
-     * @return bool
-     */
-    public function delete($bill)
-    {
-        // 
+        aggregate_account_transactions();    
     }
 }
