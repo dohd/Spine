3c3ce3d8144acbdc190f7684c87b8fc230098231
Dohd <alloicebrian@gmail.com>
2022-04-20 13:00:51 +0300

    Update bills pay button

--- resources/views/focus/bills/partials/bills-header-buttons.blade.php
+++ resources/views/focus/bills/partials/bills-header-buttons.blade.php
@@ -1,8 +1,8 @@
 <div class="btn-group">
+    <a href="{{ route('biller.bills.create') }}" class="btn btn-purple  btn-lighten-3">
+        <i class="fa fa-money"></i> Pay
+    </a>&nbsp;
     <a href="{{ route('biller.bills.index') }}" class="btn btn-info  btn-lighten-2">
         <i class="fa fa-list-alt"></i> {{ trans('general.list') }}
-    </a>
-    <a href="{{ route('biller.bills.create') }}" class="btn btn-pink  btn-lighten-3">
-        <i class="fa fa-money"></i> Payment
     </a>
 </div>
