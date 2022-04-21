cd4d1892aafee5b2e0bbd4c65bd8a1457e861964
Dohd <alloicebrian@gmail.com>
2022-04-21 13:32:13 +0300

    Update journals form

--- resources/views/focus/journals/form.blade.php
+++ resources/views/focus/journals/form.blade.php
@@ -25,25 +25,30 @@
         </thead>
         <tbody>
             <tr>
-                <td>
-                    <select name="account_id[]" id="account-0" class="form-control account">
-                        <option value="1">KCB Loan - Expense</option>
-                    </select>
-                </td>
-                <td><input type="text" name="debit[]" value="20,000" class="form-control"></td>
-                <td><input type="text" name="credit[]" value="20,000" class="form-control"></td>
-                <td><button type="button" class="btn btn-danger">Remove</button></td>
+                <td><select name="account_id[]" id="account-0" class="form-control account" data-placeholder="Search Ledger"></select></td>
+                <td><input type="text" class="form-control debit" name="debit[]" placeholder="0.00" id="debit-0"></td>
+                <td><input type="text" class="form-control credit" name="credit[]" placeholder="0.00" id="credit-0"></td>
+                <td><button type="button" class="btn btn-danger d-none remove">Remove</button></td>
             </tr>
         </tbody>
     </table>
 </div>
-
 <div class="form-group row">
     <div class="col-2 ml-2">
-        <button type="button" class="btn btn-success">Add Ledger</button>
+        <button type="button" class="btn btn-success" id="addLedger">Add Ledger</button>
     </div>
-    <div class="col-2 ml-auto mr-5">
-        {{ Form::submit('Create Journal', ['class' => 'btn btn-primary btn-lg block round mt-5']) }}
+</div>
+<div class="row">
+    <div class="form-inline col-3 ml-auto">
+        <label for="debit_total">Debit Total</label>
+        <input type="text" class="form-control ml-2 mb-1" name="debit_ttl" id="debitTtl" readonly>
+        <label for="debit_total">Credit Total</label>
+        <input type="text" class="form-control ml-2" name="credit_ttl"  id="creditTtl" readonly>
+    </div>
+</div>
+<div class="form-group row">
+    <div class="col-2 ml-auto mr-2">
+        {{ Form::submit('Create Journal', ['class' => 'btn btn-primary btn-lg block round mt-3']) }}
     </div>
 </div>
 
@@ -51,12 +56,79 @@
 {{ Html::script('focus/js/select2.min.js') }}
 <script type="text/javascript">
     $('.datepicker')
-    .datepicker({
-        autoHide: true,
-        format: "{{ config('core.user_date_format') }}"
-    })
+    .datepicker({format: "{{ config('core.user_date_format') }}", autoHide: true,})
     .datepicker('setDate', new Date());
 
-    $('.account').select2();
+    // fetch unselected manual ledgers
+    const accountIds = [];
+    function select2Config() {
+        return {
+            ajax: {
+                url: "{{ route('biller.journals.journal_ledgers') }}",
+                quietMillis: 50,
+                processResults: data => {
+                    const results = data
+                        .map(v => ({id: v.id, text: v.holder + ' - ' + v.account_type.category}))
+                        .filter(v => !accountIds.includes(v.id));
+                    return {results}; 
+                },
+            }
+        }
+    };
+
+    // on selecting account ledger update accountIds
+    $('#ledgerTbl').on('change', '.account', function() {
+        accountIds.push($(this).val()*1);
+    });
+
+    // on change debit or credit 
+    $('#ledgerTbl').on('change', '.debit, .credit', function() {
+        const credit = $(this).parents('tr').find('.credit');
+        const debit = $(this).parents('tr').find('.debit');
+        calcTotals();
+
+        if ($(this).is('.debit') && debit.val()) {
+            return credit.val(0).attr('readonly', true);
+        } else if ($(this).is('.credit') && credit.val()) {
+            return debit.val(0).attr('readonly', true);
+        }
+        debit.val('').attr('readonly', false);
+        credit.val('').attr('readonly', false);
+    });
+
+    // remove button
+    $('#ledgerTbl').on('click', '.remove', function() {
+        const row = $(this).parents('tr');
+        const id = row.find('.account').val()*1;
+        if (accountIds.includes(id)) 
+            accountIds.splice(accountIds.indexOf(id), 1);
+        row.remove();
+        calcTotals();
+    });
+
+    // click add ledger button
+    let rowId = 0;
+    const rowHtml = $('#ledgerTbl tbody tr:first').html();
+    $('#account-0').select2(select2Config());
+    $('#addLedger').click(function() {
+        rowId++;
+        const html = rowHtml.replace(/-0/g, '-'+rowId).replace(/d-none/, '');
+        $('#ledgerTbl tbody').append('<tr>'+html+'</tr>');
+        $('#account-'+rowId).select2(select2Config());
+    });
+
+    // totals
+    function calcTotals() {
+        let debitTtl = 0;
+        let creditTtl = 0;
+        $('#ledgerTbl tbody tr').each(function() {
+            const credit = $(this).find('.credit').val().replace(/,/g, '');
+            const debit = $(this).find('.debit').val().replace(/,/g, '');
+            if (credit) creditTtl += credit * 1;
+            if (debit) debitTtl += debit * 1;
+        });
+        $('#debitTtl').val(parseFloat(debitTtl.toFixed(2)).toLocaleString());
+        $('#creditTtl').val(parseFloat(creditTtl.toFixed(2)).toLocaleString());
+    }
 </script>
 @endsection
