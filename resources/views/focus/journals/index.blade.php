46e229e30cb965fb5160f0f46963ce19f73184a4
Dohd <alloicebrian@gmail.com>
2022-04-21 16:38:56 +0300

    Update journal index page

--- resources/views/focus/journals/index.blade.php
+++ resources/views/focus/journals/index.blade.php
@@ -1,1 +1,140 @@
-index page
+@extends ('core.layouts.app')
+
+@section ('title',  'Journals Management')
+
+@section('content')
+<div class="content-wrapper">
+    <div class="content-header row mb-1">
+        <div class="content-header-left col-6">
+            <h4 class="content-header-title">Journals Management</h4>
+        </div>
+        <div class="content-header-right col-6">
+            <div class="media width-250 float-right">
+                <div class="media-body media-right text-right">
+                    @include('focus.journals.partials.journals-header-buttons')
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="content-body">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-content">
+                        <div class="card-body">
+                            <table id="journalsTbl" class="table table-striped table-bordered zero-configuration" cellspacing="0" width="100%">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>#Journal ID</th>
+                                        <th>Date</th>
+                                        <th>Note</th>
+                                        <th>Debit</th>
+                                        <th>Credit</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td colspan="100%" class="text-center text-success font-large-1">
+                                            <i class="fa fa-spinner spinner"></i>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('after-scripts')
+{{ Html::script(mix('js/dataTable.js')) }}
+<script>
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': "{{ csrf_token() }}"
+        }
+    });
+
+    const language = {
+        @lang('datatable.strings')
+    };
+    const dataTable = $('#journalsTbl').dataTable({
+        processing: true,
+        serverSide: true,
+        responsive: true,
+        language,
+        ajax: {
+            url: '{{ route("biller.journals.get") }}',
+            type: 'post'
+        },
+        columns: [{
+                data: 'DT_Row_Index',
+                name: 'id'
+            },
+            {
+                data: 'tid',
+                name: 'tid'
+            },
+            {
+                data: 'date',
+                name: 'date'
+            },
+            {
+                data: 'note',
+                name: 'note'
+            },
+            {
+                data: 'debit_ttl',
+                name: 'debit_ttl'
+            },
+            {
+                data: 'credit_ttl',
+                name: 'credit_ttl'
+            },
+            {
+                data: 'actions',
+                name: 'actions',
+                searchable: false,
+                sortable: false
+            },            
+        ],
+        order: [
+            [0, "desc"]
+        ],
+        searchDelay: 500,
+        dom: 'Blfrtip',
+        buttons: {
+            buttons: [
+
+                {
+                    extend: 'csv',
+                    footer: true,
+                    exportOptions: {
+                        columns: [0, 1]
+                    }
+                },
+                {
+                    extend: 'excel',
+                    footer: true,
+                    exportOptions: {
+                        columns: [0, 1]
+                    }
+                },
+                {
+                    extend: 'print',
+                    footer: true,
+                    exportOptions: {
+                        columns: [0, 1]
+                    }
+                }
+            ]
+        }
+    });
+</script>
+@endsection
