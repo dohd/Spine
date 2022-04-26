abe95c919b22eb643c55554dc8e21b18d306f706
Dohd <alloicebrian@gmail.com>
2022-04-26 12:16:42 +0300

    Update accounts view page

--- resources/views/focus/accounts/view.blade.php
+++ resources/views/focus/accounts/view.blade.php
@@ -1,14 +1,14 @@
 @extends ('core.layouts.app')
 
-@section ('title', trans('labels.backend.accounts.management') . ' | ' . trans('labels.backend.accounts.create'))
+@section ('title', 'Acounts Management')
 
 @section('content')
 <div class="content-wrapper">
-    <div class="content-header row">
-        <div class="content-header-left col-md-6 col-12 mb-2">
-            <h3 class="mb-0">{{ trans('labels.backend.accounts.view') }}</h3>
+    <div class="content-header row mb-1">
+        <div class="content-header-left col-6">
+            <h3 class="content-header-title">Acounts Management</h3>
         </div>
-        <div class="content-header-right col-md-6 col-12">
+        <div class="content-header-right col-6">
             <div class="media width-250 float-right">
                 <div class="media-body media-right text-right">
                     @include('focus.accounts.partials.accounts-header-buttons')
@@ -33,23 +33,25 @@
                                     trans('accounts.note') => $account['note'],
                                 ];
                             @endphp
-                            @foreach ($account_details as $key => $value)
-                                <div class="row">
-                                    <div class="col-3 border-blue-grey border-lighten-5  p-1">
-                                        <p>{{ $key }} </p>
+                            <div class="ml-1 mr-1">
+                                @foreach ($account_details as $key => $value)
+                                    <div class="row">
+                                        <div class="col-3 border-blue-grey border-lighten-5 font-weight-bold p-1">
+                                            <p>{{ $key }} </p>
+                                        </div>
+                                        <div class="col border-blue-grey border-lighten-5  p-1">
+                                            <p> 
+                                                {{ $value }} &nbsp;&nbsp;
+                                                @if ($key == trans('accounts.number'))
+                                                    <a class="btn btn-purple round" href="{{ route('biller.transactions.index') }}?rel_type=9&rel_id={{ $account['id'] }}" title="List">
+                                                        <i class="fa fa-list"></i>
+                                                    </a>
+                                                @endif
+                                            </p>
+                                        </div>
                                     </div>
-                                    <div class="col border-blue-grey border-lighten-5  p-1 font-weight-bold">
-                                        <p> 
-                                            {{ $value }} &nbsp;&nbsp;
-                                            @if ($key == trans('accounts.number'))
-                                                <a class="btn btn-purple round" href="{{ route('biller.transactions.index') }}?rel_type=9&rel_id={{ $account['id'] }}" title="List">
-                                                    <i class="fa fa-list"></i>
-                                                </a>
-                                            @endif
-                                        </p>
-                                    </div>
-                                </div>
-                            @endforeach                         
+                                @endforeach    
+                            </div>                                                 
                         </div>
                     </div>
                 </div>
