251b2c7228b7811bacddbcf6a9a9bd1f64ea0848
Dohd <alloicebrian@gmail.com>
2022-04-26 12:20:17 +0300

    Update transactions view page

--- resources/views/focus/transactions/view.blade.php
+++ resources/views/focus/transactions/view.blade.php
@@ -1,14 +1,14 @@
 @extends ('core.layouts.app')
 
-@section ('title', trans('labels.backend.transactions.management') . ' | ' . trans('labels.backend.transactions.create'))
+@section ('title', 'Transactions Management')
 
 @section('content')
 <div class="content-wrapper">
-    <div class="content-header row">
-        <div class="content-header-left col-md-6 col-12 mb-2">
-            <h3 class="content-header-title mb-0">{{ trans('labels.backend.transactions.view') }}</h3>
+    <div class="content-header row mb-1">
+        <div class="content-header-left col-6">
+            <h3 class="content-header-title">Transactions Management</h3>
         </div>
-        <div class="content-header-right col-md-6 col-12">
+        <div class="content-header-right col-6">
             <div class="media width-250 float-right">
                 <div class="media-body media-right text-right">
                     @include('focus.transactions.partials.transactions-header-buttons')
@@ -23,13 +23,11 @@
                 @php
                     $tr = $transaction;
                     $tr_types = ['BILL' => 'BILL', 'PMT' => 'PAYMENT', 'INV' => 'INVOICE'];
-                    
                     $tr_type_urls = [
                         'BILL' => $tr->bill ? route('biller.bills.show', $tr->bill->id) : '#',
                         'PAYMENT' => route('biller.show_transaction_payment', $tr->id),
                         'INVOICE' => $tr->invoice ? route('biller.invoices.show', $tr->invoice->id) : '#',
                     ];
-                    
                     $tr_details = [
                         trans('transactions.account_id') => $tr->account['holder'],
                         trans('transactions.trans_category_id') => $tr->category['name'],
@@ -41,30 +39,31 @@
                         trans('general.note') => $tr['note'],                                    
                     ];
                 @endphp
-                @foreach ($tr_details as $key => $value)
-                    <div class="row">
-                        <div class="col-3 border-blue-grey border-lighten-5  p-1">
-                            <p>{{ $key }}</p>
+                <div class="mr-1 ml-1">
+                    @foreach ($tr_details as $key => $value)
+                        <div class="row">
+                            <div class="col-3 border-blue-grey border-lighten-5  font-weight-bold p-1">
+                                <p>{{ $key }}</p>
+                            </div>
+                            <div class="col border-blue-grey border-lighten-5  p-1">
+                                <p>
+                                    @if ($key == 'Transaction Type')                                                
+                                        <a href="{{ $tr_type_urls[$value] }}">{{ $value }}</a>
+                                    @else
+                                        {{ $value }} &nbsp;&nbsp;
+                                        @if ($key == trans('transactions.trans_category_id'))
+                                            <a href="{{ route('biller.print_payslip', [$transaction['id'], 1, 1]) }}" class="btn btn-blue round">
+                                                <span class="fa fa-print" aria-hidden="true"></span>
+                                            </a>
+                                        @endif
+                                    @endif                                
+                                </p>
+                            </div>
                         </div>
-                        <div class="col border-blue-grey border-lighten-5  p-1 font-weight-bold">
-                            <p>
-                                @if ($key == 'Transaction Type')                                                
-                                    <a href="{{ $tr_type_urls[$value] }}">{{ $value }}</a>
-                                @else
-                                    {{ $value }} &nbsp;&nbsp;
-                                    @if ($key == trans('transactions.trans_category_id'))
-                                        <a href="{{ route('biller.print_payslip', [$transaction['id'], 1, 1]) }}" class="btn btn-blue round">
-                                            <span class="fa fa-print" aria-hidden="true"></span>
-                                        </a>
-                                    @endif
-                                @endif                                
-                            </p>
-                        </div>
-                    </div>
-                @endforeach 
+                    @endforeach 
+                </div>
             </div>
         </div>
     </div>
-    
 </div>
 @endsection
