e4f49b397baf15f06b8a3222e7185d960d3fefa1
Dohd <alloicebrian@gmail.com>
2022-04-26 15:08:43 +0300

    Update delete method

--- app/Http/Controllers/Focus/transaction/TransactionsController.php
+++ app/Http/Controllers/Focus/transaction/TransactionsController.php
@@ -23,12 +23,10 @@ use App\Models\customer\Customer;
 use App\Models\hrm\Hrm;
 use App\Models\supplier\Supplier;
 use App\Models\transaction\Transaction;
-use App\Models\transactioncategory\Transactioncategory;
 use App\Http\Controllers\Controller;
 use App\Http\Responses\RedirectResponse;
 use App\Http\Responses\ViewResponse;
 use App\Http\Responses\Focus\transaction\CreateResponse;
-use App\Http\Responses\Focus\transaction\EditResponse;
 use App\Repositories\Focus\transaction\TransactionRepository;
 use App\Http\Requests\Focus\transaction\ManageTransactionRequest;
 
@@ -64,46 +62,44 @@ class TransactionsController extends Controller
      */
     public function index(ManageTransactionRequest $request)
     {
-        $input = $request->only('rel_type', 'rel_id');
+        // extract request fields
+        $rel_type = $request->rel_type;
+        $rel_id = $request->rel_id;
 
+        $input = compact('rel_type', 'rel_id');
         $segment = (object) array();
         $words = array();
-        switch ($input['rel_type']) {
-            case 0:
-                $segment = Transactioncategory::find($input['rel_id']);
-                $words['name'] = trans('transactioncategories.transactioncategory');
-                $words['name_data'] = $segment->name;
-                break;
+        switch ($rel_type) {
             case 1:
-                $segment = Customer::find($input['rel_id']);
+                $segment = Customer::find($rel_id);
                 $words['name'] = trans('customers.title');
                 $words['name_data'] = $segment->name;
                 $words['url'] = '<a href="' . route('biller.customers.show', [$segment['id']]) . '">
                     <i class="fa fa-user"></i> ' . $segment['name'] . ' </a>';
                 break;
             case 2:
-                $segment = Hrm::find($input['rel_id']);
+                $segment = Hrm::find($rel_id);
                 $words['name'] = trans('hrms.employee');
                 $words['name_data'] = $segment->first_name . ' ' . $segment->last_name;
                 $words['url'] = '<a href="' . route('biller.hrms.show', [$segment['id']]) . '">
                     <i class="fa fa-user"></i> ' . $words['name_data'] . ' </a>';
                 break;
             case 3:
-                $segment = Hrm::find($input['rel_id']);
+                $segment = Hrm::find($rel_id);
                 $words['name'] = trans('hrms.employee');
                 $words['name_data'] = $segment->first_name . ' ' . $segment->last_name;
                 $words['url'] = '<a href="' . route('biller.hrms.show', [$segment['id']]) . '">
                     <i class="fa fa-user"></i> ' . $words['name_data'] . ' </a>';
                 break;
             case 4:
-                $segment = Supplier::find($input['rel_id']);
+                $segment = Supplier::find($rel_id);
                 $words['name'] = trans('customers.title');
                 $words['name_data'] = $segment->name;
                 $words['url'] = '<a href="' . route('biller.customers.show', [$segment['id']]) . '">
                     <i class="fa fa-user"></i> ' . $segment['name'] . ' </a>';
                 break;
             case 9:
-                $segment = Account::find($input['rel_id']);
+                $segment = Account::find($rel_id);
                 $words['name'] = trans('accounts.holder');
                 $words['name_data'] = $segment->holder;
                 break;
@@ -124,70 +120,20 @@ class TransactionsController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param StoreTransactionRequestNamespace $request
-     * @return \App\Http\Responses\RedirectResponse
-     */
-    public function store(StoreTransactionRequest $request)
-    {
-
-        $invoice = $request->only(['tid', 'transaction_date', 'account_id', 'debit', 'credit', 'note']);
-        $invoice['user_id'] = auth()->user()->id;
-        $invoice['ins'] = auth()->user()->ins;
-
-        $result = $this->repository->create(compact('invoice'));
-
-        $msg = trans('alerts.backend.transactions.created') . '<a href="' . route('biller.transactions.create') . '" class="btn btn-outline-light round btn-min-width bg-purple">
-            <span class="fa fa-plus-circle" aria-hidden="true"></span> ' . trans('general.create') . '  </a>&nbsp; &nbsp;' 
-            . ' <a href="' . route('biller.transactions.index') . '" class="btn btn-outline-blue round btn-min-width bg-amber">
-            <span class="fa fa-list blue" aria-hidden="true"></span> <span class="blue">' . trans('general.list') . '</span> </a>';
-
-        return response()->json(['status' => 'Success', 'message' => $msg]);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param App\Models\transaction\Transaction $transaction
-     * @param EditTransactionRequestNamespace $request
-     * @return \App\Http\Responses\Focus\transaction\EditResponse
-     */
-    public function edit(Transaction $transaction, StoreTransactionRequest $request)
-    {
-        return new EditResponse($transaction);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param UpdateTransactionRequestNamespace $request
-     * @param App\Models\transaction\Transaction $transaction
-     * @return \App\Http\Responses\RedirectResponse
-     */
-    public function update(StoreTransactionRequest $request, Transaction $transaction)
-    {
-        //Input received from the request
-        $input = $request->except(['_token', 'ins']);
-        //Update the model using repository update method
-        $this->repository->update($transaction, $input);
-        //return with successfull message
-        return new RedirectResponse(route('biller.transactions.index'), ['flash_success' => trans('alerts.backend.transactions.updated')]);
-    }
-
-    /**
      * Remove the specified resource from storage.
      *
      * @param DeleteTransactionRequestNamespace $request
      * @param App\Models\transaction\Transaction $transaction
      * @return \App\Http\Responses\RedirectResponse
      */
-    public function destroy(Transaction $transaction, StoreTransactionRequest $request)
+    public function destroy(Transaction $transaction)
     {
-        //Calling the delete method on repository
-        $this->repository->delete($transaction);
-        //returning with successfull message
-        return new RedirectResponse(route('biller.transactions.index'), ['flash_success' => trans('alerts.backend.transactions.deleted')]);
+        $result = $this->repository->delete($transaction);
+
+        $res = ['flash_success' => 'Transaction deleted successfully'];
+        if ($result) $res = ['flash_error' => 'Reconciled transaction cannot be deleted'];
+
+        return redirect()->back()->with($res);
     }
 
     /**
