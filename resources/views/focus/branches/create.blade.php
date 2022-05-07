7e8b4443667a1d5b5da476c1f34ce9b63e2c2e61
Dohd <alloicebrian@gmail.com>
2022-05-07 11:43:54 +0300

    Update branch create page

--- resources/views/focus/branches/create.blade.php
+++ resources/views/focus/branches/create.blade.php
@@ -3,37 +3,32 @@
 @section ('title', 'Branch Management | Branch Create')
 
 @section('content')
-<div class="">
-    <div class="content-wrapper">
-        <div class="content-header row">
-            <div class="content-header-left col-md-6 col-12 mb-2">
-                <h4>Create Branch</h4>
-            </div>
-            <div class="content-header-right col-md-6 col-12">
-                <div class="media width-250 float-right">
-                    <div class="media-body media-right text-right">
-                        @include('focus.branches.partials.branches-header-buttons')
-                    </div>
+<div class="content-wrapper">
+    <div class="content-header row mb-1">
+        <div class="content-header-left col-6">
+            <h4>Create Branch</h4>
+        </div>
+        <div class="content-header-right col-6">
+            <div class="media width-250 float-right">
+                <div class="media-body media-right text-right">
+                    @include('focus.branches.partials.branches-header-buttons')
                 </div>
             </div>
         </div>
-        <div class="content-body">
-            <div class="row">
-                <div class="col-12">
-                    <div class="card">
-                        <div class="card-content">
-                            <div class="card-body">
-                                {{ Form::open(['route' => 'biller.branches.store', 'class' => 'form-horizontal', 'role' => 'form', 'method' => 'post', 'id' => 'create-productcategory']) }}
-                                <div class="form-group">
-                                    {{-- Including Form blade file --}}
-                                    @include("focus.branches.form")
-                                    <div class="edit-form-btn">
-                                        {{ link_to_route('biller.branches.index', trans('buttons.general.cancel'), [], ['class' => 'btn btn-danger btn-md']) }}
-                                        {{ Form::submit(trans('buttons.general.crud.create'), ['class' => 'btn btn-primary btn-md']) }}
-                                    </div>
-                                </div>
-                                {{ Form::close() }}
-                            </div>
+    </div>
+    <div class="content-body">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-content">
+                        <div class="card-body">
+                            {{ Form::open(['route' => 'biller.branches.store', 'class' => 'form-horizontal', 'role' => 'form', 'method' => 'post', 'id' => 'create-productcategory']) }}
+                                @include("focus.branches.form")
+                                <div class="edit-form-btn ml-2">
+                                    {{ link_to_route('biller.branches.index', trans('buttons.general.cancel'), [], ['class' => 'btn btn-danger btn-md']) }}
+                                    {{ Form::submit(trans('buttons.general.crud.create'), ['class' => 'btn btn-primary btn-md']) }}
+                                </div>                            
+                            {{ Form::close() }}
                         </div>
                     </div>
                 </div>
