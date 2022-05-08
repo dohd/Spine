10dceb66bbb0b4647e456fa2a3161177eb1d9285
Dohd <alloicebrian@gmail.com>
2022-05-08 08:09:10 +0300

    Update transaction view table

--- resources/views/focus/transactions/view.blade.php
+++ resources/views/focus/transactions/view.blade.php
@@ -21,14 +21,14 @@
         'STOCK' => $tr->issuance ? route('biller.issuance.show', $tr->issuance->id) : '#',
     ];
     $tr_details = [
-        trans('transactions.account_id') => $tr->account['holder'],
-        trans('transactions.trans_category_id') => $tr->category['name'],
-        'Transaction Type' => $tr_types[$tr->tr_type],
-        trans('transactions.debit') => amountFormat($tr['debit']),
-        trans('transactions.credit') => amountFormat($tr['credit']),
-        'Transaction Date' => dateFormat($tr['tr_date']),
-        trans('general.employee') => $tr->user['first_name'].' '.$tr->user['last_name'],
-        trans('general.note') => $tr['note'],                                    
+        'Account' => $tr->account->holder,
+        'Category' => $tr->category->name,
+        'Type' => $tr_types[$tr->tr_type],
+        'Debit' => amountFormat($tr['debit']),
+        'Credit' => amountFormat($tr['credit']),
+        'Date' => dateFormat($tr['tr_date']),
+        trans('general.employee') => $tr->user->first_name . ' ' . $tr->user->last_name,
+        trans('general.note') => $tr->note,                                    
     ];
 @endphp
 
@@ -49,33 +49,31 @@
 
     <div class="card">
         <div class="card-content">
-            <div class="card-body">
+            <div class="card-header">
                 <a href="javascript:" class="btn btn-warning btn-sm" data-toggle="modal" data-target="#editTrModal">
                     <i class="fa fa-pencil"></i> Edit
                 </a>
-                <div class="m-1">
+            </div>
+            <div class="card-body">
+                <table class="table table-bordered table-sm">
                     @foreach ($tr_details as $key => $value)
-                        <div class="row">
-                            <div class="col-3 border-blue-grey border-lighten-5  font-weight-bold p-1">
-                                <p>{{ $key }}</p>
-                            </div>
-                            <div class="col border-blue-grey border-lighten-5  p-1">
-                                <p>
-                                    @if ($key == 'Transaction Type')                                                
-                                        <a href="{{ $tr_type_urls[$value] }}">{{ $value }}</a>
-                                    @else
-                                        {{ $value }} &nbsp;&nbsp;
-                                        @if ($key == trans('transactions.trans_category_id'))
-                                            <a href="{{ route('biller.print_payslip', [$transaction['id'], 1, 1]) }}" class="btn btn-blue round">
-                                                <span class="fa fa-print" aria-hidden="true"></span>
-                                            </a>
-                                        @endif
-                                    @endif                                
-                                </p>
-                            </div>
-                        </div>
-                    @endforeach 
-                </div>
+                        <tr>
+                            <th>{{ $key }}</th>
+                            <td>
+                                @if ($key == 'Type')                                                
+                                    <a href="{{ $tr_type_urls[$value] }}">{{ $value }}</a>
+                                @else
+                                    {{ $value }} &nbsp;&nbsp;
+                                    @if ($key == 'Category')
+                                        <a href="{{ route('biller.print_payslip', [$transaction['id'], 1, 1]) }}" class="btn btn-blue round">
+                                            <span class="fa fa-print" aria-hidden="true"></span>
+                                        </a>
+                                    @endif
+                                @endif 
+                            </td>
+                        </tr>
+                    @endforeach
+                </table>
             </div>
         </div>
     </div>
@@ -88,12 +86,12 @@
 <script type="text/javascript">
     $.ajaxSetup({headers: { 'X-CSRF-TOKEN': "{{ csrf_token() }}" }});
 
+    // on account search
     $('#account').select2({
         dropdownParent: $('#editTrModal'),
         ajax: {
             url: "{{ route('biller.transactions.account_search') }}",
             dataType: 'json',
-            // type: 'POST',
             quietMillis: 50,
             data: ({term}) => ({keyword: term}),
             processResults: data => {
