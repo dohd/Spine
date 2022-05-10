2221cca415ece9f1228116507c3890259253abd5
Dohd <alloicebrian@gmail.com>
2022-05-10 13:22:13 +0300

    Remove obsolete comments

--- resources/views/focus/projects/modal/project_view.blade.php
+++ resources/views/focus/projects/modal/project_view.blade.php
@@ -4,7 +4,9 @@
             <section class="todo-form">
                 <form id="data_form_task_view" class="todo-input">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="viewModalLabel">{{trans('projects.project')}} : <b><span id="p_name"></span></b></h5>
+                        <h5 class="modal-title" id="viewModalLabel">
+                            {{ trans('projects.project') }} : <b><span id="p_name"></span></b>
+                        </h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
@@ -12,32 +14,20 @@
                     <div class="modal-body">
                         <div class="row">
                             <div class="col-md-6">{{trans('projects.start_date')}} : <span class="font-weight-bold" id="p_start"></span></div>
-                            {{-- hide project end_date 
-                            <div class="col-md-3">{{trans('projects.end_date')}} <span class="font-weight-bold" id="p_end"></span></div>
-                            --}}
                             <div class="col-md-3"> <span class="font-weight-bold" id="p_status"></span></div>
                             <div class="col-md-3">
                                 <select class="custom-select" id="p_status_list" name="status" onchange="update_status(this.value)"></select>
                             </div>
                         </div>
-                        {{-- Hide owners and users
-                        <hr>
-                        <div class="row">
-                            <div class="col-md-6">{{trans('projects.owner')}} <span class="font-weight-bold" id="p_creator"></span></div>
-                            <div class="col-md-6">{{trans('projects.users')}} <span class="font-weight-bold" id="p_assigned"></span></div>
-                        </div>
-                        <hr>                        
-                        <hr>
-                        --}}
+                        
                         <p class="info" id="ps_description"></p>
                         <hr>
                         <p id="p_description"></p>
                     </div>
                     <div class="modal-footer">
                         <fieldset class="form-group position-relative has-icon-left mb-0">
-                            {{-- <a href="" class="btn btn-blue" id="d_view">{{trans('projects.detailed_view')}}</a> --}}
+                            <button type="button" data-dismiss="modal" class="btn btn-danger">{{trans('general.close')}}</button>
                             <a href="#" class="btn btn-blue">{{trans('projects.detailed_view')}}</a>
-                            <button type="button" data-dismiss="modal" class="btn btn-default">{{trans('general.close')}}</button>
                         </fieldset>
                     </div>
                     <input type="hidden" value="" id="p_id">
@@ -46,6 +36,7 @@
         </div>
     </div>
 </div>
+
 <script type="text/javascript">
     function update_status(did) {
         $.ajax({
