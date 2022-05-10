11041dae54fa2732d096219cdf9c9ff05b97dec4
Dohd <alloicebrian@gmail.com>
2022-05-10 18:19:11 +0300

    Update post_transaction method

--- app/Repositories/Focus/purchase/PurchaseRepository.php
+++ app/Repositories/Focus/purchase/PurchaseRepository.php
@@ -85,6 +85,97 @@ class PurchaseRepository extends BaseRepository
         throw new GeneralException(trans('exceptions.backend.purchaseorders.create_error'));
     }
 
+    //
+    protected function post_transaction(array $purchase, array $purchase_items, $bill) 
+    {
+        // credit Accounts Payable (Creditors)
+        $account = Account::where('system', 'payable')->first(['id']);
+        $tr_category = Transactioncategory::where('code', 'BILL')->first(['id', 'code']);
+        $tid = Transaction::max('tid') + 1;
+        $cr_data = [
+            'account_id' => $account->id,
+            'tid' => $tid,
+            'trans_category_id' => $tr_category->id,
+            'credit' => $purchase['grandttl'],
+            'tr_date' => date('Y-m-d'),
+            'due_date' => $purchase['due_date'],
+            'user_id' => $purchase['user_id'],
+            'note' => $purchase['note'],
+            'ins' => $purchase['ins'],
+            'tr_type' => $tr_category->code,
+            'tr_ref' => $bill->id,
+            'user_type' => 'supplier',
+            'is_primary' => 1,
+            'ref_ledger_id' => 0,
+        ];
+        $tr = Transaction::create($cr_data);
+        $bill->update(['tr_ref' => $tr->id]);
+
+        $dr_data = array();
+        unset($cr_data['credit'], $cr_data['is_primary']);
+        // debit Stock Account
+        $wip_account = Account::where('system', 'wip')->first(['id']);
+        $stock_items = array_filter($purchase_items, function ($v) { return $v['type'] == 'Stock'; });
+        if ($stock_items) {
+            $tr_category = Transactioncategory::where('code', 'stock')->first(['id']);
+            $data = [
+                'trans_category_id' => $tr_category->id,
+                'debit' => $purchase['stock_subttl'],
+            ];
+            // on project issuance
+            $proj_stock_items = array_filter($stock_items, function ($v) { return $v['itemproject_id']; });
+            if ($proj_stock_items) {
+                $dr_data[] = array_replace($cr_data, [
+                    'account_id' => $wip_account->id,
+                    'ref_ledger_id' => $account->id,
+                    ...$data
+                ]);    
+            } else {
+                $account = Account::where('system', 'stock')->first(['id']);
+                $dr_data[] = array_replace($cr_data, [
+                    'account_id' => $account->id,
+                    ...$data
+                ]);    
+            }
+        }
+        $asset_tr_category = Transactioncategory::where('code', 'p_asset')->first(['id']);
+        foreach ($purchase_items as $item) {
+            $subttl = $item['amount'] - $item['taxrate'];
+            // debit Expense Account
+            if ($item['type'] == 'Expense') {
+                $account_id = $item['item_id'];
+                // on project expense
+                if ($item['itemproject_id']) {
+                    $account_id = $wip_account->id;
+                    $cr_data['ref_ledger_id'] = $item['item_id'];
+                }
+                $dr_data[] = array_replace($cr_data, [
+                    compact('account_id'), 'debit' => $subttl,
+                ]);
+            }
+            //  debit Asset Account
+            if ($item['type'] == 'Asset') {
+                $asset = Assetequipment::find($item['item_id']);
+                $dr_data[] = array_replace($cr_data, [
+                    'account_id' => $asset->account_id,
+                    'trans_category_id' => $asset_tr_category->id,
+                    'debit' => $subttl,
+                ]);
+            }
+        }
+        // debit tax (VAT)
+        $account = Account::where('system', 'tax')->first(['id']);
+        $dr_data[] = array_replace($cr_data, [
+            'account_id' => $account->id, 
+            'debit' => $purchase['grandtax'],
+        ]);
+        Transaction::insert($dr_data); 
+        
+        // update account ledgers debit and credit totals
+        aggregate_account_transactions();
+    }
+
+
     /**
      * For updating the respective Model in storage
      *
@@ -160,75 +251,5 @@ class PurchaseRepository extends BaseRepository
         if ($purchase->delete()) return true;
             
         throw new GeneralException(trans('exceptions.backend.purchaseorders.delete_error'));
-    }
-
-    /**
-     * Post Account Transaction
-     */
-    protected function post_transaction(array $purchase, array $purchase_items, $bill) 
-    {
-        /** credit accounts payable */ 
-        $account = Account::where('system', 'payable')->first(['id']);
-        $tr_category = Transactioncategory::where('code', 'BILL')->first(['id', 'code']);
-        $cr_data = [
-            'account_id' => $account->id,
-            'trans_category_id' => $tr_category->id,
-            'credit' => $purchase['grandttl'],
-            'tr_date' => date('Y-m-d'),
-            'due_date' => $purchase['due_date'],
-            'user_id' => $purchase['user_id'],
-            'note' => $purchase['note'],
-            'ins' => $purchase['ins'],
-            'tr_type' => $tr_category->code,
-            'tr_ref' => $bill->id,
-            'user_type' => 'supplier',
-            'is_primary' => 1
-        ];
-        $tr = Transaction::create($cr_data);
-        $bill->update(['tr_ref' => $tr->id]);
-
-        /** debit */
-        $dr_data = array();
-        unset($cr_data['credit'], $cr_data['is_primary']);
-        // stock
-        $stock_items = array_filter($purchase_items, function ($item) { return $item['type'] == 'Stock'; });
-        if ($stock_items) {
-            $account = Account::where('system', 'stock')->first(['id']);
-            $stock_tr_category = Transactioncategory::where('code', 'stock')->first(['id']);
-            $dr_data[] = array_replace($cr_data, [
-                'account_id' => $account->id,
-                'trans_category_id' => $stock_tr_category->id,
-                'debit' => $purchase['stock_subttl'],
-            ]);    
-        }
-        // expense and asset
-        $asset_tr_category = Transactioncategory::where('code', 'p_asset')->first(['id']);
-        foreach ($purchase_items as $item) {
-            $subttl = $item['amount'] - $item['taxrate'];
-            if ($item['type'] == 'Expense') {
-                $dr_data[] = array_replace($cr_data, [
-                    'account_id' => $item['item_id'],
-                    'debit' => $subttl,
-                ]);
-            }
-            if ($item['type'] == 'Asset') {
-                $asset = Assetequipment::find($item['item_id']);
-                $dr_data[] = array_replace($cr_data, [
-                    'account_id' => $asset->account_id,
-                    'trans_category_id' => $asset_tr_category->id,
-                    'debit' => $subttl,
-                ]);
-            }
-        }
-        // tax
-        $account = Account::where('system', 'tax')->first(['id']);
-        $dr_data[] = array_replace($cr_data, [
-            'account_id' => $account->id, 
-            'debit' => $purchase['grandtax'],
-        ]);
-        Transaction::insert($dr_data); 
-        
-        // update account ledgers debit and credit totals
-        aggregate_account_transactions();
     }
 }
