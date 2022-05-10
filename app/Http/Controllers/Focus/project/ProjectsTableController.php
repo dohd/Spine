7ff95bae74bf4cb32786a99668b29d470b5292e9
Dohd <alloicebrian@gmail.com>
2022-05-10 13:17:23 +0300

    Update projects tableController

--- app/Http/Controllers/Focus/project/ProjectsTableController.php
+++ app/Http/Controllers/Focus/project/ProjectsTableController.php
@@ -49,6 +49,7 @@ class ProjectsTableController extends Controller
     public function __invoke()
     {
         $core = $this->project->getForDataTable();
+
         return Datatables::of($core)
             ->escapeColumns(['id'])
             ->addIndexColumn()
@@ -56,62 +57,34 @@ class ProjectsTableController extends Controller
                 if ($project->customer_project)
                     return $project->customer_project->company;
             })
-            ->addColumn('name', function ($project) {
-                $tg = '';
-                foreach ($project->tags as $row) {
-                    $tg .= '<span class="badge" style="background-color:' . $row['color'] . '">' . $row['name'] . '</span> ';
-                }
-                return '<div class="todo-item media"><div class="media-body"><div class="todo-title">'.$project->name.'<div>' . $tg . '</div></div></div></div>';
-            })
             ->addColumn('tid', function($project) {
-                return 'Prj-'.sprintf('%04d', $project->tid);
+                return gen4tid('Prj-', $project->tid);
             })
             ->addColumn('quote_tid', function($project) {
                 $tids = array();                
                 foreach ($project->quotes as $quote) {
-                    $tid = sprintf('%04d', $quote->tid);
-                    $tid = ($quote->bank_id) ? 'PI-'. $tid : $tid = 'QT-'. $tid;
+                    $tid = $quote->bank_id ? gen4tid('PI-', $quote->tid) : gen4tid('QT-', $quote->tid);
                     $tids[] = '<a href="'.route('biller.projects.create_project_budget', $quote).'" data-toggle="tooltip" title="Budget"><b>'. $tid .'</b></a>';
                 }
-
                 return implode(', ', $tids);
             })
             ->addColumn('lead_tid', function($project) {
                 $tids = array();                
                 foreach ($project->quotes as $quote) {
-                    $tids[] = 'Tkt-' . sprintf('%04d', $quote->lead->reference);
+                    $tids[] = gen4tid('Tkt-', $quote->lead->reference);
                 }
-
                 return implode(', ', $tids);
             })
             ->addColumn('start_status', function ($project) {
-                $badge = 'badge-secondary';
-                foreach($project->quotes as $quote) {
-                    if (count($quote->budgets)) {
-                        $project->start_status = 'budgeted';
-                        $badge = 'badge-success';
-                    }
-                }
-
-                return '<span class="badge '. $badge .'">' . $project->start_status . '</span>';
+                if ($project->quote && $project->quote->budget)
+                    return 'budgeted:';
+                return 'pending:';
             })
-            ->addColumn('progress', function ($project) {
-                $task_back = task_status($project->status);
-                return '<a href="#" class="view_project" data-toggle="modal" data-target="#ViewProjectModal" data-item="' 
-                    . $project->id . '"><span class="badge" style="background-color:' . $task_back['color'] . '">' . $task_back['name'] . '</span></a> ' . numberFormat($project->progress) . ' %';
-            })
-            ->addColumn('created_at', function ($project) {
-                return dateFormat($project->created_at);
+            ->addColumn('start_date', function ($project) {
+                return dateFormat($project->start_date);
             })
             ->addColumn('actions', function ($project) {
-                $btn = '<a href="#" title="View" class="view_project success" data-toggle="modal" data-target="#ViewProjectModal" data-item="' . $project->id . '"><i class="ft-eye fa-lg"></i></a>';
-                // $valid_project_creator = isset($project->creator) && $project->creator->id == auth()->user()->id;
-                if (true) {
-                    $btn .= '&nbsp;&nbsp;<a href="'.route("biller.projects.edit", [$project->id]).'" data-toggle="tooltip" data-placement="top" title="Edit"><i class="ft-edit fa-lg"></i></a>';
-                    // $btn .= '&nbsp;&nbsp;<a class="danger" href="'.route("biller.projects.destroy", [$project->id]).'" data-method="delete" data-trans-button-cancel="' . trans('buttons.general.cancel') . '" data-trans-button-confirm="' . trans('buttons.general.crud.delete') . '" data-trans-title="' . trans('strings.backend.general.are_you_sure') . '" data-toggle="tooltip" data-placement="top" title="Delete"><i class="fa fa-trash fa-lg"></i></a>';
-                }
-
-                return $btn;
+                return $project->action_buttons;
             })
             ->make(true);
     }
