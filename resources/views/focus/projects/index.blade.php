08a8d95991252b2c430b576e9d3c2d8a9080b819
Dohd <alloicebrian@gmail.com>
2022-05-10 13:20:38 +0300

    Update projects index page

--- resources/views/focus/projects/index.blade.php
+++ resources/views/focus/projects/index.blade.php
@@ -2,53 +2,51 @@
 
 @section ('title', trans('labels.backend.projects.management'))
 
-@section('content')
-<div>    
-    <div class="content-wrapper">
-        <div class="content-header row">
-            <div class="content-header-left col-md-6 col-12 mb-2">
-                <h4 class="content-header-title">Project Management</h4>
-            </div>
-            <div class="content-header-right col">
-                <div class="media width-250 float-right">
-                    <div class="media-body media-right text-right">
-                        @include('focus.projects.partials.projects-header-buttons')
-                    </div>
+@section('content') 
+<div class="content-wrapper">
+    <div class="content-header row mb-1">
+        <div class="content-header-left col-6">
+            <h4 class="content-header-title">Project Management</h4>
+        </div>
+        <div class="content-header-right col">
+            <div class="media width-250 float-right">
+                <div class="media-body media-right text-right">
+                    @include('focus.projects.partials.projects-header-buttons')
                 </div>
             </div>
         </div>
-        <div class="content-body">
-            <div class="card todo-details rounded-0">
-                <div class="sidebar-toggle d-block d-lg-none info"><i class="ft-menu font-large-1"></i></div>
-                <div class="search"></div>
-                <div class="card-body">
-                    <table id="projects-table" class="table table-striped table-bordered zero-configuration" cellspacing="0" width="100%">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Customer</th>
-                                <th>{{ trans('projects.project') }}</th>
-                                <th>Project No</th>
-                                <th>#Quote / PI No</th>
-                                <th>Ticket No</th>
-                                <th>Started</th>  
-                                <th>{{ trans('general.createdat') }}</th>
-                                <th>{{ trans('general.action') }}</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td colspan="100%" class="text-center text-success font-large-1"><i class="fa fa-spinner spinner"></i></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
+    </div>
+
+    <div class="content-body">
+        <div class="card todo-details rounded-0">
+            <div class="sidebar-toggle d-block d-lg-none info"><i class="ft-menu font-large-1"></i></div>
+            <div class="search"></div>
+            <div class="card-body">
+                <table id="projects-table" class="table table-striped table-bordered zero-configuration" cellspacing="0" width="100%">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>#Project No</th>
+                            <th>Name</th>
+                            <th>Customer</th>
+                            <th>#Quote / PI No</th>
+                            <th>Ticket No</th>
+                            <th>Start Status</th>  
+                            <th>Start Date</th>
+                            <th>{{ trans('general.action') }}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td colspan="100%" class="text-center text-success font-large-1"><i class="fa fa-spinner spinner"></i></td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
-    </div>    
-</div>
+    </div>
+</div>    
 @include('focus.projects.modal.project_new')
-@include('focus.projects.modal.project_view')
 @endsection
 
 @section('after-styles')
@@ -58,7 +56,6 @@
 @endsection
 
 @section('after-scripts')
-{{-- For DataTables --}}
 {{ Html::script(mix('js/dataTable.js')) }}
 {{ Html::script('core/app-assets/vendors/js/extensions/moment.min.js') }}
 {{ Html::script('core/app-assets/vendors/js/extensions/fullcalendar.min.js') }}
@@ -66,20 +63,19 @@
 {{ Html::script('core/app-assets/js/scripts/pages/app-todo.js') }}
 {{ Html::script('focus/js/bootstrap-colorpicker.min.js') }}
 {{ Html::script('focus/js/select2.min.js') }}
-
 <script>
     setTimeout(() => draw_data(), "{{ config('master.delay') }}");
 
     $.ajaxSetup({ headers: {'X-CSRF-TOKEN': "{{ csrf_token() }}"} });
 
     function draw_data() {
-        const tableLang = { @lang('datatable.strings') };
+        const language = {@lang('datatable.strings')};
         var dataTable = $('#projects-table').dataTable({
             processing: true,
             serverSide: true,
             responsive: true,
             stateSave: true,
-            language: tableLang,
+            language,
             ajax: {
                 url: "{{ route('biller.projects.get') }}",
                 type: 'post'
@@ -89,16 +85,16 @@
                     name: 'id'
                 },
                 {
-                    data: 'customer',
-                    name: 'customer'
+                    data: 'tid',
+                    name: 'tid'
                 },
                 {
                     data: 'name',
                     name: 'name'
                 },
                 {
-                    data: 'tid',
-                    name: 'tid'
+                    data: 'customer',
+                    name: 'customer'
                 },
                 {
                     data: 'quote_tid',
@@ -113,8 +109,8 @@
                     name: 'start_status'
                 },
                 {
-                    data: 'created_at',
-                    name: 'created_at'
+                    data: 'start_date',
+                    name: 'start_date'
                 },
                 {
                     data: 'actions',
@@ -155,13 +151,11 @@
         });
     }
 
-    // On Click Create Modal
+    // On Create Modal click
     $('#AddProjectModal').on('shown.bs.modal', function() {
-        // initiate select2 select menu
         $("#main_quote").select2();
         $("#other_quote").select2();
         $("#branch_id").select2();
-
         // fetch customers
         $("#person").select2({
             ajax: {
@@ -169,10 +163,8 @@
                 dataType: 'json',
                 type: 'POST',
                 quietMillis: 50,
-                data: function(params) {
-                    return { search: params.term };
-                },
-                processResults: function(data) {
+                data: ({term}) => ({ search: term }),
+                processResults: (data) => {
                     return {
                         results: $.map(data, function(item) {
                             return {
@@ -192,15 +184,11 @@
                 ajax: {
                     url: "{{ route('biller.branches.branch_load') }}",
                     quietMillis: 50,
-                    data: function(params) { 
-                        return {
-                            search: params.term, 
-                            customer_id: $("#person").val()
-                        }
-                    },
-                    processResults: function(data) {
-                        return { results: data.map(v => ({ text: v.name, id: v.id })) };
-                    },
+                    data: ({term}) => ({ 
+                        search: term, 
+                        customer_id: $("#person").val()
+                    }),
+                    processResults: data => ({ results: data.map(v => ({ text: v.name, id: v.id })) }),
                 }
             });
 
@@ -214,14 +202,11 @@
                         const results = data.map(v => {
                             const tid = (''+v.tid).length < 4 ? ('000'+v.tid).slice(-4) : v.tid;
                             const text = `${v.bank_id ? '#PI-' : '#QT-'}${tid} - ${v.branch.name} - ${v.notes}`;
-
                             return {id: v.id, text}; 
                         });
-                        
                         // replace array data
                         quoteData.length = 0;
                         quoteData.push.apply(quoteData, results);
-
                         return { results };
                     },
                 }
@@ -234,33 +219,8 @@
             $("#other_quote").html('').select2({ 
                 data: quoteData.filter(v => v.id != $(this).val())
             });
-
             const quoteTitle = $(this).find(':selected').text().split(' - ')[2];
             $('#project-name').val(quoteTitle);
-        });
-    });
-
-    $(document).on('click', ".view_project", function(e) {
-        const project_id = $(this).attr('data-item');
-        $.ajax({
-            url: "{{ route('biller.projects.load') }}",
-            type: 'POST',
-            dataType: 'json',
-            data: { project_id },
-            success: function(data) {
-                $('#p_id').val(data.id);
-                $('#p_name').text(data.name);
-                $('#ps_description').text(data.short_desc);
-                $('#p_description').text(data.note);
-                $('#p_start').text(data.start_date);
-                $('#p_end').text(data.end_date);
-                $('#p_creator').text(data.creator);
-                $('#p_assigned').text(data.assigned);
-                $('#p_status').html(data.status);
-                $('#p_status_list').empty();
-                $('#p_status_list').append(data.status_list);
-                $('#d_view').attr('href', data.view);
-            }
         });
     });
 </script>
