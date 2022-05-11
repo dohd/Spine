dbc37e77b6d2a69634d91072f25576d128d335d1
Dohd <alloicebrian@gmail.com>
2022-05-11 10:33:50 +0300

    Refactor create and update methods

--- app/Repositories/Focus/purchase/PurchaseRepository.php
+++ app/Repositories/Focus/purchase/PurchaseRepository.php
@@ -72,6 +72,9 @@ class PurchaseRepository extends BaseRepository
                 if (in_array($key, ['rate', 'taxrate', 'amount'], 1))
                     $item[$key] = numberClean($val);
             }
+            // direct project stock issuance
+            if ($bill->project_id && $item['type'] == 'Stock')
+                $item['itemproject_id'] = $bill->project_id;
             $purchase_items[$i] = $item;
         }
         PurchaseItem::insert($purchase_items);
@@ -85,7 +88,75 @@ class PurchaseRepository extends BaseRepository
         throw new GeneralException(trans('exceptions.backend.purchaseorders.create_error'));
     }
 
-    //
+    /**
+     * For updating the respective Model in storage
+     *
+     * @param Purchaseorder $purchaseorder
+     * @param  $input
+     * @throws GeneralException
+     * return bool
+     */
+    public function update(Purchase $purchase, array $input)
+    {
+        DB::beginTransaction();
+
+        $bill = $input['bill'];
+        $rate_keys = [
+            'stock_subttl', 'stock_tax', 'stock_grandttl', 'expense_subttl', 'expense_tax', 'expense_grandttl',
+            'asset_tax', 'asset_subttl', 'asset_grandttl', 'grandtax', 'grandttl', 'paidttl'
+        ];
+        foreach ($bill as $key => $val) {
+            if (in_array($key, ['date', 'due_date'], 1)) 
+                $bill[$key] = date_for_database($val);
+            if (in_array($key, $rate_keys, 1)) 
+                $bill[$key] = numberClean($val);
+        }
+        $purchase->update($bill);
+
+        $purchase->products()->delete();
+        $purchase_items = $input['bill_items'];
+        foreach ($purchase_items as $i => $item) {
+            $item = $item + [
+                'ins' => $purchase->ins,
+                'user_id' => $purchase->user_id,
+                'bill_id' => $purchase->id
+            ];
+            foreach ($item as $key => $val) {
+                if (in_array($key, ['rate', 'taxrate', 'amount'], 1))
+                    $item[$key] = numberClean($val);
+            }
+            // direct project stock issuance
+            if ($purchase->project_id && $item['type'] == 'Stock')
+                $item['itemproject_id'] = $purchase->project_id;
+            $purchase_items[$i] = $item;
+        }
+        PurchaseItem::insert($purchase_items);
+
+        // accounts
+        Transaction::where('tr_ref', $purchase->id)->delete();
+        $this->post_transaction($bill, $purchase_items, $purchase);
+
+        DB::commit();
+        if ($purchase) return true;
+
+        throw new GeneralException(trans('exceptions.backend.purchaseorders.update_error'));
+    }
+
+    /**
+     * For deleting the respective model from storage
+     *
+     * @param Purchaseorder $purchaseorder
+     * @throws GeneralException
+     * @return bool
+     */
+    public function delete($purchase)
+    {
+        if ($purchase->delete()) return true;
+            
+        throw new GeneralException(trans('exceptions.backend.purchaseorders.delete_error'));
+    }
+
+    // Account transaction
     protected function post_transaction(array $purchase, array $purchase_items, $bill) 
     {
         // credit Accounts Payable (Creditors)
@@ -173,83 +244,5 @@ class PurchaseRepository extends BaseRepository
         
         // update account ledgers debit and credit totals
         aggregate_account_transactions();
-    }
-
-
-    /**
-     * For updating the respective Model in storage
-     *
-     * @param Purchaseorder $purchaseorder
-     * @param  $input
-     * @throws GeneralException
-     * return bool
-     */
-    public function update(Purchase $purchase, array $input)
-    {
-        DB::beginTransaction();
-
-        $bill = $input['bill'];
-        $rate_keys = [
-            'stock_subttl', 'stock_tax', 'stock_grandttl', 'expense_subttl', 'expense_tax', 'expense_grandttl',
-            'asset_tax', 'asset_subttl', 'asset_grandttl', 'grandtax', 'grandttl', 'paidttl'
-        ];
-        foreach ($bill as $key => $val) {
-            if (in_array($key, ['date', 'due_date'], 1)) {
-                $bill[$key] = date_for_database($val);
-            }
-            if (in_array($key, $rate_keys, 1)) {
-                $bill[$key] = numberClean($val);
-            }
-        }
-        $purchase->update($bill);
-
-        $bill_items = $input['bill_items'];
-        // delete items excluded
-        $item_ids = array_reduce($bill_items, function ($init, $item) {
-            $init[] = $item['id'];
-            return $init;
-        }, []);
-        $purchase->products()->whereNotIn('id', $item_ids)->delete();
-
-        // update or create new items
-        foreach ($bill_items as $item) {
-            $item = $item + [
-                'ins' =>  $purchase->ins,
-                'user_id' =>  $purchase->user_id,
-                'bill_id' => $purchase->id
-            ];
-
-            $bill_item = PurchaseItem::firstOrNew(['id' => $item['id']]);
-            foreach($item as $key => $val) {
-                if (in_array($key, ['rate', 'taxrate', 'amount'], 1)) {
-                    $bill_item[$key] = numberClean($val);
-                } else $bill_item[$key] = $val;
-            }
-            if (!$bill_item->id) unset($bill_item['id']);
-            $bill_item->save();                
-        }
-
-        // accounts
-        Transaction::where('tr_ref', $purchase->id)->delete();
-        $this->post_transaction($bill, $bill_items, $purchase);
-
-        DB::commit();
-        if ($purchase) return true;
-
-        throw new GeneralException(trans('exceptions.backend.purchaseorders.update_error'));
-    }
-
-    /**
-     * For deleting the respective model from storage
-     *
-     * @param Purchaseorder $purchaseorder
-     * @throws GeneralException
-     * @return bool
-     */
-    public function delete($purchase)
-    {
-        if ($purchase->delete()) return true;
-            
-        throw new GeneralException(trans('exceptions.backend.purchaseorders.delete_error'));
     }
 }
