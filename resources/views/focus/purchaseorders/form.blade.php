8319d175e6698c83b71d0dc2aaabe07b4d6cbb96
Dohd <alloicebrian@gmail.com>
2022-05-13 09:21:14 +0300

    Update purchaseorder projects field

--- resources/views/focus/purchaseorders/form.blade.php
+++ resources/views/focus/purchaseorders/form.blade.php
@@ -131,7 +131,7 @@
                 <div class="col-sm-12">
                     <div class="form-group">
                         <label for="project" class="caption">Projects</label>
-                        <select class="form-control" name="project_id" id="project" data-placeholder="Search Project by Name, Customer, Branch" required>
+                        <select class="form-control" name="project_id" id="project" data-placeholder="Search Project by Name, Customer, Branch">
                         </select>
                     </div>
                 </div>
