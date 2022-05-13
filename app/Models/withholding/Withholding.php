9874aad22dc0df6688804b871baf7fcd3505f324
Dohd <alloicebrian@gmail.com>
2022-05-13 12:50:54 +0300

    Update withholding model table

--- app/Models/withholding/Withholding.php
+++ app/Models/withholding/Withholding.php
@@ -24,7 +24,7 @@ class Withholding extends Model
      * The database table used by the model.
      * @var string
      */
-    protected $table = 'transactions';
+    protected $table = 'withholdings';
 
     /**
      * Mass Assignable fields of model
