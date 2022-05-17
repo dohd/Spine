d255970bf715fc45966f5e558a4ca6bfafbed849
Dohd <alloicebrian@gmail.com>
2022-05-17 10:32:29 +0300

    Update witholding index page

--- resources/views/focus/withholdings/index.blade.php
+++ resources/views/focus/withholdings/index.blade.php
@@ -2,113 +2,85 @@
 
 @section ('title', 'WithHolding Tax management')
 
-@section('page-header')
-    <h1>WithHolding Tax  Management</h1>
-@endsection
-
 @section('content')
-    <div class="">
-        <div class="content-wrapper">
-            <div class="content-header row">
-                <div class="content-header-left col-md-6 col-12 mb-2">
-                    <h4 class="content-header-title mb-0">WithHolding Tax  Management</h4>
-
-                </div>
-                <div class="content-header-right col-md-6 col-12">
-                    <div class="media width-250 float-right">
-
-                        <div class="media-body media-right text-right">
-                            @include('focus.withholdings.partials.withholdings-header-buttons')
-                        </div>
-                    </div>
+<div class="content-wrapper">
+    <div class="content-header row mb-1">
+        <div class="content-header-left col-6">
+            <h4 class="content-header-title">WithHolding Tax  Management</h4>
+        </div>
+        <div class="content-header-right col-6">
+            <div class="media width-250 float-right">
+                <div class="media-body media-right text-right">
+                    @include('focus.withholdings.partials.withholdings-header-buttons')
                 </div>
             </div>
-            <div class="content-body">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="card">
+        </div>
+    </div>
 
-                            <div class="card-content">
-
-                                <div class="card-body">
-                                    <table id="withholdings-table"
-                                           class="table table-striped table-bordered zero-configuration" cellspacing="0"
-                                           width="100%">
-                                        <thead>
-                                        <tr>
-                                            <th>#</th>
-                                            <th>Customer</th>
-                                            <th>Invoice</th>
-                                            <th>Certificate</th>
-                                            <th>Debit</th>
-                                            <th>Transaction Date</th>
-                                            <th>{{ trans('general.createdat') }}</th>
-                                            <th>{{ trans('labels.general.actions') }}</th>
-                                        </tr>
-                                        </thead>
-
-
-                                        <tbody></tbody>
-                                    </table>
-                                </div>
-
-
-                            </div>
+    <div class="content-body">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-content">
+                        <div class="card-body">
+                            <table id="withholdings-table" class="table table-striped table-bordered zero-configuration" cellspacing="0" width="100%">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Customer</th>
+                                        <th>Certificate</th>
+                                        <th>Amount</th>
+                                        <th>Date</th>
+                                        <th>{{ trans('labels.general.actions') }}</th>
+                                    </tr>
+                                </thead>
+                                <tbody></tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
 
 @section('after-scripts')
-    {{-- For DataTables --}}
-    {{ Html::script(mix('js/dataTable.js')) }}
-    <script>
-        $(function () {
-            $.ajaxSetup({
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                }
-            });
-
-            var dataTable = $('#withholdings-table').dataTable({
-                processing: true,
-                serverSide: true,
-                responsive: true,
-                language: {
-                    @lang('datatable.strings')
-                },
-                ajax: {
-                    url: '{{ route("biller.withholdings.get") }}',
-                    type: 'post'
-                },
-                columns: [
-                    {data: 'DT_Row_Index', name: 'id'},
-                    {data: 'account_id', name: 'account_id'},
-                    {data: 'invoice_id', name: 'invoice_id'},
-                     {data: 'refer_no', name: 'refer_no'},
-                    {data: 'debit', name: 'debit'},
-                    {data: 'transaction_date', name: 'transaction_date'},
-                    {data: 'created_at', name: 'created_at'},
-
-                    {data: 'actions', name: 'actions', searchable: false, sortable: false}
-                ],
-                order: [[0, "asc"]],
-                searchDelay: 500,
-                dom: 'Blfrtip',
-                buttons: {
-                    buttons: [
-
-                        {extend: 'csv', footer: true, exportOptions: {columns: [0, 1]}},
-                        {extend: 'excel', footer: true, exportOptions: {columns: [0, 1]}},
-                        {extend: 'print', footer: true, exportOptions: {columns: [0, 1]}}
-                    ]
-                }
-            });
-            $('#withholdings-table_wrapper').removeClass('form-inline');
-
-        });
-    </script>
+{{ Html::script(mix('js/dataTable.js')) }}
+<script>
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': "{{ csrf_token() }}"
+        }
+    });
+    const language = {@lang('datatable.strings')};
+    const dataTable = $('#withholdings-table').dataTable({
+        processing: true,
+        serverSide: true,
+        responsive: true,
+        language,
+        ajax: {
+            url: '{{ route("biller.withholdings.get") }}',
+            type: 'post'
+        },
+        columns: [
+            {data: 'DT_Row_Index', name: 'id'},
+            {data: 'customer', name: 'customer'},
+            {data: 'reference', name: 'reference'},
+            {data: 'amount', name: 'amount'},
+            {data: 'date', name: 'date'},
+            {data: 'actions', name: 'actions', searchable: false, sortable: false}
+        ],
+        order: [[0, "desc"]],
+        searchDelay: 500,
+        dom: 'Blfrtip',
+        buttons: {
+            buttons: [
+                {extend: 'csv', footer: true, exportOptions: {columns: [0, 1]}},
+                {extend: 'excel', footer: true, exportOptions: {columns: [0, 1]}},
+                {extend: 'print', footer: true, exportOptions: {columns: [0, 1]}}
+            ]
+        }
+    });
+</script>
 @endsection
