b8ad078b9993d301001b1c235dda7732f10cb433
Dohd <alloicebrian@gmail.com>
2022-05-17 17:08:22 +0300

    Update equipment categories header buttons

--- resources/views/focus/equipmentcategories/partials/equipmentcategories-header-buttons.blade.php
+++ resources/views/focus/equipmentcategories/partials/equipmentcategories-header-buttons.blade.php
@@ -1,14 +1,6 @@
-<div class="">
-    <div class="btn-group" role="group" aria-label="Basic example">
-        <a href="{{ route( 'biller.equipmentcategories.index' ) }}?module={{@$input['module']}}"
-           class="btn btn-info  btn-lighten-2 round"><i
-                    class="fa fa-list-alt"></i> {{trans( 'general.list' )}}</a>
-        @permission( 'create-misc' )
-        <a href="{{ route( 'biller.equipmentcategories.create' ) }}?module={{@$input['module']}}"
-           class="btn btn-pink  btn-lighten-3 round"><i
-                    class="fa fa-plus-circle"></i> {{trans( 'general.create' )}}</a>
-        @endauth
-    </div>
+<div class="btn-group" role="group" aria-label="Basic example">
+    <a href="{{ route('biller.equipmentcategories.index') }}" class="btn btn-info  btn-lighten-2"><i class="fa fa-list-alt"></i> {{trans( 'general.list' )}}</a>
+    @permission( 'create-misc' )
+    <a href="{{ route('biller.equipmentcategories.create') }}" class="btn btn-pink  btn-lighten-3"><i class="fa fa-plus-circle"></i> {{trans( 'general.create' )}}</a>
+    @endauth
 </div>
-<div class="clearfix"></div>
-
