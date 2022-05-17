5effce121a3f64f62bc7b454e29d518636eaa6d8
Dohd <alloicebrian@gmail.com>
2022-05-17 15:39:12 +0300

    Update credit note index page

--- resources/views/focus/creditnotes/index.blade.php
+++ resources/views/focus/creditnotes/index.blade.php
@@ -3,45 +3,44 @@
 @section('title', $is_debit ? 'Debit Notes Management' : 'Credit Notes Management')
 
 @section('content')
-<div>
-    <div class="content-wrapper">
-        <div class="content-header row">
-            <div class="content-header-left col-md-6 col-12 mb-2">
-                <h4 class="content-header-title">{{ $is_debit ? 'Debit Notes Management' : 'Credit Notes Management' }}</h4>
-            </div>
-            <div class="content-header-right col-md-6 col-12">
-                <div class="media width-250 float-right mr-3">
-                    <div class="media-body media-right text-right">
-                        @include('focus.creditnotes.partials.creditnotes-header-buttons')
-                    </div>
+<div class="content-wrapper">
+    <div class="content-header row mb-1">
+        <div class="content-header-left col-6">
+            <h4 class="content-header-title">{{ $is_debit ? 'Debit Notes Management' : 'Credit Notes Management' }}</h4>
+        </div>
+        <div class="content-header-right col-6">
+            <div class="media width-250 float-right">
+                <div class="media-body media-right text-right">
+                    @include('focus.creditnotes.partials.creditnotes-header-buttons')
                 </div>
             </div>
         </div>
+    </div>
 
-        <div class="content-body">
-            <div class="card">
-                <div class="card-content">
-                    <div class="card-body">
-                        <table id="creditnotesTbl" class="table table-striped table-bordered zero-configuration" cellspacing="0" width="100%">
-                            <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>#CN No</th>
-                                    <th>Customer</th>
-                                    <th>#Invoice No</th>
-                                    <th>Amount</th>
-                                    <th>Date</th>                                                                             
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td colspan="100%" class="text-center text-success font-large-1">
-                                        <i class="fa fa-spinner spinner"></i>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
+    <div class="content-body">
+        <div class="card">
+            <div class="card-content">
+                <div class="card-body">
+                    <table id="creditnotesTbl" class="table table-striped table-bordered zero-configuration" cellspacing="0" width="100%">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>#{{ $is_debit? 'DN' : 'CN' }} No</th>
+                                <th>Customer</th>
+                                <th>#Invoice No</th>
+                                <th>Amount</th>
+                                <th>Date</th>  
+                                <th>Action</th>                                                                           
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td colspan="100%" class="text-center text-success font-large-1">
+                                    <i class="fa fa-spinner spinner"></i>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
@@ -56,9 +55,8 @@
     setTimeout(() => draw_data(), "{{ config('master.delay') }}");
 
     function draw_data() {
-        const language = {@lang("datatable.strings")};
         const is_debit = "{{ $is_debit ? 1 : 0 }}";
-
+        const language = {@lang("datatable.strings")};
         const dataTable = $('#creditnotesTbl').dataTable({
             processing: true,
             serverSide: true,
@@ -94,7 +92,12 @@
                     data: 'date',
                     name: 'date'
                 },
-            ],
+                {
+                    data: 'actions',
+                    name: 'actions',
+                    searchable: false,
+                    sortable: false
+                }            ],
             order: [[0, "desc"]],
             searchDelay: 500,
             dom: 'Blfrtip',
