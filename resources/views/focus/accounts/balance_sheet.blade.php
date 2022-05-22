966e439292c4eeb909f131d7f4a6c3870463deb9
Dohd <alloicebrian@gmail.com>
2022-05-22 12:35:35 +0300

    Update balance_sheet page

--- resources/views/focus/accounts/balance_sheet.blade.php
+++ resources/views/focus/accounts/balance_sheet.blade.php
@@ -1,128 +1,102 @@
 @extends ('core.layouts.app')
 @section ('title', trans('accounts.balance_sheet').' | ' . trans('labels.backend.accounts.management'))
 
-@section('page-header')
-    <h1>
-        {{ trans('labels.backend.accounts.management') }}
-        <small>{{ trans('labels.backend.accounts.create') }}</small>
-    </h1>
-@endsection
 @section('content')
-    <div class="">
-        <div class="content-wrapper">
-            <div class="content-header row">
-                <div class="content-header-left col-md-6 col-12 mb-2">
-                    <h3 class="mb-0"> {{trans('accounts.balance_sheet')}} <a class="btn btn-success btn-sm"
-                                                                             href="{{ route( 'biller.accounts.balance_sheet',['p']) }}">
-                            <i class="fa fa-print"></i> {{ trans('general.print') }}</a></h3>
+<div class="content-wrapper">
+    <div class="content-header row mb-1">
+        <div class="content-header-left col-6">
+            <h3> 
+                {{trans('accounts.balance_sheet')}} 
+                <a class="btn btn-success btn-sm" href="{{ route('biller.accounts.balance_sheet', 'p') }}">
+                    <i class="fa fa-print"></i> {{ trans('general.print') }}
+                </a>
+            </h3>
+        </div>
 
-                </div>
-                <div class="content-header-right col-md-6 col-12">
-                    <div class="media width-250 float-right">
-
-                        <div class="media-body media-right text-right">
-                            @include('focus.accounts.partials.accounts-header-buttons')
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="content-body">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="card">
-                            @php
-                                $gross_ac=array();
-                            @endphp
-                            @foreach($account_types as $key => $t_row)
-                                <div class="card-content print_me">
-                                    <h5 class="title {{ isset($bg_styles[$key]) ? $bg_styles[$key] : 'bg-gradient-x-info' }} p-1 white">
-                                        {{$t_row}} {{trans('accounts.accounts')}}
-                                    </h5>
-                                    <p>&nbsp;</p>
-                                    <table class="table">
-                                        <thead>
-                                        <tr>
-                                            <th>#</th>
-                                            <th>Accoun Number</th>
-                                            <th>{{trans('accounts.account')}}</th>
-                                            <th>{{trans('accounts.balance')}}</th>
-                                        </tr>
-                                        </thead>
-                                        <tbody>
-                                        @php $i = 1;
-                    $gross = 0;
-                    foreach ($account as $row) {
-                        if ($row['account_type'] == $t_row) {
-                            $aid = $row['id'];
-                            $acn = $row['number'];
-                            $holder = $row['holder'];
-                            $balance = $row['balance'];
-                            $qty = $row['created_at'];
-                            echo "<tr>
-                    <td>$i</td>
-                    <td>".strip_tags($holder)."</td>
-                    <td>".strip_tags($acn)."</td>
-
-                    <td>" . amountFormat($balance) . "</td>
-                    </tr>";
-                            $i++;
-                            $gross += $balance;
-
-
-
-                    }
-                        }
-                       $gross_ac[]=array('name'=>$t_row,'balance'=>$gross);
-                                        @endphp
-                                        </tbody>
-                                        <tfoot>
-                                        <tr>
-                                            <th></th>
-                                            <th></th>
-
-                                            <th></th>
-
-                                            <th>
-                                                <h3 class="text-xl-left">{{ amountFormat($gross)}}</h3>
-                                            </th>
-                                        </tr>
-                                        </tfoot>
-                                    </table>
-                                    @endforeach
-
-                                    <table class="table table-striped">
-                                        <thead>
-                                        <tr>
-                                            <th>{{trans('accounts.account_type')}}</th>
-                                            <th>{{trans('accounts.balance')}}</th>
-                                        </tr>
-                                        </thead>
-                                        <tbody>
-                                        @foreach($gross_ac as $g_row)
-                                            <tr>
-                                                <td>{{$g_row['name']}}</td>
-                                                <td>{{amountFormat($g_row['balance'])}}</td>
-
-                                            </tr>
-
-                                        @endforeach
-                                        </tbody>
-                                        <tfoot>
-
-                                        </tfoot>
-                                    </table>
-                                </div>
-                        </div>
-                    </div>
+        <div class="content-header-right col-6">
+            <div class="media width-250 float-right">
+                <div class="media-body media-right text-right">
+                    @include('focus.accounts.partials.accounts-header-buttons')
                 </div>
             </div>
         </div>
     </div>
-@endsection
-@section('after-scripts')
-    <script type="text/javascript">
-        $(function () {
-            // $(".print_me").printThis();
-        });
-    </script>
+
+    <div class="content-body">
+        <div class="row">
+            <div class="col-12">
+                @php
+                    $balance_cluster = array();
+                @endphp
+                @foreach(['Asset', 'Equity', 'Expense', 'Income', 'Liability', 'Summary'] as $i => $type)
+                    <div class="card">
+                        <div class="card-content print_me">
+                            @if ($i < 5)
+                                <h5 class="title {{ $bg_styles[$i] }} p-1 white">{{ $type }} {{trans('accounts.accounts')}}</h5>
+                                <table class="table table-sm">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Account No</th>
+                                            <th>Account</th>
+                                            <th>Balance</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @php
+                                            $gross_balance = 0;
+                                            $j = 1;
+                                        @endphp
+                                        @foreach ($accounts as $account)
+                                            @if ($account->account_type == $type)
+                                                <tr>
+                                                    <td>{{ $j }}</td>
+                                                    <td>{{ $account->number }}</td>
+                                                    <td>{{ $account->holder }}</td>
+                                                    <td>{{ $account->balance }}</td>
+                                                </tr>
+                                                @php
+                                                    $gross_balance += $account->balance;
+                                                    $j++;
+                                                @endphp
+                                            @endif
+                                        @endforeach
+                                        <tr>
+                                            @for ($k = 0; $k < 3; $k++)
+                                                <td></td>
+                                            @endfor
+                                            <td><h3 class="text-xl-left">{{ amountFormat($gross_balance) }}</h3></td>
+                                        </tr>
+                                        @php
+                                            $balance_cluster[] = compact('type', 'gross_balance');
+                                        @endphp
+                                    </tbody>
+                                </table> 
+                                
+                            @else
+                                <h5 class="title {{ $bg_styles[$i] }} p-1 white">{{ $type }}</h5>
+                                <table class="table table-striped table-sm">
+                                    <thead>
+                                        <tr>
+                                            <th>{{trans('accounts.account_type')}}</th>
+                                            <th>Balance</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach($balance_cluster as $cluster)
+                                            <tr>
+                                                <td>{{ $cluster['type'] }}</td>
+                                                <td>{{ amountFormat($cluster['gross_balance']) }}</td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>    
+                            @endif                           
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
