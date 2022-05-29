0f64f144d1e13fcbc6eb443ed3b86136b8233e6d
Dohd <alloicebrian@gmail.com>
2022-05-29 22:13:35 +0300

    Update pricegroup create page

--- resources/views/focus/pricegroups/form.blade.php
+++ resources/views/focus/pricegroups/form.blade.php
@@ -1,23 +1,71 @@
-<div class='form-group'>
-    {{ Form::label( 'name', 'Price Group Name',['class' => 'col-lg-2 control-label']) }}
-    <div class='col-lg-10'>
-        {{ Form::text('name', null, ['class' => 'form-control box-size required', 'placeholder' => 'Price Group Name *','required'=>'required']) }}
+<div class="form-group row">
+    <div class="col-6">
+        <label for="client">Client</label>
+        <select name="ref_id" id="client" class="form-control" data-placeholder="Choose client" required></select>
+        <input type="hidden" name="is_client" value="1" id="is_client">
+    </div>
+    <div class="col-6">
+        <label for="supplier">Supplier</label>
+        <select name="ref_id" id="supplier" class="form-control" data-placeholder="Choose supplier" required></select>
     </div>
 </div>
-<div class='form-group'>
-    {{ Form::label( 'description', 'Description',['class' => 'col-lg-2 control-label']) }}
-    <div class='col-lg-10'>
-        {{ Form::text('description', null, ['class' => 'form-control box-size', 'placeholder' => 'Description ']) }}
+<div class='form-group row'>
+    <div class="col-6">
+        <label for="group_name">Price Group Name</label>
+        {{ Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Price Group Name', 'required']) }}
+    </div>
+    <div class="col-6">
+        <label for="description">Description</label>
+        {{ Form::text('description', null, ['class' => 'form-control', 'placeholder' => 'Description ']) }}
     </div>
 </div>
 
-@section("after-scripts")
-    <script type="text/javascript">
-        //Put your javascript needs in here.
-        //Don't forget to put `@`parent exactly after `@`section("after-scripts"),
-        //if your create or edit blade contains javascript of its own
-        $(document).ready(function () {
-            //Everything in here would execute after the DOM is ready to manipulated.
-        });
-    </script>
+@section('after-scripts')
+{{ Html::script('focus/js/select2.min.js') }}
+<script>
+    $.ajaxSetup({ headers: {'X-CSRF-TOKEN': "{{ csrf_token() }}"}});
+    const select2Config = (url, callback) => ({
+        ajax: {
+            url,
+            dataType: 'json',
+            type: 'POST',
+            quietMillis: 50,
+            data: ({term}) => ({ search: term }),
+            processResults: data  => callback(data),
+        }
+    });
+
+    // fetch customers
+    const clientUrl = "{{ route('biller.customers.select') }}";
+    function clientCb(data) {
+        return {
+            results: data.map(v => ({ 
+                id: v.id, 
+                text: `${v.name} - ${v.company}`,
+            }))
+        }
+    };
+    $("#client").select2(select2Config(clientUrl, clientCb));
+
+    // fetch suppliers
+    const suppliertUrl = "{{ route('biller.suppliers.select') }}";
+    function supplierCb(data) {
+        return {
+            results: data.map(v => ({ 
+                id: v.id, 
+                text: v.name + ' - ' + v.email 
+            }))
+        }
+    };
+    $("#supplier").select2(select2Config(suppliertUrl, supplierCb));
+
+    // select constraint
+    $('form').on('change', '#client, #supplier', function() {
+        if ($(this).is('#client')) $('#supplier').attr('disabled', true);
+        if ($(this).is('#supplier')) {
+            $('#client').attr('disabled', true);
+            $('#is_client').attr('disabled', true);
+        }
+    });
+</script>
 @endsection
