fd3bd079dbc75c497e641106dc9fc282069e3d4e
Dohd <alloicebrian@gmail.com>
2022-05-30 16:59:58 +0300

    Update bank drop down

--- resources/views/focus/reconciliations/form.blade.php
+++ resources/views/focus/reconciliations/form.blade.php
@@ -3,15 +3,17 @@
         <label for="payer" class="caption">Bank</label>                                       
         <select class="form-control" id="bank" name="account_id" required>
             <option value="">-- Select Bank --</option>
-            @foreach ($accounts as $row)
-                <option value="{{ $row->id }}">{{ $row->holder }}</option>
+            @foreach ($accounts as $bank)
+                <option value="{{ $bank->id }}" openingBalance="{{ $bank->opening_balance }}">
+                    {{ $bank->holder }}
+                </option>
             @endforeach
         </select>
     </div>
     <div class="col-2">
         <label for="recon_id" class="caption">Reconciliation ID</label>
         <div class="input-group">
-            {{ Form::text('tid', @$reconciliation->tid+1, ['class' => 'form-control', 'id' => 'tid', 'readonly']) }}
+            {{ Form::text('tid', $last_tid+1, ['class' => 'form-control', 'id' => 'tid', 'readonly']) }}
         </div>
     </div> 
     <div class="col-2">
@@ -26,7 +28,7 @@
 
 <div class="form-group row">                          
     <div class="col-3">
-        <label for="amount" class="caption">System Balance</label>
+        <label for="amount" class="caption">System Account Balance</label>
         {{ Form::text('system_amount', '0.0', ['class' => 'form-control', 'id' => 'systemBal', 'readonly']) }}
     </div>    
     <div class="col-2">
@@ -44,14 +46,14 @@
         <thead>
             <tr class="bg-gradient-directional-blue white">
                 <th class="text-center">Date</th>
-                <th>Transaction ID</th>
+                <th>Transaction Id</th>
                 <th width="40%" class="text-center">Note</th>
                 <th width="12%" class="text-center">Debit</th>
                 <th width="12%" class="text-center">Credit</th>
                 <th width="5%">Action <input class="form-check-input checkall" type="checkbox" style="margin-left: .3em;"></th>
             </tr>
         </thead>
-        <tbody>                                
+        <tbody>       
         </tbody>                
     </table>
 </div>
