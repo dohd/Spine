25737caf80206eba18e0b3ca41590817fc45472a
Dohd <alloicebrian@gmail.com>
2022-06-06 17:26:33 +0300

    Update create page

--- resources/views/focus/taskschedules/create.blade.php
+++ resources/views/focus/taskschedules/create.blade.php
@@ -38,6 +38,61 @@
 <script>
     $.ajaxSetup({ headers: {'X-CSRF-TOKEN': "{{ csrf_token() }}"}});
 
+    // form submit
+    $('form').submit(function(e) {
+        const equipments = $('#equipmentTbl tbody tr').length;
+        if (equipments < 1) {
+            e.preventDefault();
+            alert('Include at least one equipment!');
+        }
+    });
 
+    // on contract select
+    const equipRow =  $('#equipmentTbl tbody tr').html();
+    const elements = ['#id', '#unique_id', '#make_type', '#branch', '#location'];
+    $('#contract').change(function() {
+        // load task schedules
+        $.ajax({
+            url: "{{ route('biller.contracts.task_schedules')  }}",
+            type: 'POST',
+            data: {id: $(this).val()},
+            success: data => {
+               $('#schedule option').remove();
+               $('#schedule').append(new Option('-- Select Schedule --', ''));
+                data.forEach(v => {                    
+                    $('#schedule').append(new Option(v.title, v.id));
+                });
+            }
+        })
+
+        // load equipments
+        $.ajax({
+            url: "{{ route('biller.contracts.contract_equipment')  }}",
+            type: 'POST',
+            data: {id: $(this).val()},
+            success: data => {
+                $('#equipmentTbl tbody tr').remove();
+                data.forEach(obj => {
+                    let html = equipRow.replace('d-none', '');
+                    elements.forEach(el => {
+                        for (let p in obj) {
+                            if ('#'+p == el && p == 'branch') html = html.replace(el, obj.branch.name);
+                            else if ('#'+p == el) html = html.replace(el, obj[p]? obj[p] : '');
+                        }
+                    });
+                    $('#equipmentTbl tbody').append('<tr>' + html + '</tr>');
+                })
+            }
+        })
+    });
+    
+    // add schedule row
+    $('#addEquipment').click(function() {
+        $('#equipmentTbl tbody').append('<tr>' + scheduleRow + '</tr>');
+    });
+    // remove schedule row
+    $('#equipmentTbl').on('click', '.remove', function() {
+        $(this).parents('tr').remove();
+    });
 </script>
 @endsection
