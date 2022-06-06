c34189e15fb9f7c72e52cc230d1bd39da26ffb4f
Dohd <alloicebrian@gmail.com>
2022-06-06 17:25:38 +0300

    Add taskschedule_equipments relation method

--- app/Models/task_schedule/Traits/TaskScheduleRelationship.php
+++ app/Models/task_schedule/Traits/TaskScheduleRelationship.php
@@ -3,9 +3,15 @@
 namespace App\Models\task_schedule\Traits;
 
 use App\Models\contract\Contract;
+use App\Models\contract_equipment\ContractEquipment;
 
 trait TaskScheduleRelationship
 {    
+    public function taskschedule_equipments(Type $var = null)
+    {
+        return $this->hasMany(ContractEquipment::class, 'schedule_id');
+    }
+
     public function contract()
     {
         return $this->belongsTo(Contract::class)->withoutGlobalScopes();
