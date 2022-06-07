576f6daa7838f73c9e264253639fde98a6cb4021
Dohd <alloicebrian@gmail.com>
2022-06-07 17:13:10 +0300

    Add contractservice relation method

--- app/Models/task_schedule/Traits/TaskScheduleRelationship.php
+++ app/Models/task_schedule/Traits/TaskScheduleRelationship.php
@@ -4,10 +4,16 @@ namespace App\Models\task_schedule\Traits;
 
 use App\Models\contract\Contract;
 use App\Models\contract_equipment\ContractEquipment;
+use App\Models\contractservice\ContractService;
 
 trait TaskScheduleRelationship
 {    
-    public function taskschedule_equipments(Type $var = null)
+    public function contractservice()
+    {
+        return $this->hasOne(ContractService::class, 'schedule_id');
+    }
+
+    public function taskschedule_equipments()
     {
         return $this->hasMany(ContractEquipment::class, 'schedule_id');
     }
