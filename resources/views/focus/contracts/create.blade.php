3345abbc7663dfe01a889463cb6d962cb0978fd2
Dohd <alloicebrian@gmail.com>
2022-06-10 17:02:10 +0300

    Update contracts create page body

--- resources/views/focus/contracts/create.blade.php
+++ resources/views/focus/contracts/create.blade.php
@@ -3,31 +3,29 @@
 @section ('title', 'Create | Contract Management')
 
 @section('content')
-<div>
-    <div class="content-wrapper">
-        <div class="content-header row mb-1">
-            <div class="content-header-left col-6">
-                <h4 class="content-header-title">Contract Management</h4>
-            </div>
-            <div class="content-header-right col-6">
-                <div class="media width-250 float-right">
-                    <div class="media-body media-right text-right">
-                        @include('focus.contracts.partials.contracts-header-buttons')
-                    </div>
+<div class="content-wrapper">
+    <div class="content-header row mb-1">
+        <div class="content-header-left col-6">
+            <h4 class="content-header-title">Contract Management</h4>
+        </div>
+        <div class="content-header-right col-6">
+            <div class="media width-250 float-right">
+                <div class="media-body media-right text-right">
+                    @include('focus.contracts.partials.contracts-header-buttons')
                 </div>
             </div>
         </div>
+    </div>
 
-        <div class="content-body">
-            <div class="row">
-                <div class="col-12">
-                    <div class="card">
-                        <div class="card-content">
-                            <div class="card-body">
-                                {{ Form::open(['route' => 'biller.contracts.store']) }}
-                                    @include('focus.contracts.form')
-                                {{ Form::close() }}
-                            </div>
+    <div class="content-body">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-content">
+                        <div class="card-body">
+                            {{ Form::open(['route' => 'biller.contracts.store']) }}
+                                @include('focus.contracts.form')
+                            {{ Form::close() }}
                         </div>
                     </div>
                 </div>
