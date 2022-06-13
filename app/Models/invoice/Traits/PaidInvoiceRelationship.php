8685029d1d2d40eaa1d362d772d7f2d0664f1be3
Dohd <alloicebrian@gmail.com>
2022-06-13 12:54:49 +0300

    Add acount realtion method

--- app/Models/invoice/Traits/PaidInvoiceRelationship.php
+++ app/Models/invoice/Traits/PaidInvoiceRelationship.php
@@ -2,11 +2,17 @@
 
 namespace App\Models\invoice\Traits;
 
+use App\Models\account\Account;
 use App\Models\customer\Customer;
 use App\Models\items\PaidInvoiceItem;
 
 trait PaidInvoiceRelationship
 {
+    public function account()
+    {
+        return $this->belongsTo(Account::class);
+    }
+
     public function customer()
     {
         return $this->belongsTo(Customer::class);
