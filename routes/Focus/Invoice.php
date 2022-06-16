b618348b9214f3dc9ac62340efd951bb34b7e64c
Dohd <alloicebrian@gmail.com>
2022-06-16 12:18:06 +0300

    Add invoice payment routes

--- routes/Focus/Invoice.php
+++ routes/Focus/Invoice.php
@@ -12,11 +12,16 @@ Route::group(['namespace' => 'invoice'], function () {
     Route::post('drafts_load', 'InvoicesController@drafts_load')->name('invoices.drafts_load');
     Route::get('draft_view/{id}', 'InvoicesController@draft_view')->name('invoices.draft_view');
     Route::post('pos_update', 'InvoicesController@pos_update')->name('invoices.pos_update');
-    Route::get('invoices/create_payment', 'InvoicesController@create_payment')->name('invoices.create_payment');
-    Route::post('invoices/store_payment', 'InvoicesController@store_payment')->name('invoices.store_payment');
     Route::get('invoices/client_invoices', 'InvoicesController@client_invoices')->name('invoices.client_invoices');
     Route::post('invoices/unallocated_payment', 'InvoicesController@unallocated_payment')->name('invoices.unallocated_payment');
 
+    // payment
+    Route::get('invoices/index_payment', 'InvoicesController@index_payment')->name('invoices.index_payment');
+    Route::get('invoices/create_payment', 'InvoicesController@create_payment')->name('invoices.create_payment');
+    Route::post('invoices/store_payment', 'InvoicesController@store_payment')->name('invoices.store_payment');
+    Route::get('invoices/edit_payment/{id}', 'InvoicesController@edit_payment')->name('invoices.edit_payment');
+    Route::patch('invoices/update_payment/{id}', 'InvoicesController@update_payment')->name('invoices.update_payment');
+    Route::post('invoices/delete_payment/{id}', 'InvoicesController@delete_payment')->name('invoices.delete_payment');
 
     // project invoice
     Route::get('invoices/edit_project_invoice/{invoice}', 'InvoicesController@edit_project_invoice')->name('invoices.edit_project_invoice');
@@ -28,6 +33,7 @@ Route::group(['namespace' => 'invoice'], function () {
 
     //For Datatable
     Route::post('invoices/get', 'InvoicesTableController')->name('invoices.get');
+    Route::post('invoices/get_payments', 'PaymentsTableController')->name('invoices.get_payments');
     Route::get('invoices/print_document/{id}/{type}', 'InvoicesController@print_document')->name('invoices.print_document');
 });
 
