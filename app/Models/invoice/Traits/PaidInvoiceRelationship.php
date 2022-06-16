7e404dc10e9cef413f901927bc24e5c478e0c26e
Dohd <alloicebrian@gmail.com>
2022-06-16 12:13:09 +0300

    Add transaction relation method

--- app/Models/invoice/Traits/PaidInvoiceRelationship.php
+++ app/Models/invoice/Traits/PaidInvoiceRelationship.php
@@ -5,9 +5,15 @@ namespace App\Models\invoice\Traits;
 use App\Models\account\Account;
 use App\Models\customer\Customer;
 use App\Models\items\PaidInvoiceItem;
+use App\Models\transaction\Transaction;
 
 trait PaidInvoiceRelationship
 {
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class, 'tr_ref')->whereIn('tr_type', ['pmt', 'adv_pmt']);
+    }
+
     public function account()
     {
         return $this->belongsTo(Account::class);
