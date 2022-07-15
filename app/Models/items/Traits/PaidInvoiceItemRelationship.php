d65dbbe57e6182c41c02be9a3b129b394737176d
Dohd <alloicebrian@gmail.com>
2022-07-15 12:24:05 +0300

    Add paid_invoice relation method

--- app/Models/items/Traits/PaidInvoiceItemRelationship.php
+++ app/Models/items/Traits/PaidInvoiceItemRelationship.php
@@ -3,9 +3,15 @@
 namespace App\Models\items\Traits;
 
 use App\Models\invoice\Invoice;
+use App\Models\invoice\PaidInvoice;
 
 trait PaidInvoiceItemRelationship
 {
+    public function paid_invoice()
+    {
+        return $this->belongsTo(PaidInvoice::class, 'paidinvoice_id');
+    }
+
     public function invoice()
     {
         return $this->belongsTo(Invoice::class);
