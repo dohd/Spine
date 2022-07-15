fdf6100b23ad68244fcc98ff6f8d52e24df0c880
Dohd <alloicebrian@gmail.com>
2022-07-15 12:24:45 +0300

    Add skillset class properties

--- app/Models/project/BudgetSkillset.php
+++ app/Models/project/BudgetSkillset.php
@@ -7,6 +7,33 @@ use Illuminate\Database\Eloquent\Model;
 class BudgetSkillset extends Model
 {
     /**
+     * The database table used by the model.
+     * @var string
+     */
+    protected $table = 'budget_skillsets';
+
+    /**
+     * Mass Assignable fields of model
+     * @var array
+     */
+    protected $fillable = [];
+
+    /**
+     * Default values for model fields
+     * @var array
+     */
+    protected $attributes = [];
+
+    /**
+     * Dates
+     * @var array
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at'
+    ];
+
+    /**
      * Guarded fields of model
      * @var array
      */
