2268449c4b467be036f6f8db8aa7e0ae16634a73
Dohd <alloicebrian@gmail.com>
2022-07-20 08:19:35 +0300

    Update create and update method

--- app/Repositories/Focus/product/ProductRepository.php
+++ app/Repositories/Focus/product/ProductRepository.php
@@ -2,15 +2,13 @@
 
 namespace App\Repositories\Focus\product;
 
-use App\Models\product\ProductMeta;
 use App\Models\product\ProductVariation;
 use DB;
-use Carbon\Carbon;
 use App\Models\product\Product;
 use App\Exceptions\GeneralException;
 use App\Repositories\BaseRepository;
+use DateTime;
 use Illuminate\Support\Facades\Storage;
-use Mavinoo\LaravelBatch\LaravelBatchFacade as Batch;
 
 /**
  * Class ProductRepository.
@@ -51,19 +49,18 @@ class ProductRepository extends BaseRepository
 
     public function getForDataTable()
     {
-           $q = $this->query();
+        $q = $this->query();
 
-            $q->when(request('p_rel_id') AND !request('p_rel_type'), function ($q) {
-                $q->where('sub_cat_id', '=', 0);
-                return $q->where('productcategory_id', '=', request('p_rel_id', 0));
-            });
+        $q->when(request('p_rel_id') and !request('p_rel_type'), function ($q) {
+            $q->where('sub_cat_id', '=', 0);
+            return $q->where('productcategory_id', '=', request('p_rel_id', 0));
+        });
 
-            $q->when(request('p_rel_id') AND (request('p_rel_type') == 1), function ($q) {
-                return $q->where('sub_cat_id', '=', request('p_rel_id', 0));
-            });
+        $q->when(request('p_rel_id') and (request('p_rel_type') == 1), function ($q) {
+            return $q->where('sub_cat_id', '=', request('p_rel_id', 0));
+        });
 
-            return $q->get(['id','productcategory_id','name','sub_cat_id','created_at']);
-
+        return $q->get(['id', 'productcategory_id', 'name', 'sub_cat_id', 'created_at']);
     }
 
     /**
@@ -75,58 +72,46 @@ class ProductRepository extends BaseRepository
      */
     public function create(array $input)
     {
-
+        // dd($input);
         DB::beginTransaction();
-        $product_des= strip_tags($input['main']['product_des'],config('general.allowed'));
-        $input['main'] = array_map( 'strip_tags', $input['main']);
-        $input['main']['product_des']=$product_des;
-        $product = Product::create($input['main']);
-        if ($product->id) {
-            $variations = array();
-            $i = 0;
-            $std_p = 0;
-            foreach ($input['variation']['price'] as $key => $value) {
 
-                if (!empty($input['variation']['image'][$key])) {
-                    $input['variation']['image'][$key] = $this->uploadFile($input['variation']['image'][$key]);
-                } else {
-                    $input['variation']['image'][$key] = 'example.png';
+        $data = $input['data'];
+        $data['taxrate'] = numberClean($data['taxrate']);
+        $result = Product::create($data);
+
+        // product variations
+        $data_items = $input['data_items'];
+        $data_items = array_map(function ($item) use ($result) {
+            foreach ($item as $key => $val) {
+                if (in_array($key, ['price', 'purchase_price', 'disrate', 'qty', 'alert']))
+                    $item[$key] = numberClean($val);
+                if ($key == 'image' && !empty($val))
+                    $item[$key] = $this->uploadFile($val);
+                if ($key == 'expiry') {
+                    $expiry = new DateTime(date_for_database($val));
+                    $now = new DateTime(date('Y-m-d'));
+                    if ($expiry > $now) $item[$key] = date_for_database($val);
+                    else $item[$key] = null;
                 }
-                if (!empty($input['variation']['expiry'][$key])){
-                  if(strtotime(date_for_database($input['variation']['expiry'][$key])) > strtotime(date('Y-m-d')))  $input['variation']['expiry'][$key] = date_for_database($input['variation']['expiry'][$key]);
-                }
-                if (!$input['variation']['barcode'][$key]) $input['variation']['barcode'][$key] = rand(100, 999) . rand(0, 9) . rand(1000000, 9999999) . rand(0, 9);
-
-
-                if (!$i) {
-                    $std = array('product_id' => $product->id, 'parent_id' => 0, 'variation_class' => 0, 'name' => $input['variation']['variation_name'][$key], 'warehouse_id' => $input['variation']['warehouse_id'][$key], 'code' => $input['variation']['code'][$key], 'price' => numberClean($input['variation']['price'][$key]), 'purchase_price' => numberClean($input['variation']['purchase_price'][$key]), 'disrate' => numberClean($input['variation']['disrate'][$key]), 'qty' => numberClean($input['variation']['qty'][$key]), 'alert' => numberClean($input['variation']['alert'][$key]), 'image' => $input['variation']['image'][$key], 'barcode' => $input['variation']['barcode'][$key], 'expiry' => $input['variation']['expiry'][$key], 'ins' => $product->ins);
-                    $std = array_map( 'strip_tags', $std);
-                    $std_p = ProductVariation::create($std);
-                } else {
-
-                    $variations[] = array('product_id' => $product->id, 'parent_id' => 1, 'variation_class' => 0, 'name' => strip_tags($input['variation']['variation_name'][$key]), 'warehouse_id' => $input['variation']['warehouse_id'][$key], 'code' => $input['variation']['code'][$key], 'price' => numberClean($input['variation']['price'][$key]), 'purchase_price' => numberClean($input['variation']['purchase_price'][$key]), 'disrate' => numberClean($input['variation']['disrate'][$key]), 'qty' => numberClean($input['variation']['qty'][$key]), 'alert' => numberClean($input['variation']['alert'][$key]), 'image' => $input['variation']['image'][$key], 'barcode' => $input['variation']['barcode'][$key], 'expiry' => $input['variation']['expiry'][$key], 'ins' => $product->ins);
-                }
-
-
-                $i++;
+                if ($key == 'barcode' && !$val)
+                    $item[$key] =  rand(100, 999) . rand(0, 9) . rand(1000000, 9999999) . rand(0, 9);
             }
 
+            if (!isset($item['image'])) $item['image'] = 'example.png';
+            $name = $item['variation_name'];
+            unset($item['variation_name']);
 
-            if (is_array($input['custom_field'])) {
-                if (ProductVariation::insert($variations)) {
-                    save_custom_field($input['custom_field'], $product->id, 3);
-                }
-            }
-            if (isset($input['serial']['product_serial'])) {
-                $serial = array();
-                foreach ($input['serial']['product_serial'] as $key => $value) {
-                    $serial[] = array('rel_type' => 2, 'rel_id' => 0, 'ref_id' => $std_p->id, 'value' => strip_tags($value));
-                }
-                ProductMeta::insert($serial);
-            }
-            DB::commit();
-            return $product->id;
-        }
+            return array_replace($item, [
+                'product_id' => $result->id,
+                'ins' => $result->ins,
+                'name' => $name,
+            ]);
+        }, $data_items);
+        ProductVariation::insert($data_items);
+
+        DB::commit();
+        if ($result) return $result;
+
         throw new GeneralException(trans('exceptions.backend.products.create_error'));
     }
 
@@ -138,77 +123,48 @@ class ProductRepository extends BaseRepository
      * @throws GeneralException
      * return bool
      */
-    public function update(Product $product, array $input)
+    public function update($product, array $input)
     {
+        // dd($input);
         DB::beginTransaction();
-       $pv_i=$input['main']['pv_id'];
-       unset($input['main']['pv_id']);
-        $input['main'] = array_map( 'strip_tags', $input['main']);
-        if ($product->update($input['main'])) {
-            // dd($product->id);
-            $variations = array();
-            $varriation_new = array();
-            $i = 0;
-            $parent_id = 0;
-            foreach ($input['variation']['warehouse_id'] as $key => $value) {
-                if (!empty($input['variation']['expiry'][$key])) {
-                          if(strtotime(date_for_database($input['variation']['expiry'][$key])) > strtotime(date('Y-m-d')))  $input['variation']['expiry'][$key] = date_for_database($input['variation']['expiry'][$key]);
+
+        $data = $input['data'];
+        $data['taxrate'] = numberClean($data['taxrate']);
+        $result = $product->update($data);
+
+        // create or update product variations
+        $data_items = $input['data_items'];
+        foreach ($data_items as $item) {
+            foreach ($item as $key => $val) {
+                if (in_array($key, ['price', 'purchase_price', 'disrate', 'qty', 'alert']))
+                    $item[$key] = numberClean($val);
+                if ($key == 'image' && !empty($val))
+                    $item[$key] = $this->uploadFile($val);
+                if ($key == 'expiry') {
+                    $expiry = new DateTime(date_for_database($val));
+                    $now = new DateTime(date('Y-m-d'));
+                    if ($expiry > $now) $item[$key] = date_for_database($val);
+                    else $item[$key] = null;
                 }
-                if ($input['variation']['v_id'][$key] > 0) {
-                    if (!empty($input['variation']['image'][$key])) {
-                        $input['variation']['image'][$key] = $this->uploadFile($input['variation']['image'][$key]);
-                        $variations[] = array('id' => $input['variation']['v_id'][$key], 'product_id' => $product->id, 'variation_class' => 0, 'name' => strip_tags($input['variation']['variation_name'][$key]), 'warehouse_id' => $input['variation']['warehouse_id'][$key], 'code' => $input['variation']['code'][$key], 'price' => numberClean($input['variation']['price'][$key]), 'purchase_price' => numberClean($input['variation']['purchase_price'][$key]), 'disrate' => numberClean($input['variation']['disrate'][$key]), 'qty' => numberClean($input['variation']['qty'][$key]), 'alert' => numberClean($input['variation']['alert'][$key]), 'image' => $input['variation']['image'][$key], 'barcode' => $input['variation']['barcode'][$key], 'expiry' => $input['variation']['expiry'][$key], 'ins' => $product->ins);
-                    } else {
-                        $variations[] = array('id' => $input['variation']['v_id'][$key], 'product_id' => $product->id, 'variation_class' => 0, 'name' => strip_tags($input['variation']['variation_name'][$key]), 'warehouse_id' => $input['variation']['warehouse_id'][$key], 'code' => $input['variation']['code'][$key], 'price' => numberClean($input['variation']['price'][$key]), 'purchase_price' => numberClean($input['variation']['purchase_price'][$key]), 'disrate' => numberClean($input['variation']['disrate'][$key]), 'qty' => numberClean($input['variation']['qty'][$key]), 'alert' => numberClean($input['variation']['alert'][$key]), 'barcode' => $input['variation']['barcode'][$key], 'expiry' => $input['variation']['expiry'][$key], 'ins' => $product->ins);
-                    }
-                } else {
-                    if (!empty($input['variation']['image'][$key])) {
-                        $input['variation']['image'][$key] = $this->uploadFile($input['variation']['image'][$key]);
-                    } else {
-                        $input['variation']['image'][$key] = 'example.png';
-                    }
-                    $varriation_new[] = array('product_id' => $product->id, 'parent_id' => 1, 'variation_class' => 0, 'name' => strip_tags($input['variation']['variation_name'][$key]), 'warehouse_id' => $input['variation']['warehouse_id'][$key], 'code' => $input['variation']['code'][$key], 'price' => numberClean($input['variation']['price'][$key]), 'purchase_price' => numberClean($input['variation']['purchase_price'][$key]), 'disrate' => numberClean($input['variation']['disrate'][$key]), 'qty' => numberClean($input['variation']['qty'][$key]), 'alert' => numberClean($input['variation']['alert'][$key]), 'image' => $input['variation']['image'][$key], 'barcode' => $input['variation']['barcode'][$key], 'expiry' => $input['variation']['expiry'][$key], 'ins' => $product->ins);
-                }
-                $i++;
+                if ($key == 'barcode' && !$val)
+                    $item[$key] =  rand(100, 999) . rand(0, 9) . rand(1000000, 9999999) . rand(0, 9);
             }
+            if (!isset($item['image'])) $item['image'] = 'example.png';
 
-            $update_variation = new ProductVariation;
-            $index = 'id';
-            Batch::update($update_variation, $variations, $index);
-            if (isset($varriation_new[0])) {
-                ProductVariation::insert($varriation_new);
-            }
-            if (isset($input['variation']['remove_v'])) {
-                ProductVariation::whereIn('id', $input['variation']['remove_v'])->delete();
-            }
-
-            if (isset($input['serial']['product_serial'])) {
-                $serial = array();
-                foreach ($input['serial']['product_serial'] as $key => $value) {
-                    $serial[] = array('rel_type' => 2, 'rel_id' => 0, 'ref_id' => $pv_i, 'value' => strip_tags($value));
-                }
-                ProductMeta::insert($serial);
-            }
-
-            if (is_array($input['custom_field'])) {
-                $input['custom_field']['ins'] = $product->ins;
-                update_custom_field($input['custom_field'], $product->id, 3);
-            }
-
-            if (isset($input['product_serial']['product_serial_e'])) {
-                $update_serial = new ProductMeta();
-                $index = 'id';
-                $serials = array();
-                foreach ($input['product_serial']['product_serial_e'] as $key => $value) {
-                    $serials[] = array('id' => $key, 'value' => strip_tags($value));
-                }
-                Batch::update($update_serial, $serials, $index);
-            }
-
-
-            DB::commit();
-            return true;
+            $item = array_replace($item, [
+                'product_id' => $product->id,
+                'ins' => $product->ins,
+                'name' => $item['variation_name'],
+            ]);
+            $new_item = ProductVariation::firstOrNew(['id' => $item['v_id']]);
+            $new_item->fill($item);
+            if (!$new_item->id) unset($new_item->id);
+            unset($new_item->v_id, $new_item->variation_name);
+            $new_item->save();
         }
+
+        DB::commit();
+        if ($result) return $result;
 
         throw new GeneralException(trans('exceptions.backend.products.update_error'));
     }
@@ -222,10 +178,8 @@ class ProductRepository extends BaseRepository
      */
     public function delete(Product $product)
     {
-        if ($product->delete()) {
-            return true;
-        }
-
+        if ($product->delete()) return true;
+            
         throw new GeneralException(trans('exceptions.backend.products.delete_error'));
     }
 
@@ -234,11 +188,8 @@ class ProductRepository extends BaseRepository
 */
     public function uploadFile($file)
     {
-        $path = $this->file_path;
-
-        $file_name = time() . $file->getClientOriginalName();
-
-        $this->storage->put($path . $file_name, file_get_contents($file->getRealPath()));
+        $file_name = $this->file_path . time() . $file->getClientOriginalName();
+        $this->storage->put($file_name, file_get_contents($file->getRealPath()));
 
         return $file_name;
     }
@@ -248,18 +199,11 @@ class ProductRepository extends BaseRepository
     */
     public function removePicture(Product $file, $type)
     {
-        $path = $this->file_path;
-
-        if ($file->$type && $this->storage->exists($path . $file->$type)) {
-            $this->storage->delete($path . $file->$type);
-        }
-
-        $result = $file->update([$type => null]);
-
-        if ($result) {
-            return true;
-        }
-
+        if ($file->$type && $this->storage->exists( $this->file_path . $file->$type)) 
+            $this->storage->delete( $this->file_path . $file->$type);
+        
+        if ($file->update([$type => null])) return true;
+            
         throw new GeneralException(trans('exceptions.backend.settings.update_error'));
     }
 }
