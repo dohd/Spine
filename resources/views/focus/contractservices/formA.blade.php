7f6543edc4292d2aff277f3710a7649beae3503d
Dohd <alloicebrian@gmail.com>
2022-07-26 15:05:52 +0300

    Update contract service form

--- resources/views/focus/contractservices/formA.blade.php
+++ resources/views/focus/contractservices/formA.blade.php
@@ -1,19 +1,19 @@
 <div class="form-group row">
     <div class="col-4">
         <label for="client">Customer</label>
-        <select name="customer_id" id="customer" class="form-control"></select>
+        <select name="customer_id" id="customer" class="form-control" data-placeholder="Choose Client"></select>
     </div>
     <div class="col-2">
         <label for="branch">Branch</label>
-        <select name="branch_id" id="branch" class="form-control"></select>
+        <select name="branch_id" id="branch" class="form-control" data-placeholder="Choose Branch"></select>
     </div>
     <div class="col-4">
         <label for="contract">Contract</label>
-        <select name="contract_id" id="contract" class="form-control"></select>
+        <select name="contract_id" id="contract" class="form-control" data-placeholder="Choose Contract"></select>
     </div>
     <div class="col-2">
         <label for="schedule">Schedule</label>
-        <select name="schedule_id" id="schedule" class="form-control"></select>
+        <select name="schedule_id" id="schedule" class="form-control" data-placeholder="Choose Schedule"></select>
     </div>
 </div>
 <div class="form-group row">
@@ -31,40 +31,53 @@
     </div>
 </div>
 <div class="table-reponsive">
-    <table id="equipmentTbl" class="table text-center">
+    <table id="equipTbl" class="table text-center">
         <thead>
             <tr class="bg-gradient-directional-blue white">
                 <th>System ID</th>
-                <th>Location</th>
                 <th>Description</th>                                           
+                <th>Location</th>
                 <th>Rate</th>
                 <th>Status</th>
                 <th>Bill</th>
                 <th width="30%">Note</th>
+                <th>Action</th>
             </tr>
         </thead>
         <tbody>        
             <tr>                                                    
-                <td>Eq-0001</td>
-                <td>ATM ROOM</td>                                                    
-                <td>456; 1; 24000; R22; LG HIGHWALL</td> 
-                <td>1,586.69</td>	
+                <td><span id="tid-0"></span></td>                                                               
                 <td>
-                    <select name="status[]" class="custom-select" id="">
+                    <textarea class="form-control" name="description" id="descr-0" cols="20" required></textarea>
+                </td> 
+                <td><span id="location-0"></span></td>     
+                <td><span id="rate-0"></span></td>	
+                <td>
+                    <select name="status[]" class="custom-select" id="status-0">
                         @foreach (['working', 'faulty', 'cannibalised', 'decommissioned'] as $val)
                             <option value="{{ $val }}">{{ ucfirst($val) }}</option>
                         @endforeach
                     </select>                                                   
                 </td>
                 <td>
-                    <select name="billed[]" class="custom-select" id="">
+                    <select name="bill[]" class="custom-select" id="bill-0">
                         @foreach (['No', 'Yes'] as $k => $val)
                             <option value="{{ $k }}" {{ $k? 'selected' : ''}}>{{ $val }}</option>
                         @endforeach
                     </select>     
                 </td>                       
-                <td><input type="text" class="form-control" name="note[]"></td>                   
+                <td><input type="text" class="form-control" name="note[]" id="note-0"></td>    
+                <td><a href="javascript:" class="btn btn-light del"><i class="danger fa fa-trash fa-lg"></i></a></td> 
+                <input type="hidden" name="equipment_id[]" id="equipmentid-0">           
             </tr>                                                        
         </tbody>
     </table>
+</div>
+<a href="javascript:" class="btn btn-success" aria-label="Left Align" id="add_equip">
+    <i class="fa fa-plus-square"></i> Add Equipment
+</a>
+<div class="form-group row mt-1">
+    <div class="col-12">
+        {{ Form::submit('Submit', ['class' => 'btn btn-primary btn-lg float-right']) }}
+    </div>
 </div>
