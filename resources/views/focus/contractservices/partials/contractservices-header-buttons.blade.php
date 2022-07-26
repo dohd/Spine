25e88adf0ddfdace524a9edce18a57a450c0c7ef
Dohd <alloicebrian@gmail.com>
2022-07-26 15:06:36 +0300

    Update contractservice header buttons

--- resources/views/focus/contractservices/partials/contractservices-header-buttons.blade.php
+++ resources/views/focus/contractservices/partials/contractservices-header-buttons.blade.php
@@ -2,4 +2,9 @@
     <a href="{{ route('biller.contractservices.index') }}" class="btn btn-info  btn-lighten-2">
         <i class="fa fa-list-alt"></i> {{trans( 'general.list' )}}
     </a>
+    @permission('product-create' )
+    <a href="{{ route('biller.contractservices.create' ) }}" class="btn btn-pink  btn-lighten-3">
+        <i class="fa fa-plus-circle"></i> {{trans( 'general.create' )}}
+    </a>
+    @endauth
 </div>
