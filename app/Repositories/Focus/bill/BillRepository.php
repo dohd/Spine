ea2a2ff2ab01fd20579237b43de1bb4e468040a8
Dohd <alloicebrian@gmail.com>
2022-07-29 14:38:52 +0300

    Update create_kra method

--- app/Repositories/Focus/bill/BillRepository.php
+++ app/Repositories/Focus/bill/BillRepository.php
@@ -7,9 +7,12 @@ use App\Models\account\Account;
 use App\Repositories\BaseRepository;
 use App\Models\bill\Bill;
 use App\Models\bill\Paidbill;
+use App\Models\billitem\BillItem;
 use App\Models\items\PaidbillItem;
+use App\Models\supplier\Supplier;
 use App\Models\transaction\Transaction;
 use App\Models\transactioncategory\Transactioncategory;
+use App\Repositories\Focus\purchase\PurchaseRepository;
 use Illuminate\Support\Facades\DB;
 use Mavinoo\LaravelBatch\LaravelBatchFacade as Batch;
 
@@ -91,7 +94,7 @@ class BillRepository extends BaseRepository
         throw new GeneralException(trans('exceptions.backend.purchaseorders.create_error'));
     }
 
-    // create bill transaction 
+    // bill payment transaction 
     public function post_transaction($bill)
     {
         // credit Bank (Income)
@@ -130,6 +133,92 @@ class BillRepository extends BaseRepository
      */
     public function create_kra(array $input)
     {
+        // dd($input);
+        DB::beginTransaction();
+
+        $data = (object) $input;
+        $data->amount = numberClean($data->amount);
+        $data->reg_date = date_for_database($data->reg_date);
+        $note = implode(' - ', [
+            $data->reg_no, $data->payment_type, $data->tax_type, $data->tax_period, 
+            $data->amount
+        ]);
+
+        $supplier = Supplier::find($data->supplier_id);
+        $bill_data = [
+            'date' => $data->reg_date,
+            'due_date' => $data->reg_date,
+            'supplier_type' => 'supplier',
+            'supplier_id' => $supplier->id,
+            'supplier_taxid' => $supplier->taxid,
+            'expense_subttl' => $data->amount,
+            'expense_grandttl' => $data->amount,
+            'paidttl' => $data->amount,
+            'grandttl' => $data->amount,
+            'note' => (!$data->note? $note : $note . ' - ' . $data->note),
+            'ins' => auth()->user()->ins,
+            'user_id' => auth()->user()->id,
+            'doc_ref_type' => 'Receipt',
+            'doc_ref' => $data->reg_no,
+            'suppliername' => $supplier->name,
+            'tid' => $data->tid,
+        ];
+        $result = Bill::create($bill_data);
+
+        $item_data = [
+            'bill_id' => $result->id,
+            'description' => $result->note,
+            'qty' => 1,
+            'rate' => $result->grandttl,
+            'amount' => $result->grandttl,
+            'ins' => $result->ins,
+            'user_id' => $result->user_id,
+            'type' => 'Expense',
+        ];
+        BillItem::create($item_data);
+
+        /** accounting */
+        $this->post_kra_transaction($result);
+
+        DB::commit();
+        if ($result) return $result;
+
         throw new GeneralException(trans('exceptions.backend.purchaseorders.create_error'));
     }
+
+    // create kra bill transaction
+    public function post_kra_transaction($bill) 
+    {
+        // credit Accounts Payable (Creditors)
+        $account = Account::where('system', 'payable')->first(['id']);
+        $tr_category = Transactioncategory::where('code', 'bill')->first(['id', 'code']);
+        $tid = Transaction::max('tid') + 1;
+        $cr_data = [
+            'tid' => $tid,
+            'account_id' => $account->id,
+            'trans_category_id' => $tr_category->id,
+            'credit' => $bill->grandttl,
+            'tr_date' => $bill->date,
+            'due_date' => $bill->due_date,
+            'user_id' => $bill->user_id,
+            'note' => $bill->note,
+            'ins' => $bill->ins,
+            'tr_type' => $tr_category->code,
+            'tr_ref' => $bill->id,
+            'user_type' => 'supplier',
+            'is_primary' => 1,
+        ];
+        Transaction::create($cr_data);
+
+        // debit Expense account
+        $account = Account::where('system', 'kra_tax')->first(['id']);
+        unset($cr_data['credit'], $cr_data['is_primary']);
+        $item = $bill->items->first();
+        $dr_data = array_replace($cr_data, [
+            'account_id' => $account->id,
+            'debit' => $item->amount,
+        ]);
+        Transaction::create($dr_data); 
+        aggregate_account_transactions();
+    }    
 }
