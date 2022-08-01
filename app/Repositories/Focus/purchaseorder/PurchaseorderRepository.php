aa428a344beebe94c1d8f879897d39e403b72133
Dohd <alloicebrian@gmail.com>
2022-08-01 12:46:18 +0300

    Update grn methods

--- app/Repositories/Focus/purchaseorder/PurchaseorderRepository.php
+++ app/Repositories/Focus/purchaseorder/PurchaseorderRepository.php
@@ -153,9 +153,19 @@ class PurchaseorderRepository extends BaseRepository
      */
     public function delete($purchaseorder)
     {
-        if ($purchaseorder->delete()) return true;         
-               
-        throw new GeneralException(trans('exceptions.backend.purchaseorders.delete_error'));
+        try {
+            DB::beginTransaction();
+
+            $purchaseorder->transactions()->delete();
+            aggregate_account_transactions();
+            $purchaseorder->delete();
+
+            DB::commit();
+            return true;
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            throw new GeneralException(trans('exceptions.backend.purchaseorders.delete_error'));
+        }     
     }
 
     
@@ -168,80 +178,88 @@ class PurchaseorderRepository extends BaseRepository
         DB::beginTransaction();
 
         $order = $input['order'];
+        foreach ($order as $key => $val) {
+            $keys = ['purchaseorder_id', 'stock_grn', 'expense_grn', 'asset_grn', 'ins', 'user_id'];
+            if (!in_array($key, $keys)) $order[$key] = numberClean($val);
+        }
         $grn = Grn::create($order);    
 
         $order_items = $input['order_items'];
-        foreach ($order_items as $k => $item) {
-            if (!isset($item['qty'])) $item['qty'] = 0;
-            $order_items[$k] = array_replace($item, [
+        $order_items = array_map(function ($v) use($grn) {
+            if (empty($v['qty'])) $v['qty'] = 0;
+            return array_replace($v, [
                 'grn_id' => $grn->id,
-                'ins' => $order['ins'],
-                'user_id' => $order['user_id'],
-                'date' => date_for_database($item['date']),
+                'ins' => $grn->ins,
+                'user_id' => $grn->user_id,
+                'date' => date_for_database($v['date']),
             ]);
-        }
+        }, $order_items);
         GrnItem::insert($order_items);
 
-        // increase stock
+        // create bill
+        $bill_data = collect([$grn])->map(function ($v) use($grn) {
+            $v = array_diff_key($v->toArray(), array_flip(['id', 'purchaseorder_id', 'stock_grn', 'expense_grn', 'asset_grn']));
+            $po = $grn->purchaseorder;
+            $v = array_replace($v, [
+                'tid' => Bill::max('tid') + 1,
+                'date' => date('Y-m-d'), 
+                'due_date' => date('Y-m-d'), 
+                'supplier_type' => 'supplier',
+                'supplier_id' => $po->supplier_id,
+                'supplier_taxid' => $po->supplier_taxid,
+                'project_id' => $po->project_id,
+                'doc_ref_type' => $po->doc_ref_type,
+                'doc_ref' => $po->doc_ref,
+                'po_id' => $po->id,
+                'tax' => $po->tax,
+                'note' => $po->note,
+            ]);
+            return $v;
+        })->first();
+        $bill = Bill::create($bill_data);
+
+        // create bill items from grn items
+        $bill_items_data = $grn->items->map(function ($v) use($bill) {
+            $po_item = $v->purchaseorder_item;
+            $rate_per_item = $po_item->taxrate / $po_item->qty;
+            $amount_per_item = $po_item->amount / $po_item->qty;
+
+            $item = array_diff_key($po_item->toArray(), array_flip(['id', 'uom', 'purchaseorder_id']));
+            $item = array_replace($item, [
+                'bill_id' => $bill->id,
+                'qty' => $v->qty,
+                'taxrate' => $rate_per_item * $v->qty,
+                'amount' => $amount_per_item * $v->qty,
+            ]);
+            return $item;
+        })->toArray();
+        BillItem::insert($bill_items_data);
+
+        // increase inventory per received items
         foreach ($grn->items as $item) {
-            $product = $item->purchaseorder_item->product;
-            if ($product) $product->increment('qty', $item->qty);
+            $po_item = $item->purchaseorder_item;
+            if ($po_item->type == 'Stock' && $po_item->product) {
+                $po_item->product->increment('qty', $item->qty);
+            }
         }
 
         // update purchaseorder status
         $grn_qty = $purchaseorder->grn_items->sum('qty');
-        if ($grn_qty) {
-            $order_qty = $purchaseorder->items->sum('qty');
-            if ($grn_qty < $order_qty) $purchaseorder->update(['status' => 'Partial']);
-            else $purchaseorder->update(['status' => 'Complete']);
-        }
+        $order_qty = $purchaseorder->items->sum('qty');
+        if ($order_qty > $grn_qty) $purchaseorder->update(['status' => 'Partial']);
+        else $purchaseorder->update(['status' => 'Complete']);
 
-        // create bill
-        $exclude_keys = ['id', 'purchaseorder_id', 'stock_grn', 'expense_grn', 'asset_grn', 'items'];
-        $bill_inp = array_diff_key($grn->toArray(), array_flip($exclude_keys));
-        $po = $grn->purchaseorder;
-        $bill_inp = $bill_inp + [
-            'date' => date('Y-m-d'), 
-            'due_date' => $po->due_date, 
-            'supplier_type' => 'supplier',
-            'supplier_id' => $po->supplier_id,
-            'supplier_taxid' => $po->supplier_taxid,
-            'project_id' => $po->project_id,
-            'doc_ref_type' => $po->doc_ref_type,
-            'doc_ref' => $po->doc_ref,
-            'po_id' => $po->id,
-            'tax' => $po->tax,
-            'note' => $po->note,
-        ];
-        $bill['tid'] = Bill::max('tid') + 1;
-        $bill = Bill::create($bill_inp);
-
-        // create bill items
-        $bill_items = array();
-        foreach ($grn->items as $item) {
-            $poitem = $item->purchaseorder_item->toArray();
-            $bill_item = array_diff_key($poitem, array_flip(['id', 'uom', 'purchaseorder_id', 'product']));
-            $bill_item = array_replace($bill_item, [
-                'bill_id' => $bill->id,
-                'qty' => $item->qty,
-                'taxrate' => ($poitem['taxrate'] / $poitem['qty'] * $item->qty),
-                'amount' => ($poitem['amount'] / $poitem['qty'] * $item->qty),
-            ]);
-            $bill_items[] = $bill_item;
-        }
-        BillItem::insert($bill_items);
-
-        // accounting
+        /**accounting */
         $this->post_transaction($bill);
 
         DB::commit();
-        if ($grn) return true;
+        if ($grn) return $grn;
     }
 
     /**
      * Post Account Transaction
      */
-    protected function post_transaction($order) 
+    protected function post_transaction($bill) 
     {
         // credit Accounts Payable (Creditors) 
         $account = Account::where('system', 'payable')->first(['id']);
@@ -251,58 +269,55 @@ class PurchaseorderRepository extends BaseRepository
             'tid' => $tid,
             'account_id' => $account->id,
             'trans_category_id' => $tr_category->id,
-            'credit' => $order['grandttl'],
-            'tr_date' => date('Y-m-d'),
-            'due_date' => $order['due_date'],
-            'user_id' => $order['user_id'],
-            'note' => $order->note,
-            'ins' => $order['ins'],
+            'credit' => $bill->grandttl,
+            'tr_date' => $bill->date,
+            'due_date' => $bill->due_date,
+            'user_id' => $bill->user_id,
+            'note' => $bill->note,
+            'ins' => $bill->ins,
             'tr_type' => $tr_category->code,
-            'tr_ref' => $order->id,
+            'tr_ref' => $bill->id,
             'user_type' => 'supplier',
             'is_primary' => 1
         ];
-        $tr = Transaction::create($cr_data);
-        $order->update(['tr_ref' => $tr->id]);
+        Transaction::create($cr_data);
 
         $dr_data = array();
-        // debit stock account
+        // debit Inventory/Stock Account
         unset($cr_data['credit'], $cr_data['is_primary']);
-        $is_stock = $order->items()->where('type', 'Stock')->count();
+        $is_stock = $bill->items()->where('type', 'Stock')->count();
         if ($is_stock) {
             $account = Account::where('system', 'stock')->first(['id']);
             $dr_data[] = array_replace($cr_data, [
                 'account_id' => $account->id,
-                'debit' => $order['stock_subttl'],
+                'debit' => $bill->stock_subttl,
             ]);    
         }
-        // debit expense and asset account
-        foreach ($order->items as $item) {
-            $subttl = $item['amount'] - $item['taxrate'];
-            if ($item['type'] == 'Expense') {
+        // debit Expense and Asset Account
+        foreach ($bill->items as $item) {
+            $subttl = $item->amount - $item->taxrate;
+            if ($item->type == 'Expense') {
                 $dr_data[] = array_replace($cr_data, [
-                    'account_id' => $item['item_id'],
+                    'account_id' => $item->item_id,
                     'debit' => $subttl,
                 ]);
-            }
-            if ($item['type'] == 'Asset') {
-                $asset = Assetequipment::find($item['item_id']);
+            } elseif ($item->type == 'Asset') {
+                $asset = Assetequipment::find($item->item_id);
                 $dr_data[] = array_replace($cr_data, [
                     'account_id' => $asset->account_id,
                     'debit' => $subttl,
                 ]);
             }
         }
-        // debit tax
-        if ($order['grandtax'] > 0) {
+        // debit Tax
+        if ($bill->grandtax > 0) {
             $account = Account::where('system', 'tax')->first(['id']);
             $dr_data[] = array_replace($cr_data, [
                 'account_id' => $account->id, 
-                'debit' => $order['grandtax'],
+                'debit' => $bill->grandtax,
             ]);
         }
         Transaction::insert($dr_data); 
-        // update account ledgers debit and credit totals
         aggregate_account_transactions();
     }
 }
