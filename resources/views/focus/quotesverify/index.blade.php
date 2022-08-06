517126cfb5431cc69f12e8ff2cf57a9094da4a7b
Dohd <alloicebrian@gmail.com>
2022-08-06 10:17:35 +0300

    Add filter for quotesverify page

--- resources/views/focus/quotesverify/index.blade.php
+++ resources/views/focus/quotesverify/index.blade.php
@@ -29,19 +29,31 @@
                         <div class="card-content">
                             <div class="card-body">
                                 <div class="row">
-                                    <div class="col-md-2">{{ trans('general.search_date')}} </div>
-                                    <div class="col-md-2">
+                                    <div class="col-2">
+                                        <select name="verify_state" id="verify_state" class="custom-select">
+                                            <option value="">-- Verification State--</option>
+                                            @foreach (['yes' => 'verified', 'no' => 'unverified'] as $key => $val)
+                                                <option value="{{ ucfirst($key) }}">{{ ucfirst($val) }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                </div>
+                                <br>
+                                <div class="row">
+                                    <div class="col-2">{{ trans('general.search_date')}} </div>
+                                    <div class="col-2">
                                         <input type="text" name="start_date" id="start_date" class="form-control datepicker date30  form-control-sm" autocomplete="off" />
                                     </div>
-                                    <div class="col-md-2">
+                                    <div class="col-2">
                                         <input type="text" name="end_date" id="end_date" class="form-control datepicker form-control-sm" autocomplete="off" />
                                     </div>
-                                    <div class="col-md-2">
+                                    <div class="col-2">
                                         <input type="button" name="search" id="search" value="Search" class="btn btn-info btn-sm" />
                                     </div>
                                 </div>
+                                
                                 <hr>
-                                <table id="quotes-table" class="table table-striped table-bordered zero-configuration" cellspacing="0" width="100%">
+                                <table id="quotesTbl" class="table table-striped table-bordered zero-configuration" cellspacing="0" width="100%">
                                     <thead>
                                         <tr>
                                             <th>#</th>
@@ -58,7 +70,9 @@
                                     </thead>
                                     <tbody>
                                         <tr>
-                                            <td colspan="8" class="text-center text-success font-large-1"><i class="fa fa-spinner spinner"></i></td>
+                                            <td colspan="100%" class="text-center text-success font-large-1">
+                                                <i class="fa fa-spinner spinner"></i>
+                                            </td>
                                         </tr>
                                     </tbody>
                                 </table>
@@ -73,119 +87,109 @@
 @endsection
 
 @section('after-scripts')
-{{-- For DataTables --}}
 {{ Html::script(mix('js/dataTable.js')) }}
 <script>
-    $.ajaxSetup({
-        headers: { 'X-CSRF-TOKEN': "{{ csrf_token() }}" }
-    });
-    setTimeout(() => draw_data(), "{{ config('master.delay') }}");
+    const config = {
+        ajaxSetup: {
+            headers: { 'X-CSRF-TOKEN': "{{ csrf_token() }}" }
+        },
+        datepicker: {format: "{{ config('core.user_date_format') }}", autoHide: true}
+    };
 
-    $('#search').click(function() {
-        var start_date = $('#start_date').val();
-        var end_date = $('#end_date').val();
-        if (start_date && end_date) {
-            $('#quotes-table').DataTable().destroy();
-            return draw_data(start_date, end_date);
-        } 
-        alert("Date range is Required");            
-    });
+    const Index = {
+        init(config) {
+            $.ajaxSetup(config.ajaxSetup);
+            $('.datepicker').datepicker(config.datepicker).datepicker('setDate', new Date());
 
-    $('.datepicker')
-    .datepicker({format: "{{ config('core.user_date_format') }}", autoHide: true})
-    .datepicker('setDate', new Date());
+            $('#verify_state').change(this.verifyStateChange);
+            $('#search').click(this.searchDateClick);
+            this.drawDataTable();
+        },
 
-    function draw_data(start_date = '', end_date = '') {
-        const language = { @lang('datatable.strings') };
-        const table = $('#quotes-table').dataTable({
-            processing: true,
-            responsive: true,
-            stateSave: true,
-            language,
-            ajax: {
-                url: '{{ route("biller.quotes.get_project") }}',
-                type: 'post',
-                data: {
-                    start_date: start_date,
-                    end_date: end_date,
-                    pi_page: location.href.includes('page=pi') ? 1 : 0
+        verifyStateChange() {
+            const el = $(this);
+            $('#quotesTbl').DataTable().destroy();
+            return Index.drawDataTable({verify_state: el.val()});
+        },
+
+        searchDateClick() {
+            const start_date = $('#start_date').val();
+            const end_date = $('#end_date').val();
+            if (start_date && end_date) {
+                $('#quotesTbl').DataTable().destroy();
+                return Index.drawDataTable({start_date, end_date});
+            } 
+            alert("Date range required!");    
+        },
+
+        drawDataTable(params={}) {
+            $('#quotesTbl').dataTable({
+                processing: true,
+                responsive: true,
+                stateSave: true,
+                language: {@lang('datatable.strings')},
+                ajax: {
+                    url: '{{ route("biller.quotes.get_project") }}',
+                    type: 'post',
+                    data: {
+                        ...params,
+                        pi_page: location.href.includes('page=pi') ? 1 : 0
+                    },
                 },
-            },
-            columns: [{
-                    data: 'DT_Row_Index',
-                    name: 'id'
-                },
-                {
-                    data: 'customer',
-                    name: 'customer'
-                },
-                {
-                    data: 'tid',
-                    name: 'tid'
-                },
-                {
-                    data: 'notes',
-                    name: 'notes'
-                },
-                {
-                    data: 'total',
-                    name: 'total'
-                },
-                {
-                    data: 'verified_total',
-                    name: 'verified_total'
-                },
-                {
-                    data: 'project_tid',
-                    name: 'project_tid'
-                },
-                {
-                    data: 'lpo_number',
-                    name: 'lpo_number'
-                },
-                {
-                    data: 'client_ref',
-                    name: 'client_ref'
-                },
-                {
-                    data: 'actions',
-                    name: 'actions',
-                    searchable: false,
-                    sortable: false
-                }
-            ],
-            columnDefs: [
-                { type: "custom-number-sort", targets: [4, 5] },
-            ],
-            order: [[0, "desc"]],
-            searchDelay: 500,
-            dom: 'Blfrtip',
-            buttons: {
-                buttons: [
-                    {
-                        extend: 'csv',
-                        footer: true,
-                        exportOptions: {
-                            columns: [0, 1]
-                        }
+                columns: [{
+                        data: 'DT_Row_Index',
+                        name: 'id'
                     },
                     {
-                        extend: 'excel',
-                        footer: true,
-                        exportOptions: {
-                            columns: [0, 1]
-                        }
+                        data: 'customer',
+                        name: 'customer'
                     },
                     {
-                        extend: 'print',
-                        footer: true,
-                        exportOptions: {
-                            columns: [0, 1]
-                        }
+                        data: 'tid',
+                        name: 'tid'
+                    },
+                    {
+                        data: 'notes',
+                        name: 'notes'
+                    },
+                    {
+                        data: 'total',
+                        name: 'total'
+                    },
+                    {
+                        data: 'verified_total',
+                        name: 'verified_total'
+                    },
+                    {
+                        data: 'project_tid',
+                        name: 'project_tid'
+                    },
+                    {
+                        data: 'lpo_number',
+                        name: 'lpo_number'
+                    },
+                    {
+                        data: 'client_ref',
+                        name: 'client_ref'
+                    },
+                    {
+                        data: 'actions',
+                        name: 'actions',
+                        searchable: false,
+                        sortable: false
                     }
-                ]
-            }
-        });
-    }
+                ],
+                columnDefs: [
+                    { type: "custom-number-sort", targets: [4, 5] },
+                ],
+                order: [[0, "desc"]],
+                searchDelay: 500,
+                dom: 'Blfrtip',
+                buttons: [ 'csv', 'excel', 'print']
+            });
+        }
+    };
+
+    $(() => Index.init(config));
 </script>
 @endsection
