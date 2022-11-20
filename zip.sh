ac6cc77c59af5fbc42b62c75c5547bc7e76bd616
Dohd <alloicebrian@gmail.com>
2022-11-20 11:39:52 +0300

    Update zip script

--- zip.sh
+++ zip.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # compress core directories; app, routes and views
 echo ========== compressing app ===========
-zip -rq ~/Downloads/app ./app
+zip -rq ~/Downloads/app app
 echo ========== compressing routes ===========
-zip -rq ~/Downloads/routes ./routes
+zip -rq ~/Downloads/routes routes
 echo ========== compressing views ===========
-zip -rq ~/Downloads/views ./resources/views
+cd resources && zip -rq ~/Downloads/views views
