c3d07ad8298318df2d2bdd2f37f407e9b2d10b94
Dohd <alloicebrian@gmail.com>
2022-12-21 17:26:43 +0300

    Remove timestamps in project_quote model

--- app/Models/project/ProjectQuote.php
+++ app/Models/project/ProjectQuote.php
@@ -8,10 +8,11 @@ class ProjectQuote extends Model
 {
     protected $table = 'project_quotes';
 
-    protected $fillable = [
-        'project_id', 'quote_id'
-    ];
+    protected $fillable = ['project_id', 'quote_id'];
 
+    public $timestamps = false;
+
+    // relation
     public function project()
     {
         return $this->belongsTo(Project::class);
