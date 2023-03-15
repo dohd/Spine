d2e5245b63fe1fdbba11cbd9fb42925ffcba9f3d
Dohd <alloicebrian@gmail.com>
2023-03-15 21:40:53 +0300

    Update data import portal

--- app/Http/Controllers/Focus/import/ImportController.php
+++ app/Http/Controllers/Focus/import/ImportController.php
@@ -1,39 +1,14 @@
 <?php
-/*
- * Rose Business Suite - Accounting, CRM and POS Software
- * Copyright (c) UltimateKode.com. All Rights Reserved
- * ***********************************************************************
- *
- *  Email: support@ultimatekode.com
- *  Website: https://www.ultimatekode.com
- *
- *  ************************************************************************
- *  * This software is furnished under a license and may be used and copied
- *  * only  in  accordance  with  the  terms  of such  license and with the
- *  * inclusion of the above copyright notice.
- *  * If you Purchased from Codecanyon, Please read the full License from
- *  * here- http://codecanyon.net/licenses/standard/
- * ***********************************************************************
- */
 
 namespace App\Http\Controllers\Focus\import;
 
 use App\Http\Requests\Focus\report\ManageReports;
-use App\Imports\AccountsImport;
-use App\Imports\CustomersImport;
-use App\Imports\ProductsImport;
-use App\Imports\TransactionsImport;
-use App\Imports\EquipmentsImport;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 use Maatwebsite\Excel\Facades\Excel;
 use App\Http\Controllers\Controller;
 use App\Http\Responses\ViewResponse;
-use App\Imports\ClientPricelistImport;
-use App\Imports\SupplierPricelistImport;
-use App\Models\equipmentcategory\EquipmentCategory;
 use DB;
-use Error;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
 
@@ -42,6 +17,7 @@ use Illuminate\Validation\ValidationException;
  */
 class ImportController extends Controller
 {
+    // temp upload
     private $upload_temp;
 
     public function __construct()
@@ -54,7 +30,7 @@ class ImportController extends Controller
      */
     public function index(ManageReports $request, $type)
     {
-        $titles = [
+        $labels = [
             'customers' => trans('import.import_customers'),
             'products' => trans('import.import_products'),
             'accounts' => trans('import.import_accounts'),
@@ -63,36 +39,18 @@ class ImportController extends Controller
             'client_pricelist' => 'Import Client Pricelist',
             'supplier_pricelist' => 'Import Supplier Pricelist',
         ];
-        $data = ['title' => $titles[$type]] + compact('type');
-            
+        $data = ['title' => $labels[$type], 'type' => $type];
+
+        if ($type == 'products') {
+            $data['warehouses'] = \App\Models\warehouse\Warehouse::get(['id', 'title']);
+            $data['product_categories'] = \App\Models\productcategory\Productcategory::get(['id', 'title']);
+        }
+        
         return new ViewResponse('focus.import.index', compact('data'));
     }
 
     /**
-     * Download sample template
-     */
-    public function sample_template($file_name)
-    {
-        $file_path = public_path() . '/storage/app/public/sample/' . $file_name . '.csv';
-        $file_exists = file_exists($file_path);
-        if (!$file_exists) throw ValidationException::withMessages(['Template file does not exist!']);
-        
-        // generate equipment_categories csv file
-        if ($file_name == 'equipment_categories') {
-            $categories = EquipmentCategory::all();
-            $fw = fopen($file_path, 'w');
-            fputcsv($fw, ['equipment_category_id', 'name']);
-            foreach ($categories as $row) {
-                fputcsv($fw, [$row->id, $row->name]);
-            }
-            fclose($fw);
-        }
-
-        return response(file_get_contents($file_path), 200, ['Content-Type' => 'text/csv']);
-    }    
-
-    /**
-     * store template data in storage 
+     * Process and Store imported data
      */
     public function store(Request $request, $type)
     {
@@ -106,63 +64,52 @@ class ImportController extends Controller
 
         $file = $request->file('import_file');
         $file_name = $file->getClientOriginalName();
-
         if (preg_match('/[\'^£$%&*()}{@#~?><>,|=+¬]/', $file_name))
             throw ValidationException::withMessages(['Remove special characters from file name!']);
 
         $file_name = preg_replace('/\s+/', '', $file_name);
         $filename = date('Ymd_his') . rand(9999, 99999) . $file_name;
 
+        // temp storage
         $path = 'temp' . DIRECTORY_SEPARATOR;
         $is_success = $this->upload_temp->put($path . $filename, file_get_contents($file->getRealPath()));
-        
-        return new ViewResponse('focus.import.import_progress', compact('filename', 'is_success', 'data'));
-    }    
+        if (!$is_success) throw ValidationException::withMessages(['Something went wrong try again later!']);
 
-    /**
-     * Process imported template
-     */
-    public function process_template(ManageReports $request)
-    {   
-        $data = $request->except('_token');
+        // process file
         $data['ins'] = auth()->user()->ins;
-
-        $filename = $request->name;
-        $path = 'temp' . DIRECTORY_SEPARATOR;
-        $file_exists = Storage::disk('public')->exists($path . $filename);
-        if (!$file_exists) throw new Error('Data processing failed! File import was unsuccessful');
-
         $models = [
-            'customer' => new CustomersImport($data),
-            'products' => new ProductsImport($data),
-            'accounts' => new AccountsImport($data),
-            'transactions' => new TransactionsImport($data),
-            'equipments' => new EquipmentsImport($data),
-            'client_pricelist' => new ClientPricelistImport($data),
-            'supplier_pricelist' => new SupplierPricelistImport($data),
+            'customer' => new \App\Imports\CustomersImport($data),
+            'products' => new \App\Imports\ProductsImport($data),
+            'accounts' => new \App\Imports\AccountsImport($data),
+            'transactions' => new \App\Imports\TransactionsImport($data),
+            'equipments' => new \App\Imports\EquipmentsImport($data),
+            'client_pricelist' => new \App\Imports\ClientPricelistImport($data),
+            'supplier_pricelist' => new \App\Imports\SupplierPricelistImport($data),
         ];
 
-        $storage_path = Storage::disk('public')->path($path . $filename);
+        $file_path = $path . $filename;
         $model = $models[$data['type']];
+
+        DB::beginTransaction();
         
         try {
-            DB::beginTransaction();
-            Excel::import($model, $storage_path);
+            // set maximum php execution time
+            ini_set('max_execution_time', 0);
+            ini_set('memory_limit', -1);
+
+            Excel::import($model, Storage::disk('public')->path($file_path));
             DB::commit();
         } catch (\Exception $e) {
             DB::rollBack();
-            Storage::disk('public')->delete($path . $filename);
+            Storage::disk('public')->delete($file_path);
             printlog($e->getMessage());
-            throw new Error(trans('import.import_process_failed') . ' OR Try a different file format');
+            throw ValidationException::withMessages([trans('import.import_process_failed') . ' OR Try a different file format']);
         }
 
         $row_count = $model->getRowCount();
-        if (!$row_count) throw new Error(trans('import.import_process_failed') . " {$row_count} rows imported");
-        Storage::disk('public')->delete($path . $filename);
-        
-        return response()->json([
-            'status' => 'Success', 
-            'message' => trans('import.import_process_success') . " {$row_count} rows imported successfully",
-        ]);
-    }
+        if (!$row_count) throw ValidationException::withMessages([trans('import.import_process_failed') . " {$row_count} rows imported"]);
+        Storage::disk('public')->delete($file_path);
+
+        return redirect()->back()->with('flash_success', trans('import.import_process_success') . " {$row_count} rows imported successfully");
+    }    
 }
--- resources/views/focus/import/index.blade.php
+++ resources/views/focus/import/index.blade.php
@@ -16,7 +16,7 @@
                     <h4>{{ $data['title'] }}</h4><hr>
                     <p class="alert alert-light mb-2">
                         Imported data format should be as per downloaded template file. 
-                        <a href="{{ route('biller.import.sample_template', $data['type']) }}" target="_blank" id="download-btn">
+                        <a href="{{ asset('storage/app/public/sample/' . $data['type'] . '.csv') }}" download>
                             <b>{{ trans('import.download_template') }}</b> ({{ $data['title'] }}).
                         </a>
                     </p>
