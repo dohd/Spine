1651518ba759e36c604e97cc59971e973a32d337
Dohd <alloicebrian@gmail.com>
2023-04-13 12:10:45 +0300

    Menu header update Prospects

--- resources/views/focus/prospects/partials/prospects-header-buttons.blade.php
+++ resources/views/focus/prospects/partials/prospects-header-buttons.blade.php
@@ -1,10 +1,10 @@
 <div class="btn-group" role="group" aria-label="Basic example">
-    <a href="{{ route('biller.leads.index') }}" class="btn btn-info  btn-lighten-2">
+    <a href="{{ route('biller.prospects.index') }}" class="btn btn-info  btn-lighten-2">
         <i class="fa fa-list-alt"></i> {{ trans('general.list') }}
     </a>
     @permission('create-client' ) 
-        <a href="{{ route('biller.leads.create') }}" class="btn btn-pink  btn-lighten-3">
-            <i class="fa fa-plus-circle"></i> Ticket
+        <a href="{{ route('biller.prospects.create') }}" class="btn btn-pink  btn-lighten-3">
+            <i class="fa fa-plus-circle"></i> Prospect
         </a>
         &nbsp;&nbsp;
         <a href="{{ route('biller.djcs.create') }}" class="btn btn-success  btn-lighten-3">
