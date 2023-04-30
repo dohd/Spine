ed9d964868a5f9657f2be923c970d9523dd295a8
Dohd <alloicebrian@gmail.com>
2023-04-30 09:54:01 +0300

    Add edit button to milestone

--- resources/views/focus/projects/tabs/milestone.blade.php
+++ resources/views/focus/projects/tabs/milestone.blade.php
@@ -1,8 +1,7 @@
 <div class="tab-pane" id="tab_data2" aria-labelledby="tab2" role="tabpanel">
     {{-- @if(project_access($project->id)) --}}
-        <button type="button" class="btn btn-info" id="addt" data-toggle="modal"
-                data-target="#AddMileStoneModal">
-                <i class="fa fa-plus-circle"></i> Milestone
+        <button type="button" class="btn btn-info" id="addt" data-toggle="modal" data-target="#AddMileStoneModal">
+            <i class="fa fa-plus-circle"></i> Milestone
         </button>
     {{-- @endif --}}
     <ul class="timeline">
@@ -12,8 +11,9 @@
         @endphp
         @foreach ($project->milestones as $row)
             <li class="{!! (!$flag)? 'timeline-inverted' : '' !!}" id="m_{{$row['id']}}">
-                <div class="timeline-badge"
-                        style="background-color:@if ($row['color']) {{$row['color']}} @else #0b97f4  @endif;">{{$total}}</div>
+                <div class="timeline-badge" style="background-color:@if ($row['color']) {{$row['color']}} @else #0b97f4  @endif;">
+                    {{$total}}
+                </div>
                 <div class="timeline-panel">
                     <div class="timeline-heading">
                         <h4 class="timeline-title">{{$row['name']}}</h4>
@@ -26,16 +26,23 @@
                     {{-- @if (project_access($project->id)) --}}
                         <div class="timeline-body mb-1">
                             <p>{{$row['note']}}</p>
-                            <p>Extimated Milestone Amount: <b>{{amountFormat($row['extimated_milestone_amount'])}}</b></p>
-                            <a href="#" class=" delete-object" data-object-type="2" data-object-id="{{$row['id']}}">
-                                <i class="danger fa fa-trash"></i>
-                            </a>
+                            <p>Extimated Milestone Amount: <b>{{ amountFormat($row['extimated_milestone_amount']) }}</b></p>
                         </div>
                     {{-- @endif --}}
                     <small class="text-muted"><i class="fa fa-user"></i>
-                        <strong>{{$row->creator->first_name}}  {{$row->creator->last_name}}</strong>
+                        <strong>{{ @$row->creator->fullname }}</strong>
                         <i class="fa fa-clock-o"></i> {{trans('general.created')}} {{dateTimeFormat($row['created_at'])}}
                     </small>
+                    <div>
+                        <div class="btn-group">
+                            <a href="#" class="delete-object" data-object-type="2" data-object-id="{{$row['id']}}">
+                                <i class="fa fa-trash danger"></i>
+                            </a> &nbsp;&nbsp;
+                            <a href="#" class="edit-object"  data-object-id="{{$row['id']}}">
+                                <i class="ft ft-edit"></i>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </li>
             @php
