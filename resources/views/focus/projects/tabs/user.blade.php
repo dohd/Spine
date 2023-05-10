e00d4a1f757305e64dedbb89e9b441e2fb0779df
Dohd <alloicebrian@gmail.com>
2023-05-10 15:19:15 +0300

    Update project users tab

--- resources/views/focus/projects/tabs/user.blade.php
+++ resources/views/focus/projects/tabs/user.blade.php
@@ -2,8 +2,7 @@
     <div class="card">
         <div class="card-header mb-0">
             <h4 class="card-title">{{trans('projects.users')}}</h4>
-            <a class="heading-elements-toggle"><i
-                        class="fa fa-ellipsis-v font-medium-3"></i></a>
+            <a class="heading-elements-toggle"><i class="fa fa-ellipsis-v font-medium-3"></i></a>
             <div class="heading-elements">
                 <ul class="list-inline mb-0">
                     <li><a data-action="collapse"><i class="ft-minus"></i></a></li>
@@ -12,27 +11,31 @@
                 </ul>
             </div>
         </div>
+
         <div class="card-content collapse show">
             <div class="card-content">
                 <div class="card-body  py-0 px-0">
                     <div class="list-group">
-                        @foreach ($project->users as $row)
-                            <a href="javascript:void(0)" class="list-group-item">
+                        @foreach ($project->users as $i => $row)
+                            <a href="javascript:" class="list-group-item">
                                 <div class="media">
-                                    <div class="media-left pr-1"><span
-                                                class="avatar avatar-sm"><img
-                                                    src="{{ Storage::disk('public')->url('app/public/img/users/' . @$row->picture) }}"><i></i></span>
+                                    <div class="media-left pr-1">
+                                        <span class="avatar avatar-sm">
+                                            <img src="{{ Storage::disk('public')->url('app/public/img/users/' . @$row->picture) }}">
+                                        </span>
                                     </div>
-                                    <div class="media-body w-100">
-                                        <h6 class="media-heading mb-0">{{$row['first_name']}} {{$row['last_name']}}</h6>
-
+                                    <div class="media-body w-25">
+                                        <h6 class="media-heading mb-0 text-center" style="font-size:1.5em;">{{ $i+1 }}. {{ $row->fullname }}</h6>
+                                    </div>
+                                    <div class="media-body w-75">
+                                        <h6 class="media-heading mb-0" style="font-size:1.5em;">{{ @$row->role->name }}</h6>
                                     </div>
                                 </div>
-                            </a>@endforeach
+                            </a>
+                        @endforeach
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
 </div>
