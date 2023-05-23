214ccfc4299b94c23b3fc2afe2c6510de107aeb5
Dohd <alloicebrian@gmail.com>
2023-05-23 08:14:30 +0300

    Update unlink withholding edit

--- app/Models/withholding/Traits/WithholdingAttribute.php
+++ app/Models/withholding/Traits/WithholdingAttribute.php
@@ -15,7 +15,7 @@ trait WithholdingAttribute
     public function getActionButtonsAttribute()
     {
         return $this->getViewButtonAttribute("manage-withholding-cert", "biller.withholdings.show")
-             .' '.$this->getEditButtonAttribute("edit-withholding-cert", "biller.withholdings.edit")
+             .' '.$this->getEditButtonAttribute("edit-withholding-cert*", "biller.withholdings.edit")
              .' '.$this->getDeleteButtonAttribute("delete-withholding-cert", "biller.withholdings.destroy");
     }
 }
