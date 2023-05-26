a2936d989e71564ab4c59692a6fb715c133d1c7b
Dohd <alloicebrian@gmail.com>
2023-05-26 18:39:32 +0300

    Add balance column

--- app/Http/Controllers/Focus/verification/VerificationQuotesTableController.php
+++ app/Http/Controllers/Focus/verification/VerificationQuotesTableController.php
@@ -70,11 +70,20 @@ class VerificationQuotesTableController extends Controller
                 return $customer;
             })
             ->addColumn('total', function ($quote) {
-                if ($quote->currency) return amountFormat($quote->total, $quote->currency->id);
+                if ($quote->currency) 
+                    return amountFormat($quote->total, $quote->currency->id);
                 return numberFormat($quote->total);
             })
             ->addColumn('verified_total', function ($quote) {
-                if ($quote->currency) return amountFormat($quote->verified_total, $quote->currency->id);
+                if ($quote->currency) 
+                    return amountFormat($quote->verified_total, $quote->currency->id);
+                return numberFormat($quote->verified_total);
+            })
+            ->addColumn('balance', function ($quote) {
+                $balance = $quote->total - $quote->verified_total;
+                if (round($quote->verified_total) > round($quote->total)) $balance = 0;
+                if ($quote->currency) 
+                    return amountFormat($balance, $quote->currency->id);
                 return numberFormat($quote->verified_total);
             })
             ->addColumn('lpo_number', function($quote) {
