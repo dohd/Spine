2d034fb8f3e91b4ece0177c676a1517e46e8a0b2
Dohd <alloicebrian@gmail.com>
2023-05-28 10:49:40 +0300

    Add project relationship

--- app/Models/items/Traits/GoodsreceivenoteItemRelationship.php
+++ app/Models/items/Traits/GoodsreceivenoteItemRelationship.php
@@ -5,9 +5,15 @@ namespace App\Models\items\Traits;
 use App\Models\goodsreceivenote\Goodsreceivenote;
 use App\Models\items\PurchaseorderItem;
 use App\Models\product\ProductVariation;
+use App\Models\project\Project;
 
 trait GoodsreceivenoteItemRelationship
 {
+    public function project()
+    {
+        return $this->belongsTo(Project::class, 'itemproject_id');
+    }
+
     public function goodsreceivenote()
     {
         return $this->belongsTo(Goodsreceivenote::class, 'goods_receive_note_id');
