f90b1841ad0d0da46d476c07cbb063302e7a621c
Dohd <alloicebrian@gmail.com>
2023-12-21 08:17:25 +0300

    Update tenant service view template

--- resources/views/focus/tenant_services/view.blade.php
+++ resources/views/focus/tenant_services/view.blade.php
@@ -1,16 +1,16 @@
 @extends ('core.layouts.app')
 
-@section('title', 'Leave Category Management')
+@section('title', 'Tenant Service Management')
 
 @section('content')
 <div class="content-wrapper">
     <div class="content-header row mb-1">
         <div class="content-header-left col-6">
-            <h4 class="content-header-title">Leave Category Management</h4>
+            <h4 class="content-header-title">Tenant Service Management</h4>
         </div>
         <div class="col-6">
             <div class="btn-group float-right">
-                @include('focus.leave_category.partials.leave-category-header-buttons')
+                @include('focus.tenant_services.partials.tenant-services-header-buttons')
             </div>
         </div>
     </div>
@@ -21,19 +21,9 @@
                 <div class="card-body">
                     <table class="table table-bordered table-sm">
                         @php
-                            $gender = '';
-                            switch ($leave_category->gender) {
-                                case 'a': $gender = 'All'; break;
-                                case 'm': $gender = 'Male'; break;
-                                case 'f': $gender = 'Female'; break;
-                            }
+                            
                             $details = [
-                                'Title' => $leave_category->title,
-                                'Gender' => $gender,
-                                'Policy' => $leave_category->policy,
-                                'No of Days' => $leave_category->qty,
-                                'Payable Leave' => $leave_category->is_payable? 'Yes' : 'No',
-                                'Encashed Leave' => $leave_category->is_encashed? 'Yes' : 'No'
+                                
                             ];
                         @endphp
                         @foreach ($details as $key => $val)
