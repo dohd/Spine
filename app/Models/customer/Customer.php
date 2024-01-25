b0ba4e1e2498a4e5a84bcf6fea17d76b050dad03
Dohd <alloicebrian@gmail.com>
2024-01-25 12:02:14 +0300

    Add user_id and ins on branch, customer and journal

--- app/Models/customer/Customer.php
+++ app/Models/customer/Customer.php
@@ -79,11 +79,12 @@ class Customer extends Model
 
         static::creating(function ($instance) {
             $instance->ins = auth()->user()->ins;
+            $instance->user_id = auth()->user()->id;
             return $instance;
         });
 
         static::addGlobalScope('ins', function ($builder) {
-            $builder->where('ins', '=', auth()->user()->ins);
+            $builder->where('ins', auth()->user()->ins);
         });
     }
 
